808e629a2a8ada6b1fc6c9f24842647440f449d3
Lan Tian <xuyh0120@outlook.com>
2021-11-02 15:31:29 -0500

    Revert "nginx: temporarily disable proxy protocol (impl bug with plain mode)"
    This reverts commit c8bc92d8c0885266a7fd346233bd43acca6f2bd0.

--- common/apps/nginx-proxy.nix
+++ common/apps/nginx-proxy.nix
@@ -40,12 +40,14 @@ in
           server {
             listen 43;
             listen [::]:43;
-            proxy_pass ${thisHost.ltnet.IPv4Prefix}.1:43;
+            proxy_pass ${thisHost.ltnet.IPv4Prefix}.1:13243;
+            proxy_protocol on;
           }
           server {
             listen 70;
             listen [::]:70;
-            proxy_pass ${thisHost.ltnet.IPv4Prefix}.1:70;
+            proxy_pass ${thisHost.ltnet.IPv4Prefix}.1:13270;
+            proxy_protocol on;
           }
         }
       '';
