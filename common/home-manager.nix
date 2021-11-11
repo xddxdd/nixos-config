ae0eba187dd1d5dd8521f684d2bd697aac4b4d1c
Lan Tian <xuyh0120@outlook.com>
2021-11-11 01:12:16 -0600

    Revert "home-manager: deploy with deploy-rs"
    This reverts commit 676e5adec15522539e53b5d32c23f0d75a5a3251.

---
+++ common/home-manager.nix
@@ -0,0 +1,17 @@
+{ inputs, ... }:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.lantian = {
+    imports = [
+      ./home-lantian.nix
+      inputs.nixos-vscode-server.nixosModules.homeManager
+    ];
+    services.auto-fix-vscode-server.enable = true;
+  };
+}
