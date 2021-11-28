702ff01533313fe60f218a09411e07076131d39b
Lan Tian <xuyh0120@outlook.com>
2021-11-28 03:42:09 -0600

    genshin-helper: use custom package instead of docker container

--- common/apps/genshin-helper.nix
+++ common/apps/genshin-helper.nix
@@ -3,8 +3,25 @@
 {
   age.secrets.genshin-impact-cookies.file = ../../secrets/genshin-impact-cookies.age;
 
-  virtualisation.oci-containers.containers.genshin-helper = {
-    image = "yindan/genshinhelper:2.0.2";
-    environmentFiles = [ config.age.secrets.genshin-impact-cookies.path ];
+  systemd.services.genshin-helper = {
+    serviceConfig.Type = "oneshot";
+    script = "${pkgs.nur.repos.xddxdd.genshin-checkin-helper}/bin/genshin-checkin-helper-once";
+    serviceConfig = {
+      EnvironmentFile = config.age.secrets.genshin-impact-cookies.path;
+    };
+    unitConfig = {
+      After = "network.target";
+    };
+  };
+
+  systemd.timers.genshin-helper = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "genshin-helper.service" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 09:00:00 Asia/Shanghai";
+      Persistent = true;
+      RandomizedDelaySec = "12h";
+      Unit = "genshin-helper.service";
+    };
   };
 }
