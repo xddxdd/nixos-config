27c45f34b8af85bb2a9c93b4e9ff2e2887338605
Lan Tian <xuyh0120@outlook.com>
2021-11-30 01:16:55 -0600

    dns: add mailtie forwarding as backup

--- dns/common-records.nix
+++ dns/common-records.nix
@@ -5,9 +5,11 @@ with dns;
   ForwardEmail = [
     (MX { name = "@"; priority = 10; target = "mx1.forwardemail.net."; })
     (MX { name = "@"; priority = 10; target = "mx2.forwardemail.net."; })
+    (MX { name = "@"; priority = 20; target = "mx.mailtie.com."; })
     (TXT { name = "@"; contents = "v=DMARC1; p=none"; })
     (TXT { name = "@"; contents = "v=spf1 a mx include:spf.forwardemail.net -all"; })
     (TXT { name = "@"; contents = "forward-email=xuyh0120@gmail.com"; })
+    (TXT { name = "@"; contents = "mailtie=xuyh0120@gmail.com"; })
     (TXT { name = "_dmarc"; contents = "v=DMARC1; p=none"; })
   ];
 }
