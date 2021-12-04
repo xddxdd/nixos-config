510463ac2fbf29583c748bc8943a40ec718b2d9e
Lan Tian <xuyh0120@outlook.com>
2021-12-04 01:00:30 -0600

    general: lockdown rootfs

--- common/impermanence.nix
+++ common/impermanence.nix
@@ -3,8 +3,6 @@
 {
   environment.persistence."/nix/persistent" = {
     directories = [
-      "/home"
-      "/root"
       "/var"
     ];
     files = [
@@ -20,13 +18,11 @@
 
   age.sshKeyPaths = [ "/nix/persistent/etc/ssh/ssh_host_ed25519_key" ];
 
-  fileSystems."/home".neededForBoot = true;
-  fileSystems."/root".neededForBoot = true;
   fileSystems."/var".neededForBoot = true;
 
   fileSystems."/" = {
     device = "tmpfs";
     fsType = "tmpfs";
-    options = [ "relatime" "mode=755" ];
+    options = [ "relatime" "mode=755" "nosuid" "noexec" "nodev" ];
   };
 }
