358312afe7a28676d498920e41cad21e308cbf4f
Lan Tian <xuyh0120@outlook.com>
2022-01-14 03:33:01 -0600

    home: setup profile-sync-daemon

---
+++ home/components/profile-sync-daemon.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  xdg.configFile."psd/psd.conf".text = ''
+    # https://github.com/graysky2/profile-sync-daemon/blob/master/common/psd.conf
+    USE_OVERLAYFS="yes"
+    USE_SUSPSYNC="yes"
+    USE_BACKUPS="yes"
+    BACKUP_LIMIT=1
+  '';
+}
