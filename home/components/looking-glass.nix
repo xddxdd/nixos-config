376e56c8113fbeeb7d54d765aa4cc5a4b910587a
Lan Tian <xuyh0120@outlook.com>
2022-01-23 01:19:56 -0600

    home-manager: add config for looking glass client

--- home/components/looking-glass.nix
+++ home/components/looking-glass.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  xdg.configFile."looking-glass/client.ini".text = pkgs.lib.generators.toINI { } {
+    app.shmFile = "/dev/kvmfr0";
+    input.escapeKey = 119;
+    input.rawMouse = "yes";
+    win.fullScreen = "yes";
+    spice.enable = "no";
+  };
+
   xdg.dataFile = {
     "applications/looking-glass.desktop".text = ''
       [Desktop Entry]
