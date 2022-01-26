7c059400cd51917e3cc364d907d5af4b5b32dbc1
Lan Tian <xuyh0120@outlook.com>
2022-01-26 01:55:39 -0600

    networking: bird: babeld: throttle CPU usage

--- common/apps/bird.nix
+++ common/apps/bird.nix
@@ -300,6 +300,8 @@ in
     '';
   };
 
+  systemd.services.bird2.serviceConfig.CPUQuota = "10%";
+
   systemd.services.bird-lgproxy-go = {
     description = "Bird-lgproxy-go";
     wantedBy = [ "multi-user.target" ];
