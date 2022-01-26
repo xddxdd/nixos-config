c58bb9bafe683089837ec56299b6f1f96b133a8f
Lan Tian <xuyh0120@outlook.com>
2022-01-26 01:32:59 -0600

    general: disable watchdog

--- common/common.nix
+++ common/common.nix
@@ -134,6 +134,9 @@
       # Disable coredump
       "fs.suid_dumpable" = 0;
       "kernel.core_pattern" = pkgs.lib.mkForce "|${pkgs.coreutils}/bin/false";
+
+      # Other optimizations
+      "kernel.nmi_watchdog" = 0;
     };
   };
 
