bff8b39b50ac3777909a9d215c181c523d023022
Lan Tian <xuyh0120@outlook.com>
2022-02-06 17:55:07 -0600

    postgresql: use packaged phppgadmin

--- common/optional-apps/postgresql.nix
+++ common/optional-apps/postgresql.nix
@@ -16,13 +16,19 @@ in
 
   systemd.services.postgresql.serviceConfig = LT.serviceHarden;
 
+  age.secrets.phppgadmin-conf = {
+    file = ../../secrets/phppgadmin-conf.age;
+    owner = "nginx";
+    group = "nginx";
+  };
+  systemd.tmpfiles.rules = [
+    "L+ /etc/phppgadmin/config.inc.php - - - - ${config.age.secrets.phppgadmin-conf.path}"
+  ];
   services.nginx.virtualHosts."pga.lantian.pub" = pkgs.lib.mkIf config.lantian.enable-php {
     listen = LT.nginx.listenHTTPS;
-    root = "/var/www/pga.lantian.pub";
+    root = "${pkgs.phppgadmin}";
     locations = LT.nginx.addCommonLocationConf {
-      "/" = {
-        index = "index.php";
-      };
+      "/".index = "index.php";
     };
     extraConfig = LT.nginx.makeSSL "lantian.pub_ecc"
       + LT.nginx.commonVhostConf true
