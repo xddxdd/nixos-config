50c5da77ad34c443f5d79d764b0bf17cac9368a1
Lan Tian <xuyh0120@outlook.com>
2022-02-12 18:09:09 -0600

    general: update netboot.xyz

--- common/required-components/boot-params.nix
+++ common/required-components/boot-params.nix
@@ -30,10 +30,15 @@
         memtest86.enable = true;
         splashImage = null;
 
-        extraFiles = pkgs.lib.mkIf pkgs.stdenv.isx86_64 {
-          "netboot.xyz.lkrn" = ../files/netboot.xyz.lkrn;
+        extraFiles = {
+          "netboot.xyz.efi" = pkgs.lib.mkIf (pkgs.stdenv.isx86_64 && config.boot.loader.grub.efiSupport) "${pkgs.netboot-xyz}/netboot.xyz.efi";
+          "netboot.xyz.lkrn" = pkgs.lib.mkIf (pkgs.stdenv.isx86_64 && !config.boot.loader.grub.efiSupport) "${pkgs.netboot-xyz}/netboot.xyz.lkrn";
         };
-        extraEntries = pkgs.lib.optionalString pkgs.stdenv.isx86_64 ''
+        extraEntries = pkgs.lib.optionalString (pkgs.stdenv.isx86_64 && config.boot.loader.grub.efiSupport) ''
+          menuentry "Netboot.xyz" {
+            chainloader @bootRoot@/netboot.xyz.efi;
+          }
+        '' + pkgs.lib.optionalString (pkgs.stdenv.isx86_64 && !config.boot.loader.grub.efiSupport) ''
           menuentry "Netboot.xyz" {
             linux16 @bootRoot@/netboot.xyz.lkrn;
           }
