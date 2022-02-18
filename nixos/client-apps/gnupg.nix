326f35f1d7b71560e6fe26e2412f02661ce50f34
Lan Tian <xuyh0120@outlook.com>
2022-02-18 17:20:40 -0600

    gui: setup gnupg

---
+++ nixos/client-apps/gnupg.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      pinentryFlavor = "qt";
+      enableBrowserSocket = true;
+      enableExtraSocket = true;
+      enableSSHSupport = true;
+    };
+    dirmngr.enable = true;
+  };
+}
