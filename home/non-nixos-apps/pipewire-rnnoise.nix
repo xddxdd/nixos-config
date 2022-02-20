a1c782546c64a0f57616d9503cd8abcbec971bc5
Lan Tian <xuyh0120@outlook.com>
2022-02-20 01:44:55 -0600

    home-manager: add pipewire rnnoise settings

---
+++ home/non-nixos-apps/pipewire-rnnoise.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  # https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Filter-Chain#virtual-surround
+  xdg.configFile."pipewire/pipewire.conf.d/rnnoise.conf".text = ''
+    context.modules = [
+      { name = libpipewire-module-filter-chain
+        args = {
+          node.name =  "rnnoise_source"
+          node.description =  "Noise Canceling source"
+          media.name =  "Noise Canceling source"
+          filter.graph = {
+            nodes = [
+              {
+                type = ladspa
+                name = rnnoise
+                plugin = ${pkgs.noise-suppression-for-voice}/lib/ladspa/librnnoise_ladspa.so
+                label = noise_suppressor_stereo
+                control = {
+                  "VAD Threshold (%)" 50.0
+                }
+              }
+            ]
+          }
+          capture.props = {
+            node.passive = true
+          }
+          playback.props = {
+            media.class = Audio/Source
+          }
+        }
+      }
+    ]
+  '';
+}
