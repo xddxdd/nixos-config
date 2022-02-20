84b2c3d0b32e15ed61d690bbc6c661bcadaa81df
Lan Tian <xuyh0120@outlook.com>
2022-02-20 03:25:16 -0600

    gui: increase pipewire resample quality

---
+++ home/gui-apps/pipewire.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  xdg.configFile."pipewire/client.conf.d/override.conf".text = ''
+    context.modules = [
+      stream.properties = {
+        resample.quality = 10
+      }
+    ]
+  '';
+
+  xdg.configFile."pipewire/pipewire-pulse.conf.d/override.conf".text = ''
+    context.modules = [
+      stream.properties = {
+        resample.quality = 10
+      }
+    ]
+  '';
+}
