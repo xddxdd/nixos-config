c8975d011bf43773615f90edbb486f576ee4be37
Lan Tian <xuyh0120@outlook.com>
2022-02-20 03:11:24 -0600

    gui: switch pipewire to use wireplumber

--- nixos/client-components/kde.nix
+++ nixos/client-components/kde.nix
@@ -7,10 +7,14 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    pulse.enable = true;
-    jack.enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
+    jack.enable = true;
+    pulse.enable = true;
+
+    media-session.enable = false;
+    wireplumber.enable = true;
   };
 
   users.users.lantian.extraGroups = [ "video" "audio" "users" "input" ];
