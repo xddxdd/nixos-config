6957c4172613266c0730713cd3860b20e654bcca
Lan Tian <xuyh0120@outlook.com>
2022-03-06 12:29:41 -0600

    gui: setup global environment variables

--- nixos/client-apps/fcitx.nix
+++ nixos/client-apps/fcitx.nix
@@ -10,4 +10,13 @@
       libsForQt5.fcitx5-qt
     ];
   };
+
+  environment.variables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    INPUT_METHOD = "fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
+  };
 }
