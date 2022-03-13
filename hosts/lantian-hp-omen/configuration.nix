0cf65929f5846fad379d6446568405b0d7cd1d7b
Lan Tian <xuyh0120@outlook.com>
2022-03-13 14:40:34 -0500

    general: adjust underclock on lantian-hp-omen

--- hosts/lantian-hp-omen/configuration.nix
+++ hosts/lantian-hp-omen/configuration.nix
@@ -21,7 +21,7 @@ in
 
   environment.etc."intel-undervolt.conf".text = ''
     undervolt 0 'CPU' -80
-    undervolt 1 'GPU' -80
+    undervolt 1 'GPU' -70
     undervolt 2 'CPU Cache' 0
     undervolt 3 'System Agent' 0
     undervolt 4 'Analog I/O' 0
