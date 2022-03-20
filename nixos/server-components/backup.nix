fccaacde355dfa3013ef2c3245a6daad7f54b33b
Lan Tian <xuyh0120@outlook.com>
2022-03-20 01:36:47 -0500

    general: use kopia for backups

--- nixos/server-components/backup.nix
+++ nixos/server-components/backup.nix
@@ -1,66 +1,116 @@
-{ pkgs, config, ... }:
+{ pkgs, config, utils, ... }:
 
 let
-  backupExcluded = [
-    "/var/lib/cni"
-    "/var/lib/containers"
-    "/var/lib/docker"
-    "/var/lib/docker-dind"
-    "/var/lib/filebeat"
-    "/var/lib/grafana"
-    "/var/lib/journalbeat"
-    "/var/lib/machines"
-    "/var/lib/private"
-    "/var/lib/systemd"
-    "/var/lib/udisks2"
-    "/var/lib/vm"
-  ];
+  kopiaStorage = {
+    ibm-cos = {
+      "type" = "s3";
+      "config" = {
+        "bucket" = { _secret = config.age.secrets.kopia-ibm-cos-bucket.path; };
+        "endpoint" = "s3.us.cloud-object-storage.appdomain.cloud";
+        "accessKeyID" = { _secret = config.age.secrets.kopia-ibm-cos-ak.path; };
+        "secretAccessKey" = { _secret = config.age.secrets.kopia-ibm-cos-sk.path; };
+        "sessionToken" = "";
+      };
+    };
+    scaleway = {
+      "type" = "s3";
+      "config" = {
+        "bucket" = { _secret = config.age.secrets.kopia-scaleway-bucket.path; };
+        "endpoint" = "s3.fr-par.scw.cloud";
+        "accessKeyID" = { _secret = config.age.secrets.kopia-scaleway-ak.path; };
+        "secretAccessKey" = { _secret = config.age.secrets.kopia-scaleway-sk.path; };
+        "sessionToken" = "";
+      };
+    };
+  };
 
-  backupIncluded = [
-    "/var/backup"
-    "/var/lib"
-  ];
+  kopiaIgnored = pkgs.writeText ".kopiaignore" ''
+    media/
+    sync-servers/
+    var/cache/
+    var/lib/btrfs/
+    var/lib/cni/
+    var/lib/containers/
+    var/lib/docker/
+    var/lib/docker-dind/
+    var/lib/filebeat/
+    var/lib/grafana/
+    var/lib/machines/
+    var/lib/os-prober/
+    var/lib/private/
+    var/lib/prometheus/
+    var/lib/systemd/
+    var/lib/udisks2/
+    var/lib/vm/
+    var/log/
+  '';
 
-  backupScript = configPath: ''
-    FILENAME=/nix/backup/${config.networking.hostName}-$(${pkgs.coreutils}/bin/date +%Y-%m-%d).tar.zst
+  kopiaConfig = name: storage: {
+    inherit storage;
+    "caching" = {
+      "cacheDirectory" = "/var/cache/kopia/${name}";
+      "maxCacheSize" = 10485760;
+      "maxMetadataCacheSize" = 10485760;
+      "maxListCacheDuration" = 30;
+    };
+    "hostname" = config.networking.hostName;
+    "username" = "root";
+    "description" = "Backup repository";
+    "enableActions" = false;
+    "formatBlobCacheDuration" = 900000000000;
+  };
 
-    ${pkgs.coreutils}/bin/mkdir -p /nix/backup/
-    ${pkgs.coreutils}/bin/rm -rf /nix/backup/*
+  kopiaScript = name: storage: ''
+    export KOPIA_CACHE_DIRECTORY=/var/cache/kopia/${name}
+    export KOPIA_CONFIG_PATH=/run/kopia-repository.config
+    export KOPIA_LOG_DIR=/var/log/kopia
+    export KOPIA_PASSWORD=$(cat ${config.age.secrets.kopia-pw.path})
 
-    ${pkgs.gnutar}/bin/tar \
-      ${builtins.concatStringsSep " " (builtins.map (v: "--exclude=${v}") backupExcluded)} \
-      -I ${pkgs.zstd}/bin/zstd -cf /nix/backup/${config.networking.hostName}-$(date +%Y-%m-%d).tar.zst \
-      ${builtins.concatStringsSep " " backupIncluded}
+    ${utils.genJqSecretsReplacementSnippet
+      (kopiaConfig name storage)
+      "/run/kopia-repository.config"}
 
-    echo "Uploading to Dropbox"
-    ${pkgs.rclone}/bin/rclone --config ${configPath} mkdir dropbox:/Backups/ || ${pkgs.coreutils}/bin/true
-    ${pkgs.rclone}/bin/rclone --config ${configPath} delete dropbox:/Backups/${config.networking.hostName}-$(date +%Y-%m-%d --date='3 days ago').tar.zst || ${pkgs.coreutils}/bin/true
-    ${pkgs.rclone}/bin/rclone --config ${configPath} copy $FILENAME dropbox:/Backups/ || ${pkgs.coreutils}/bin/true
+    ${pkgs.kopia}/bin/kopia snapshot create \
+      /nix/.snapshot/persistent
 
-    #echo "Uploading to OneDrive"
-    #${pkgs.rclone}/bin/rclone --config ${configPath} mkdir onedrive:/Backups/ || ${pkgs.coreutils}/bin/true
-    #${pkgs.rclone}/bin/rclone --config ${configPath} delete onedrive:/Backups/${config.networking.hostName}-$(date +%Y-%m-%d --date='3 days ago').tar.zst || ${pkgs.coreutils}/bin/true
-    #${pkgs.rclone}/bin/rclone --config ${configPath} copy $FILENAME onedrive:/Backups/ || ${pkgs.coreutils}/bin/true
-
-    echo "Uploading to IBM COS"
-    ${pkgs.rclone}/bin/rclone --config ${configPath} delete ibm:lantian/${config.networking.hostName}-$(date +%Y-%m-%d --date='3 days ago').tar.zst || ${pkgs.coreutils}/bin/true
-    ${pkgs.rclone}/bin/rclone --config ${configPath} copy $FILENAME ibm:lantian/ || ${pkgs.coreutils}/bin/true
-
-    echo "Uploading to Scaleway"
-    ${pkgs.rclone}/bin/rclone --config ${configPath} delete scaleway:lantian/${config.networking.hostName}-$(date +%Y-%m-%d --date='10 days ago').tar.zst || ${pkgs.coreutils}/bin/true
-    ${pkgs.rclone}/bin/rclone --config ${configPath} copy $FILENAME scaleway:lantian/ || ${pkgs.coreutils}/bin/true
-
-    echo "Uploading to Soyoustart Backup FTP"
-    ${pkgs.rclone}/bin/rclone --config ${configPath} delete sys-ftp:/${config.networking.hostName}-$(date +%Y-%m-%d --date='30 days ago').tar.zst || ${pkgs.coreutils}/bin/true
-    ${pkgs.rclone}/bin/rclone --config ${configPath} copy $FILENAME sys-ftp:/ || ${pkgs.coreutils}/bin/true
+    rm -f $KOPIA_CONFIG_PATH
   '';
 in
 {
-  age.secrets.rclone-conf.file = pkgs.secrets + "/rclone-conf.age";
+  age.secrets.kopia-ibm-cos-ak.file = pkgs.secrets + "/kopia/ibm-cos-ak.age";
+  age.secrets.kopia-ibm-cos-bucket.file = pkgs.secrets + "/kopia/ibm-cos-bucket.age";
+  age.secrets.kopia-ibm-cos-sk.file = pkgs.secrets + "/kopia/ibm-cos-sk.age";
+  age.secrets.kopia-pw.file = pkgs.secrets + "/kopia/pw.age";
+  age.secrets.kopia-scaleway-ak.file = pkgs.secrets + "/kopia/scaleway-ak.age";
+  age.secrets.kopia-scaleway-bucket.file = pkgs.secrets + "/kopia/scaleway-bucket.age";
+  age.secrets.kopia-scaleway-sk.file = pkgs.secrets + "/kopia/scaleway-sk.age";
 
   systemd.services.backup = {
     serviceConfig.Type = "oneshot";
-    script = backupScript config.age.secrets.rclone-conf.path;
+    script = ''
+      SNAPSHOT_DIR=/nix/.snapshot
+
+    '' + (if config.fileSystems."/nix".fsType == "btrfs" then ''
+      # Btrfs snapshot
+      [ -e "$SNAPSHOT_DIR" ] && ${pkgs.btrfs-progs}/bin/btrfs subvolume delete $SNAPSHOT_DIR
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume snapshot -r /nix $SNAPSHOT_DIR
+
+    '' else ''
+      # Fake snapshot with link
+      [ -e "$SNAPSHOT_DIR" ] && rm -f $SNAPSHOT_DIR
+      ln -s /nix $SNAPSHOT_DIR
+
+    '') + (builtins.concatStringsSep "\n" (
+      pkgs.lib.mapAttrsToList kopiaScript kopiaStorage
+    )) + (if config.fileSystems."/nix".fsType == "btrfs" then ''
+      # Remove snapshot
+      ${pkgs.btrfs-progs}/bin/btrfs subvolume delete $SNAPSHOT_DIR
+
+    '' else ''
+      # Remove snapshot
+      rm -f $SNAPSHOT_DIR
+
+    '');
   };
 
   systemd.timers.backup = {
@@ -72,4 +122,12 @@ in
       Unit = "backup.service";
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "d /var/cache/kopia 700 root root"
+    "d /var/log/kopia 700 root root"
+    "C /nix/persistent/.kopiaignore - - - - ${kopiaIgnored}"
+  ] ++ (pkgs.lib.mapAttrsToList
+    (n: v: "d /var/cache/kopia/${n} 700 root root")
+    kopiaStorage);
 }
