4567fb98f238e42989678fb263b96243138416b2
Lan Tian <xuyh0120@outlook.com>
2022-03-22 00:21:34 -0500

    gui: add looking glass (preliminary work)

---
+++ nixos/client-apps/looking-glass.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    kvmfr
+  ];
+  boot.extraModprobeConfig = ''
+    options kvmfr static_size_mb=64
+  '';
+  boot.kernelModules = [ "kvmfr" ];
+}
