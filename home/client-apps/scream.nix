3baead32936aaeaa862649435c2ad8cfa3cae14a
Lan Tian <xuyh0120@outlook.com>
2022-03-26 20:31:48 -0500

    vfio: receive vm sound with scream

---
+++ home/client-apps/scream.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  systemd.user.services.scream = {
+    Service = {
+      ExecStart = "${pkgs.scream}/bin/scream -i virbr0 -v";
+      Restart = "always";
+      RestartSec = "3";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+}
