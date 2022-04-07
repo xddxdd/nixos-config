2ab85892aad72f8090d8f01bb65a21e3ed5f7ccb
Lan Tian <xuyh0120@outlook.com>
2022-04-07 15:37:50 -0500

    gui: add dnsmasq forward rules for network manager

--- nixos/client-components/network-manager.nix
+++ nixos/client-components/network-manager.nix
@@ -1,6 +1,16 @@
 { config, pkgs, lib, ... }:
 
+let
+  LT = import ../../helpers { inherit config pkgs lib; };
+in
 {
+  environment.etc."NetworkManager/dnsmasq.d/dns-forward.conf".text =
+    builtins.concatStringsSep "\n"
+      (lib.flatten
+        ((builtins.map (n: [ "server=/${n}/172.18.0.253" "server=/${n}/fdbc:f9dc:67ad:2547::53" ])
+          (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones)))
+        ++ [ "" ]));
+
   environment.persistence."/nix/persistent" = {
     directories = [
       "/etc/NetworkManager/system-connections"
@@ -11,6 +21,7 @@
 
   networking.networkmanager = {
     enable = true;
+    dns = "dnsmasq";
     unmanaged = [ "interface-name:*,except:interface-name:eth*,except:interface-name:wlan*,except:interface-name:nm-*" ];
   };
 
