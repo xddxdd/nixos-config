7aabe0705478617b6f434a72001871c122a79e23
Lan Tian <xuyh0120@outlook.com>
2022-04-24 16:49:48 -0500

    nginx: hide email for dn42 & neonetwork registries

--- nixos/optional-apps/gitea.nix
+++ nixos/optional-apps/gitea.nix
@@ -92,6 +92,15 @@ in
         proxyPass = "http://unix:/run/gitea/gitea.sock";
         extraConfig = LT.nginx.locationProxyConf;
       };
+      "/backup/" = {
+        proxyPass = "http://unix:/run/gitea/gitea.sock";
+        extraConfig = ''
+          body_filter_by_lua_block {
+            -- https://stackoverflow.com/a/201378
+            ngx.arg[1] = ngx.re.gsub(ngx.arg[1], [[(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])]], "[EMAIL HIDDEN]");
+          }
+        '';
+      };
       "= /user/login".return = "302 /user/oauth2/Keycloak";
     };
     extraConfig = LT.nginx.makeSSL "lantian.pub_ecc"
