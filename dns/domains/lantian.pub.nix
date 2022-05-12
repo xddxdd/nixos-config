0b726d07db4e506fa5839d1033b70933058c9db7
Lan Tian <xuyh0120@outlook.com>
2022-05-12 13:36:57 -0500

    dns: ignore hosts handled by CF workers

--- dns/domains/lantian.pub.nix
+++ dns/domains/lantian.pub.nix
@@ -32,9 +32,9 @@ let
   ];
 
   externalServices = [
-    (CNAME { name = "backblaze"; target = "f002.backblazeb2.com."; cloudflare = true; })
+    (IGNORE { name = "backblaze"; }) # Handled by CF Worker
     (CNAME { name = "comments"; target = "cname.vercel-dns.com."; })
-    (CNAME { name = "ga"; target = "lantian.pub."; cloudflare = true; })
+    (IGNORE { name = "ga"; }) # Handled by CF Worker
     (CNAME { name = "gcore"; target = "cl-47f440a2.gcdn.co."; })
     (CNAME { name = "github-pages"; target = "lantian1998.github.io."; })
     (CNAME { name = "netlify"; target = "lantian.netlify.com."; })
