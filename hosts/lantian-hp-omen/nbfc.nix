cc64c68aabbb0d24dc18ab675f0eb6ab99da7722
Lan Tian <xuyh0120@outlook.com>
2022-05-11 23:05:17 -0500

    general: add acpi-ec kernel module for nbfc

--- hosts/lantian-hp-omen/nbfc.nix
+++ hosts/lantian-hp-omen/nbfc.nix
@@ -1,21 +1,28 @@
 { config, pkgs, lib, ... }:
 
 {
-  systemd.services.nbfc-linux = let
-    cfg = pkgs.writeText "nbfc.json" (builtins.toJSON {
-      SelectedConfigId = "HP Omen 17t-ck000";
-      EmbeddedControllerType = "ec_sys_linux";
-    });
-  in {
-    description = "NBFC-Linux";
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.kmod ];
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "3";
-      ExecStart = "${pkgs.nbfc-lantian}/bin/nbfc_service -c ${cfg}";
-      TimeoutStopSec = "5";
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    acpi-ec
+  ];
+  boot.kernelModules = [ "acpi_ec" ];
+
+  systemd.services.nbfc-linux =
+    let
+      cfg = pkgs.writeText "nbfc.json" (builtins.toJSON {
+        SelectedConfigId = "HP Omen 17t-ck000";
+        EmbeddedControllerType = "ec_sys_linux";
+      });
+    in
+    {
+      description = "NBFC-Linux";
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.kmod ];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "3";
+        ExecStart = "${pkgs.nbfc-lantian}/bin/nbfc_service -c ${cfg}";
+        TimeoutStopSec = "5";
+      };
     };
-  };
 }
--- nixos/common-components/boot-params.nix
+++ nixos/common-components/boot-params.nix
@@ -1,51 +1,15 @@
 { pkgs, lib, config, ... }:
 
 {
-  boot = {
-    kernelParams = [
-      "audit=0"
-      "cgroup_enable=memory"
-      "net.ifnames=0"
-      "swapaccount=1"
-    ];
-    kernelPackages =
-      if pkgs.stdenv.isx86_64 then
-        pkgs.linuxPackagesFor pkgs.linux-xanmod-lantian
-      else pkgs.linuxPackages_latest;
-    kernelModules = [ "cryptodev" ];
-    extraModulePackages = with config.boot.kernelPackages; [
-      cryptodev
-    ];
-
-    initrd = {
-      compressor = "zstd";
-      compressorArgs = [ "-19" "-T0" ];
-      includeDefaultModules = false;
-    };
-
-    loader = {
-      grub = {
-        enable = true;
-        default = "saved";
-        version = 2;
-        splashImage = null;
-        font = lib.mkDefault
-          "${pkgs.terminus_font_ttf}/share/fonts/truetype/TerminusTTF.ttf";
-        fontSize = lib.mkDefault 16;
-      };
-    };
-
-    kernel.sysctl = {
-      # https://wiki.archlinux.org/title/Security#Kernel_hardening
-      "kernel.dmesg_restrict" = 1;
-      "kernel.kptr_restrict" = 1;
-      "net.core.bpf_jit_harden" = 1;
-      "kernel.unprivileged_bpf_disabled" = 1;
-      "kernel.yama.ptrace_scope" = 1;
-      "kernel.kexec_load_disabled" = 1;
-
-      # Other optimizations
-      "kernel.nmi_watchdog" = 0;
+  boot.loader = {
+    grub = {
+      enable = true;
+      default = "saved";
+      version = 2;
+      splashImage = null;
+      font = lib.mkDefault
+        "${pkgs.terminus_font_ttf}/share/fonts/truetype/TerminusTTF.ttf";
+      fontSize = lib.mkDefault 16;
     };
   };
 
