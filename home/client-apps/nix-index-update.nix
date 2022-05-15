069a44aac771ce35cb0859ac2b21141196c920f7
Lan Tian <xuyh0120@outlook.com>
2022-05-15 18:53:52 -0500

    home: add timer for nix-index-update

---
+++ home/client-apps/nix-index-update.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+{
+  systemd.user.services.nix-index-update = {
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.nix-index-update}/bin/nix-index-update";
+    };
+  };
+
+  systemd.user.timers.nix-index-update = {
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+    Timer = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "nix-index-update.service";
+    };
+  };
+}
