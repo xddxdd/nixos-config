9c5772d252c6b0507649645602c87069e3c444a6
Lan Tian <xuyh0120@outlook.com>
2022-05-16 16:41:46 -0500

    general: remove nonfunctional ccache

--- overlays/99-ccache.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, nixpkgs, ... }:
-
-final: prev: rec {
-  linux-xanmod-lantian = prev.linux-xanmod-lantian.override { stdenv = final.ccacheStdenv; };
-  tdesktop = prev.tdesktop.override { stdenv = final.ccacheStdenv; };
-}
