bc98e29e9db90fddf558f0a8751b75f6043a9c01
Lan Tian <xuyh0120@outlook.com>
2022-05-23 22:52:46 -0500

    gui: combine console font size to hidpi logic

--- nixos/client-components/wayland-hidpi.nix
+++ nixos/client-components/wayland-hidpi.nix
@@ -2,6 +2,20 @@
 
 let
   LT = import ../../helpers { inherit config pkgs lib; };
+
+  consoleFontSize =
+    let
+      targetSize = builtins.floor (config.lantian.wayland-hidpi * 16);
+    in
+    if targetSize >= 30 then 32
+    else if targetSize >= 26 then 28
+    else if targetSize >= 23 then 24
+    else if targetSize >= 21 then 22
+    else if targetSize >= 19 then 20
+    else if targetSize >= 17 then 18
+    else if targetSize >= 15 then 16
+    else if targetSize >= 13 then 14
+    else 12;
 in
 {
   options.lantian.wayland-hidpi = lib.mkOption {
@@ -10,9 +24,15 @@ in
     description = "HiDPI scaling for Wayland";
   };
 
-  config.environment.variables = {
-    GDK_SCALE = "1";
-    GDK_DPI_SCALE = builtins.toString config.lantian.wayland-hidpi;
-    QT_WAYLAND_FORCE_DPI = builtins.toString (builtins.floor (config.lantian.wayland-hidpi * 96));
+  config = {
+    environment.variables = {
+      GDK_SCALE = "1";
+      GDK_DPI_SCALE = builtins.toString config.lantian.wayland-hidpi;
+      QT_WAYLAND_FORCE_DPI = builtins.toString (builtins.floor (config.lantian.wayland-hidpi * 96));
+    };
+
+    boot.loader.grub.fontSize = consoleFontSize;
+    console.packages = with pkgs; [ terminus_font ];
+    console.font = "ter-v${builtins.toString consoleFontSize}n";
   };
 }
