e254fc5ff0820b1e4deaba3d5e530c5701e8d48e
Lan Tian <xuyh0120@outlook.com>
2022-05-25 02:47:51 -0500

    Revert "gui: delay autostart to fix tray icon missing"
    This reverts commit e838f4b552c0168655f4aaa5f02ff4bd28b00cdc.

--- helpers/autostart.nix
+++ helpers/autostart.nix
@@ -1,24 +1,15 @@
 { config, pkgs, lib, ... }:
 
-l: builtins.listToAttrs (builtins.map
-  ({ name, command }:
-    let
-      startScript = pkgs.writeShellScript "start.sh" ''
-        sleep 5
-        exec ${command}
-      '';
-    in
-    {
-      name = "autostart/${name}.desktop";
-      value = {
-        text = ''
-          [Desktop Entry]
-          Name=${name}
-          Exec=${startScript}
-          Type=Application
-          X-KDE-autostart-after=panel
-          X-KDE-autostart-phase=2
-        '';
-      };
-    })
-  l)
+l: builtins.listToAttrs (builtins.map ({ name, command }: {
+  name = "autostart/${name}.desktop";
+  value = {
+    text = ''
+      [Desktop Entry]
+      Name=${name}
+      Exec=${command}
+      Type=Application
+      X-KDE-autostart-after=panel
+      X-KDE-autostart-phase=2
+    '';
+  };
+}) l)
