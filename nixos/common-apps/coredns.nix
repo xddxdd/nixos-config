c39c78db0f823726ca52450994d837ec4a06e4c1
Lan Tian <xuyh0120@outlook.com>
2022-05-26 06:12:41 -0500

    networking: coredns: cache nextdns response longer

--- nixos/common-apps/coredns.nix
+++ nixos/common-apps/coredns.nix
@@ -23,6 +23,19 @@ in
             cache
           }
         '';
+        forwardToGoogleDNS = zone: ''
+          ${zone} {
+            any
+            bind ${LT.constants.localDNSBindIP}
+            bufsize 1232
+            loadbalance round_robin
+
+            forward . 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 {
+              tls_servername dns.google
+            }
+            cache
+          }
+        '';
       in
       builtins.concatStringsSep "\n" (
         [
@@ -36,10 +49,18 @@ in
               forward . tls://45.90.28.0 tls://45.90.30.0 tls://2a07:a8c0::0 tls://2a07:a8c1::0 {
                 tls_servername ${config.networking.hostName}-378897.dns.nextdns.io
               }
-              cache
+              cache {
+                success 32768 86400 3600
+                denial 32768 86400 3600
+              }
             }
           ''
         ]
+        ++ (builtins.map forwardToGoogleDNS [
+          "humio.com"
+          "resilio.com"
+          "syncthing.net"
+        ])
         ++ (builtins.map forwardToLtnet
           (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones ++ yggdrasilAlfisZones)))
         ++ [ "" ]
