8319e43dd9f16d0d501880bf22700b8c01e688fe
Lan Tian <xuyh0120@outlook.com>
2022-05-26 21:45:15 -0500

    coredns: dont use nextdns for servers

--- nixos/common-apps/coredns.nix
+++ nixos/common-apps/coredns.nix
@@ -27,6 +27,22 @@ in
 
     config =
       let
+        forwardToNextDNS = zone: ''
+          ${zone} {
+            any
+            bind ${LT.constants.localDNSBindIP}
+            bufsize 1232
+            loadbalance round_robin
+
+            forward . tls://45.90.28.0 tls://45.90.30.0 tls://2a07:a8c0::0 tls://2a07:a8c1::0 {
+              tls_servername ${config.networking.hostName}-378897.dns.nextdns.io
+            }
+            cache {
+              success 32768 86400 3600
+              denial 32768 86400 3600
+            }
+          }
+        '';
         forwardToLtnet = zone: ''
           ${zone} {
             any
@@ -51,30 +67,17 @@ in
             cache
           }
         '';
-      in
-      builtins.concatStringsSep "\n" (
-        [
-          ''
-            . {
-              any
-              bind ${LT.constants.localDNSBindIP}
-              bufsize 1232
-              loadbalance round_robin
 
-              forward . tls://45.90.28.0 tls://45.90.30.0 tls://2a07:a8c0::0 tls://2a07:a8c1::0 {
-                tls_servername ${config.networking.hostName}-378897.dns.nextdns.io
-              }
-              cache {
-                success 32768 86400 3600
-                denial 32768 86400 3600
-              }
-            }
-          ''
-        ]
-        ++ (builtins.map forwardToGoogleDNS bypassedDomains)
-        ++ (builtins.map forwardToLtnet
-          (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones ++ yggdrasilAlfisZones)))
-        ++ [ "" ]
-      );
+        cfgEntriesClient = [ (forwardToNextDNS ".") ]
+          ++ (builtins.map forwardToGoogleDNS bypassedDomains)
+          ++ (builtins.map forwardToLtnet
+          (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones ++ yggdrasilAlfisZones)));
+        cfgEntriesServer = [ (forwardToGoogleDNS ".") ]
+          ++ (builtins.map forwardToLtnet
+          (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones ++ yggdrasilAlfisZones)));
+
+        cfgEntries = if LT.this.role == LT.roles.client then cfgEntriesClient else cfgEntriesServer;
+      in
+      builtins.concatStringsSep "\n" (cfgEntries ++ [ "" ]);
   };
 }
