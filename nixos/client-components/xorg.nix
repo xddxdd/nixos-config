472b518bfe14d7bca0fa02a41f7e1860de5bad19
Lan Tian <xuyh0120@outlook.com>
2022-06-13 20:36:40 -0400

    gui: disable firefox sandbox again

--- nixos/client-components/xorg.nix
+++ nixos/client-components/xorg.nix
@@ -50,6 +50,7 @@ in
     # Firefox fixes
     MOZ_X11_EGL = "1";
     MOZ_USE_XINPUT2 = "1";
+    MOZ_DISABLE_RDD_SANDBOX = "1";
 
     # SDL Soundfont
     SDL_SOUNDFONTS = LT.constants.soundfontPath pkgs;
