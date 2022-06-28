14750e4d72da552faa5fa42b68d7e5b89a454e41
Lan Tian <xuyh0120@outlook.com>
2022-06-27 22:11:38 -0400

    gui: move many packages to home manager

--- home/client-apps/packages.nix
+++ home/client-apps/packages.nix
@@ -2,15 +2,76 @@
 
 let
   LT = import ../../helpers { inherit config pkgs lib; };
+
+  # Quickstart for wordle, set time offset to advance into next day
+  wordle = pkgs.writeShellScriptBin "wordle" ''
+    ${pkgs.libfaketime}/bin/faketime \
+      -f "+12h" \
+      ${pkgs.google-chrome}/bin/google-chrome-stable \
+      https://www.nytimes.com/games/wordle/index.html
+  '';
 in
 {
   home.packages = with pkgs; [
+    (LT.wrapNetns "ns-wg-lantian" deluge)
+    (LT.wrapNetns "ns-wg-lantian" qbittorrent-enhanced-edition)
+    (lutris.override { extraPkgs = p: with p; [ xdelta ]; })
+    android-tools
+    aria
+    audacious
+    baidupcs-go
+    bilibili
+    calibre
+    cloudpan189-go
+    colmena
+    deepspeech-gpu
+    deepspeech-wrappers
     discord
     element-desktop
+    ffmpeg
+    filezilla
+    firefox
+    flake.agenix.packages."${system}".agenix
     gcdemu
+    gimp-with-plugins
+    gnome.gedit
+    google-chrome
+    gopherus
+    kubectl
+    kubernetes-helm
+    libfaketime
+    librewolf
+    libsForQt5.ark
+    linphone
+    lm_sensors
+    mediainfo
+    megatools
+    minecraft
+    mpv
+    nix-top
+    nix-tree
+    nodePackages.node2nix
+    nvfetcher
+    osdlyrics
+    playonlinux
+    qq
+    quasselClient
+    rnix-lsp
     tdesktop
     thunderbird
+    tigervnc
+    transmission-qt
+    transmission-remote-gtk
     ulauncher
+    virt-manager
+    vscode
+    wechat-uos
+    winetricks
+    wineWowPackages.stable
+    wordle
+    wpsoffice
+    yuzu
+    zoom-us
   ];
 
   xdg.configFile = LT.autostart [
