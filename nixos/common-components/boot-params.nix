89a880fb91b52bd4713d48fc2b6f562b4b74ad60
Lan Tian <xuyh0120@outlook.com>
2022-06-29 19:30:24 -0400

    general: no longer use secure boot

--- nixos/common-components/boot-params.nix
+++ nixos/common-components/boot-params.nix
@@ -15,22 +15,6 @@
 
     } // (if pkgs.stdenv.isx86_64 then {
       efiInstallAsRemovable = lib.mkForce false;
-      extraPrepareConfig =
-        let
-          preloader = pkgs.fetchurl {
-            url = "https://blog.hansenpartnership.com/wp-uploads/2013/PreLoader.efi";
-            sha256 = "1ap5x74y2vapkbwd7bplvyz34g3f41bx0a982083ryd3qla6342h";
-          };
-          hashTool = pkgs.fetchurl {
-            url = "https://blog.hansenpartnership.com/wp-uploads/2013/HashTool.efi";
-            sha256 = "0s24q76cg0gn4m1ik5lls3m7lqkvxlf6p64h3ml21jx5xrhkb7wi";
-          };
-        in
-        ''
-          mkdir -p "@bootPath@/EFI/Boot"
-          cp -f ${preloader} "@bootPath@/EFI/Boot/bootx64.efi"
-          cp -f ${hashTool} "@bootPath@/EFI/Boot/HashTool.efi"
-        '';
       extraInstallCommands = lib.optionalString config.boot.loader.grub.efiSupport
         (lib.concatStringsSep "\n" (builtins.map
           (args:
@@ -40,8 +24,8 @@
               bootloaderId = if args.efiBootloaderId == null then "NixOS${efiSysMountPoint'}" else args.efiBootloaderId;
             in
             ''
-              cp -f ${args.path}/EFI/grub/grubx64.efi ${args.path}/EFI/Boot/loader.efi \
-                || cp -f ${args.path}/EFI/${bootloaderId}/grubx64.efi ${args.path}/EFI/Boot/loader.efi
+              cp -f ${args.path}/EFI/grub/grubx64.efi ${args.path}/EFI/Boot/bootx64.efi \
+                || cp -f ${args.path}/EFI/${bootloaderId}/grubx64.efi ${args.path}/EFI/Boot/bootx64.efi
             '')
           config.boot.loader.grub.mirroredBoots));
 
