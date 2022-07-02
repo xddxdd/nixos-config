16fe3e56fe2bc9789f503daf3a57a58c494918a0
Lan Tian <xuyh0120@outlook.com>
2022-07-01 23:09:57 -0400

    coredns: disable bypassing nextdns

--- nixos/common-apps/coredns.nix
+++ nixos/common-apps/coredns.nix
@@ -3,14 +3,6 @@
 let
   LT = import ../../helpers { inherit config pkgs lib; };
 
-  yggdrasilPublicPeers = builtins.filter (v: v != null) (builtins.map
-    (v:
-      let
-        matchResult = (builtins.match "^tls://([^:]*[a-zA-z][^:]*):.*$" v);
-      in
-      if matchResult == null then null else builtins.head matchResult)
-    (lib.flatten (lib.mapAttrsToList (k: v: v) (lib.importJSON ./yggdrasil/public-peers.json))));
-
   corednsClientNetns = LT.netns {
     name = "coredns-client";
   };
@@ -19,17 +11,6 @@ let
     "8.8.8.8"
     "2001:4860:4860::8888"
   ];
-
-  bypassedDomains = [
-    "cachix.org"
-    "dn42.us"
-    "hath.network"
-    "humio.com"
-    "lantian.pub"
-    "nixos.org"
-    "resilio.com"
-    "syncthing.net"
-  ] ++ yggdrasilPublicPeers;
 in
 {
   networking.nameservers = [ "${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.coredns-client}" ] ++ backupDNSServers;
@@ -65,21 +46,8 @@ in
             cache
           }
         '';
-        forwardToGoogleDNS = zone: ''
-          ${zone} {
-            any
-            bufsize 1232
-            loadbalance round_robin
-
-            forward . 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 {
-              tls_servername dns.google
-            }
-            cache
-          }
-        '';
 
         cfgEntries = [ (forwardToNextDNS ".") ]
-          ++ (builtins.map forwardToGoogleDNS bypassedDomains)
           ++ (builtins.map forwardToLtnet
           (with LT.constants; (dn42Zones ++ neonetworkZones ++ openNICZones ++ emercoinZones ++ yggdrasilAlfisZones)));
       in
