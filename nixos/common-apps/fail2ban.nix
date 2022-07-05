16d3215c50ce493bcb00a2162d13fa42f6b767e9
Lan Tian <xuyh0120@outlook.com>
2022-07-04 20:22:50 -0400

    fail2ban: setup

---
+++ nixos/common-apps/fail2ban.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../helpers { inherit config pkgs lib; };
+in
+{
+  environment.systemPackages = [ config.services.fail2ban.package ];
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    banaction = "nftables-multiport";
+    banaction-allports = "nftables-allports";
+    packageFirewall = pkgs.nftables;
+    ignoreIP = [
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.0.0.0/24"
+      "192.0.2.0/24"
+      "192.168.0.0/16"
+      "198.18.0.0/15"
+      "198.51.100.0/24"
+      "203.0.113.0/24"
+      "240.0.0.0/4"
+      "fc00::/7"
+    ];
+
+    daemonConfig = ''
+      [DEFAULT]
+      loglevel = NOTICE
+      logtarget = SYSLOG
+    '';
+
+    bantime-increment = {
+      enable = true;
+      overalljails = true;
+    };
+  };
+}
