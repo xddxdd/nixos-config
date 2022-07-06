cb247900061f56cea80f67d8b975954f4205a80a
Lan Tian <xuyh0120@outlook.com>
2022-07-06 00:36:33 -0400

    general: add cache for cuda packages

--- nixos/common-components/nix.nix
+++ nixos/common-components/nix.nix
@@ -25,12 +25,14 @@
         "https://xddxdd.cachix.org"
         "https://colmena.cachix.org"
         "https://nixos-cn.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         # "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
         "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
         "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
         "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
   };
