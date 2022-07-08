5d4d0bcbecd9f2d3b06b0f5e8622ee53aa29a2ac
Lan Tian <xuyh0120@outlook.com>
2022-07-07 20:09:36 -0400

    gui: add packages

--- home/client-apps/packages.nix
+++ home/client-apps/packages.nix
@@ -80,7 +80,7 @@ in
     ({ name = "element"; command = "${pkgs.element-desktop}/bin/element-desktop --hidden"; })
     ({ name = "gcdemu"; command = "${pkgs.gcdemu}/bin/gcdemu"; })
     ({ name = "linphone"; command = "${pkgs.linphone}/bin/linphone"; })
-    ({ name = "newsflash"; command = "${pkgs.newsflash}/bin/com.gitlab.newsflash"; })
+    ({ name = "newsflash"; command = "${pkgs.newsflash}/bin/com.gitlab.newsflash -h"; })
     ({ name = "telegram"; command = "${pkgs.tdesktop}/bin/telegram-desktop -autostart"; })
     ({ name = "thunderbird"; command = "${pkgs.thunderbird}/bin/thunderbird"; })
     ({ name = "ulauncher"; command = "${pkgs.ulauncher}/bin/ulauncher --hide-window"; })
