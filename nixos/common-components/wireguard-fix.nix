03c25f66f9af0ce0585ded655da4a7b3904edf82
Lan Tian <xuyh0120@outlook.com>
2022-07-16 15:57:51 -0400

    networking: fix wireguard not removing interface on stop

---
+++ nixos/common-components/wireguard-fix.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, options, ... }:
+
+{
+  config.systemd.services =
+    let
+      cfgToWg = n: v: lib.nameValuePair "wireguard-${n}" ({
+        serviceConfig.ExecStartPre = if v.interfaceNamespace == null
+          then [ "-${pkgs.iproute2}/bin/ip link del \"${n}\"" ]
+          else [ "-${pkgs.iproute2}/bin/ip netns exec \"${v.interfaceNamespace}\" ${pkgs.iproute2}/bin/ip link del \"${n}\"" ];
+      });
+    in
+    lib.mapAttrs' cfgToWg config.networking.wireguard.interfaces;
+}
