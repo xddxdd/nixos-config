99cbb7e2eac8cf4377dd1f5eb4e96372aa381fad
Lan Tian <xuyh0120@outlook.com>
2022-08-08 19:38:36 -0400

    oauth2-proxy: downgrade

--- nixos/common-apps/nginx/oauth2-proxy.nix
+++ nixos/common-apps/nginx/oauth2-proxy.nix
@@ -2,12 +2,30 @@
 
 let
   LT = import ../../../helpers { inherit config pkgs lib; };
+
+  oauth2Proxy = pkgs.buildGoModule rec {
+    pname = "oauth2-proxy";
+    version = "7.2.1";
+
+    src = pkgs.fetchFromGitHub {
+      repo = pname;
+      owner = "oauth2-proxy";
+      sha256 = "sha256-8hYsyHq0iyWzY/HHE4JWBtlaRcSTyM6BdAPcByThme8=";
+      rev = "v${version}";
+    };
+
+    vendorSha256 = "sha256-+5/j2lZJpyo67uRRSn4Fd8S2K0gfAGMe69OoEEdWijc=";
+
+    # Taken from https://github.com/oauth2-proxy/oauth2-proxy/blob/master/Makefile
+    ldflags = [ "-X main.VERSION=${version}" ];
+  };
 in
 {
   age.secrets.oauth2-proxy-conf.file = pkgs.secrets + "/oauth2-proxy-conf.age";
 
   services.oauth2_proxy = {
     enable = true;
+    package = oauth2Proxy;
     clientID = "oauth-proxy";
     cookie = {
       expire = "24h";
