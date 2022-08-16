4ba82cf4dd4b0f894ef66358e1fd03e83803b3c2
Lan Tian <xuyh0120@outlook.com>
2022-08-16 01:04:51 -0500

    matrix-synapse: add job to compress db state

--- nixos/optional-apps/matrix-synapse.nix
+++ nixos/optional-apps/matrix-synapse.nix
@@ -76,4 +76,30 @@ in
       + LT.nginx.commonVhostConf true
       + LT.nginx.noIndex true;
   };
+
+  systemd.services.synapse-compress-state = {
+    after = [ "matrix-synapse.service" ];
+    requires = [ "matrix-synapse.service" ];
+    script = ''
+      exec ${pkgs.matrix-synapse-tools.rust-synapse-compress-state}/bin/synapse_auto_compressor \
+        -p "host=/run/postgresql user=matrix-synapse dbname=matrix-synapse" \
+        -c 500 \
+        -n 100
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "matrix-synapse";
+    };
+  };
+
+  systemd.timers.synapse-compress-state = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "synapse-compress-state.service" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "4h";
+      Unit = "synapse-compress-state.service";
+    };
+  };
 }
