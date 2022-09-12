ccd39e9067c2015a6da89bcd9fc766bed36c4236
Lan Tian <xuyh0120@outlook.com>
2022-09-12 02:40:17 -0500

    asterisk: add connection to zadarma service

---
+++ nixos/optional-apps/asterisk/external-trunks.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../../helpers { inherit config pkgs; };
+in
+rec {
+  externalTrunk = { name, number, url }: ''
+    [${name}]
+    type=registration
+    outbound_auth=${name}
+    server_uri=sip:${url}
+    client_uri=sip:${number}@${url}
+    retry_interval=60
+    expiration=120
+    contact_user=${number}
+    line=yes
+    endpoint=${name}
+
+    [${name}](template-endpoint-common)
+    context=src-${name}
+    outbound_auth=${name}
+    from_user=${number}
+    aors=${name}
+
+    [${name}](template-aor)
+    contact=sip:${url}
+
+    [${name}]
+    type=identify
+    endpoint=${name}
+    match=${url}
+  '';
+}
