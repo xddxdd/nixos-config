c927dca49e3e847c20be2213d229e84ff42a521d
Lan Tian <xuyh0120@outlook.com>
2022-09-30 02:46:18 -0500

    Revert "nginx: use xuyh0120.win as default sni for old android devices"
    This reverts commit b75bb39ebd2886db743e6a864c59248889dc81b5.

--- nixos/common-apps/nginx/vhosts.nix
+++ nixos/common-apps/nginx/vhosts.nix
@@ -3,18 +3,6 @@
 let
   LT = import ../../../helpers { inherit config pkgs lib; };
 
-  localhostLocations = {
-    "= /.well-known/openid-configuration".extraConfig = ''
-      root ${files/openid-configuration};
-      try_files /openid-configuration =404;
-    '';
-    "= /openid-configuration".extraConfig = ''
-      root ${files/openid-configuration};
-      try_files /openid-configuration =404;
-    '';
-    "/generate_204".return = "204";
-  };
-
   addConfLantianPub = lib.recursiveUpdate {
     locations = LT.nginx.addCommonLocationConf { } {
       "/" = {
@@ -77,35 +65,31 @@ let
 in
 {
   services.nginx.virtualHosts = {
-    "localhost-https" = {
+    "localhost" = {
       listen = [
         { addr = "0.0.0.0"; port = 443; extraParameters = [ "ssl" "http2" ] ++ LT.nginx.listenDefaultFlags; }
         { addr = "[::]"; port = 443; extraParameters = [ "ssl" "http2" ] ++ LT.nginx.listenDefaultFlags; }
-      ];
-
-      locations = localhostLocations // {
-        "/".return = "444";
-      };
-
-      # Enable TLSv1 on default vhost, so config on other vhosts work
-      extraConfig = ''
-        access_log off;
-      ''
-      + LT.nginx.makeSSL "xuyh0120.win_ecc";
-    };
-
-    "localhost" = {
-      listen = [
         { addr = "0.0.0.0"; port = 80; extraParameters = LT.nginx.listenDefaultFlags; }
         { addr = "[::]"; port = 80; extraParameters = LT.nginx.listenDefaultFlags; }
       ];
 
-      locations = localhostLocations // {
+      locations = {
         "/".return = "301 https://$host$request_uri";
+        "= /.well-known/openid-configuration".extraConfig = ''
+          root ${files/openid-configuration};
+          try_files /openid-configuration =404;
+        '';
+        "= /openid-configuration".extraConfig = ''
+          root ${files/openid-configuration};
+          try_files /openid-configuration =404;
+        '';
+        "/generate_204".return = "204";
       };
 
       extraConfig = ''
         access_log off;
+        ssl_reject_handshake on;
+        ssl_stapling off;
       '';
     };
 
