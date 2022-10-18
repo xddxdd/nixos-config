6d3ee3187af807f6cb0a1bd162f34b176a285946
Lan Tian <xuyh0120@outlook.com>
2022-10-18 13:36:23 -0500

    waline: use own network for distribution

---
+++ nixos/common-apps/nginx/ingress-proxy.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../../helpers { inherit config pkgs lib; };
+in
+{
+  services.nginx.virtualHosts = {
+    "comments.lantian.pub" = {
+      listen = LT.nginx.listenHTTPS;
+      locations = LT.nginx.addCommonLocationConf { } {
+        "/".extraConfig = ''
+          proxy_pass http://${LT.hosts."oneprovider".ltnet.IPv4}:${LT.portStr.Waline};
+          proxy_set_header REMOTE-HOST $remote_addr;
+        '' + LT.nginx.locationProxyConf;
+        "= /".return = "302 /ui/";
+      };
+      extraConfig = LT.nginx.makeSSL "lantian.pub_ecc"
+        + LT.nginx.commonVhostConf true
+        + LT.nginx.noIndex true;
+    };
+  };
+}
