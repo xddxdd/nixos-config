5b60047d86bd0f774d882c06adfc4e8058a640fd
Lan Tian <xuyh0120@outlook.com>
2022-10-22 15:55:36 -0500

    general: replace timesyncd with chrony

---
+++ nixos/common-components/ntp.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../helpers { inherit config pkgs lib; };
+
+  ntpServers = [ ];
+  ntpPools = [
+    "nixos.pool.ntp.org"
+    "time.apple.com"
+    "time.cloudflare.com"
+    "time.google.com"
+    "time.windows.com"
+  ];
+in
+{
+  networking.timeServers = ntpServers;
+
+  services.timesyncd.enable = false;
+
+  services.chrony = rec {
+    enable = true;
+    servers = [ ];
+    extraConfig = ''
+      ${builtins.concatStringsSep "\n" (builtins.map (k: "server ${k} ${serverOption}") ntpServers)}
+      ${builtins.concatStringsSep "\n" (builtins.map (k: "pool ${k} ${serverOption}") ntpPools)}
+
+      rtcsync
+      cmdport 0
+    '';
+    serverOption = if config.networking.networkmanager.enable then "offline" else "iburst";
+  };
+
+  systemd.services.chronyd.restartIfChanged = !config.networking.networkmanager.enable;
+
+  networking.networkmanager.dispatcherScripts = [{
+    type = "basic";
+    source = pkgs.writeShellScript "chrony" ''
+      INTERFACE=$1
+      STATUS=$2
+
+      # Make sure we're always getting the standard response strings
+      LANG='C'
+
+      CHRONY=${config.services.chrony.package}/bin/chronyc
+
+      chrony_cmd() {
+        echo "Chrony going $1."
+        exec $CHRONY -a $1
+      }
+
+      nm_connected() {
+        [ "$(${pkgs.networkmanager}/bin/nmcli -t --fields STATE g)" = 'connected' ]
+      }
+
+      case "$STATUS" in
+        up)
+          chrony_cmd online
+        ;;
+        vpn-up)
+          chrony_cmd online
+        ;;
+        down)
+          # Check for active interface, take offline if none is active
+          nm_connected || chrony_cmd offline
+        ;;
+        vpn-down)
+          # Check for active interface, take offline if none is active
+          nm_connected || chrony_cmd offline
+        ;;
+      esac
+    '';
+  }];
+}
