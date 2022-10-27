3d4c33715c110eea60dc3bd00514cdd277f66c31
Lan Tian <xuyh0120@outlook.com>
2022-10-27 16:51:30 -0500

    hosts: enable hardware encoding on lt-hp-z220-sff

--- hosts/lt-hp-z220-sff/configuration.nix
+++ hosts/lt-hp-z220-sff/configuration.nix
@@ -26,6 +26,7 @@ in
     ./hardware-configuration.nix
 
     ../../nixos/client-components/network-manager.nix
+    ../../nixos/client-components/xorg.nix
 
     ../../nixos/optional-apps/flexget.nix
     ../../nixos/optional-apps/ksmbd.nix
@@ -37,24 +38,6 @@ in
   ];
 
   powerManagement.powertop.enable = true;
-
-  services.transmission.settings = {
-    # Speed limit of ourbits
-    speed-limit-down = 51200;
-    speed-limit-down-enabled = true;
-    speed-limit-up = 51200;
-    speed-limit-up-enabled = true;
-  };
-  system.activationScripts.transmission-download-auto =
-    let
-      cfg = config.services.transmission;
-    in
-    ''
-      install -d -m '${cfg.downloadDirPermissions}' -o '${cfg.user}' -g '${cfg.group}' '/mnt/storage/downloads-auto'
-    '';
-  systemd.services.transmission.serviceConfig.BindPaths = [
-    "/mnt/storage/downloads-auto"
-  ];
 
   # services.beesd.filesystems.root = {
   #   spec = "/nix";
@@ -85,14 +68,34 @@ in
 
   services.yggdrasil.regions = [ "united-states" "canada" ];
 
+  services.xserver.enable = lib.mkForce false;
+
   systemd.services.jellyfin = {
     after = [ "mnt-storage.mount" ];
     requires = [ "mnt-storage.mount" ];
   };
 
-  services.transmission.settings.download-dir = "/mnt/storage/downloads";
+  services.transmission.settings = {
+    download-dir = "/mnt/storage/downloads";
+
+    # Speed limit of ourbits
+    speed-limit-down = 51200;
+    speed-limit-down-enabled = true;
+    speed-limit-up = 51200;
+    speed-limit-up-enabled = true;
+  };
+  system.activationScripts.transmission-download-auto =
+    let
+      cfg = config.services.transmission;
+    in
+    ''
+      install -d -m '${cfg.downloadDirPermissions}' -o '${cfg.user}' -g '${cfg.group}' '/mnt/storage/downloads-auto'
+    '';
   systemd.services.transmission = {
     after = [ "mnt-storage.mount" ];
     requires = [ "mnt-storage.mount" ];
+    serviceConfig.BindPaths = [
+      "/mnt/storage/downloads-auto"
+    ];
   };
 }
