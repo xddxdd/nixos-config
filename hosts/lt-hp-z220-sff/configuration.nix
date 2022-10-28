c9276cdcbe4222f7c437f166d5846d96f6f51fd9
Lan Tian <xuyh0120@outlook.com>
2022-10-28 18:44:42 -0500

    hosts: reorganize config for lt-hp-z220-sff

--- hosts/lt-hp-z220-sff/configuration.nix
+++ hosts/lt-hp-z220-sff/configuration.nix
@@ -24,19 +24,21 @@ in
     ../../nixos/none.nix
 
     ./hardware-configuration.nix
+    ./media-center.nix
 
     ../../nixos/client-components/network-manager.nix
-    ../../nixos/client-components/xorg.nix
 
     ../../nixos/optional-apps/flexget.nix
     ../../nixos/optional-apps/ksmbd.nix
-    ../../nixos/optional-apps/jellyfin.nix
     ../../nixos/optional-apps/libvirt
     ../../nixos/optional-apps/netns-wg-lantian.nix
     ../../nixos/optional-apps/resilio.nix
-    ../../nixos/optional-apps/transmission-daemon.nix
   ];
 
+  # ECC RAM
+  hardware.rasdaemon.enable = true;
+
+  # Reduce idle power consumption
   powerManagement.powertop.enable = true;
 
   services.beesd.filesystems.root = {
@@ -67,35 +69,4 @@ in
   services.resilio.directoryRoot = lib.mkForce "/mnt/storage/media";
 
   services.yggdrasil.regions = [ "united-states" "canada" ];
-
-  services.xserver.enable = lib.mkForce false;
-
-  systemd.services.jellyfin = {
-    after = [ "mnt-storage.mount" ];
-    requires = [ "mnt-storage.mount" ];
-  };
-
-  services.transmission.settings = {
-    download-dir = "/mnt/storage/downloads";
-
-    # Speed limit of private trackets
-    speed-limit-down = 25600;
-    speed-limit-down-enabled = false;
-    speed-limit-up = 25600;
-    speed-limit-up-enabled = true;
-  };
-  system.activationScripts.transmission-download-auto =
-    let
-      cfg = config.services.transmission;
-    in
-    ''
-      install -d -m '${cfg.downloadDirPermissions}' -o '${cfg.user}' -g '${cfg.group}' '/mnt/storage/downloads-auto'
-    '';
-  systemd.services.transmission = {
-    after = [ "mnt-storage.mount" ];
-    requires = [ "mnt-storage.mount" ];
-    serviceConfig.BindPaths = [
-      "/mnt/storage/downloads-auto"
-    ];
-  };
 }
