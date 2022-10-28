34cc0cf1d7ee678d22dec8cac25963ab22b28369
Lan Tian <xuyh0120@outlook.com>
2022-10-28 18:44:42 -0500

    hosts: reorganize config for lt-hp-z220-sff

---
+++ hosts/lt-hp-z220-sff/media-center.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../helpers { inherit config pkgs lib; };
+in
+{
+  imports = [
+    ../../nixos/client-components/xorg.nix
+
+    ../../nixos/optional-apps/jellyfin.nix
+    ../../nixos/optional-apps/transmission-daemon.nix
+  ];
+
+  services.xserver.enable = lib.mkForce false;
+
+  systemd.services.jellyfin = {
+    after = [ "mnt-storage.mount" ];
+    requires = [ "mnt-storage.mount" ];
+  };
+
+  services.transmission.settings = {
+    download-dir = "/mnt/storage/downloads";
+
+    # Speed limit of private trackets
+    speed-limit-down = 25600;
+    speed-limit-down-enabled = false;
+    speed-limit-up = 25600;
+    speed-limit-up-enabled = true;
+  };
+  system.activationScripts.transmission-download-auto =
+    let
+      cfg = config.services.transmission;
+    in
+    ''
+      install -d -m '${cfg.downloadDirPermissions}' -o '${cfg.user}' -g '${cfg.group}' '/mnt/storage/downloads-auto'
+    '';
+  systemd.services.transmission = {
+    after = [ "mnt-storage.mount" ];
+    requires = [ "mnt-storage.mount" ];
+    serviceConfig.BindPaths = [
+      "/mnt/storage/downloads-auto"
+    ];
+  };
+}
