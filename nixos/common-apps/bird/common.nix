db79c67a86b5332ea464bd99f1939dae25ba0002
Lan Tian <xuyh0120@outlook.com>
2022-11-08 14:45:16 -0600

    bird: use dn42 communities within ltnet

--- nixos/common-apps/bird/common.nix
+++ nixos/common-apps/bird/common.nix
@@ -16,4 +16,22 @@ rec {
     LT_ROA_FAIL = "(${DN42_AS}, 2547, 0)";
     LT_ROA_UNKNOWN = "(${DN42_AS}, 2547, 1)";
   };
+
+  latencyToDN42Community = { latencyMs, badRouting, ... }:
+    if badRouting then 9 else
+    if latencyMs <= 3 then 1 else
+    if latencyMs <= 7 then 2 else
+    if latencyMs <= 20 then 3 else
+    if latencyMs <= 55 then 4 else
+    if latencyMs <= 148 then 5 else
+    if latencyMs <= 403 then 6 else
+    if latencyMs <= 1097 then 7 else
+    if latencyMs <= 2981 then 8 else
+    9;
+
+  typeToDN42Community = type:
+    if type == "openvpn" then 33 else
+    if type == "wireguard" then 34 else
+    if type == "gre" then 31 else
+    31;
 }
