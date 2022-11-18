c339996a06032bab21f53e1f435615b05d810fc4
Lan Tian <xuyh0120@outlook.com>
2022-11-18 00:01:52 -0600

    v2ray: do not directly connect to netease music unblock

--- nixos/client-apps/v2ray.nix
+++ nixos/client-apps/v2ray.nix
@@ -41,7 +41,10 @@ let
         streamSettings = {
           network = "grpc";
           security = "tls";
-          tlsSettings.serverName = "lantian.pub";
+          tlsSettings = {
+            serverName = "lantian.pub";
+            fingerprint = "firefox";
+          };
           grpcSettings = {
             serviceName = "ray";
             multiMode = false;
@@ -50,16 +53,6 @@ let
           };
         };
         tag = "proxy";
-      }
-      {
-        protocol = "shadowsocks";
-        settings.servers = [{
-          address = "music.desperadoj.com";
-          method = "aes-128-gcm";
-          password = "desperadoj.com_free_proxy_etg0";
-          port = 30001;
-        }];
-        tag = "music";
       }
     ];
     policy.levels."0" = {
@@ -71,48 +64,6 @@ let
       balancers = [ ];
       domainStrategy = "IPOnDemand";
       rules = [
-        {
-          domain = [
-            "full:api.iplay.163.com"
-            "full:apm3.music.163.com"
-            "full:apm.music.163.com"
-            "full:interface3.music.163.com"
-            "full:interface3.music.163.com.163jiasu.com"
-            "full:interface.music.163.com"
-            "full:music.163.com"
-          ];
-          outboundTag = "music";
-          type = "field";
-        }
-        {
-          ip = [
-            "39.105.63.80/32"
-            "39.105.175.128/32"
-            "45.254.48.1/32"
-            "47.100.127.239/32"
-            "59.111.160.195/32"
-            "59.111.160.197/32"
-            "59.111.181.35/32"
-            "59.111.181.38/32"
-            "59.111.181.60/32"
-            "101.71.154.241/32"
-            "103.126.92.132/32"
-            "103.126.92.133/32"
-            "112.13.119.17/32"
-            "112.13.119.18/32"
-            "112.13.122.1/32"
-            "112.13.122.4/32"
-            "115.236.118.33/32"
-            "115.236.121.1/32"
-            "118.24.63.156/32"
-            "182.92.170.253/32"
-            "193.112.159.225/32"
-            "223.252.199.66/32"
-            "223.252.199.67/32"
-          ];
-          outboundTag = "music";
-          type = "field";
-        }
         {
           outboundTag = "block";
           protocol = [ "bittorrent" ];
