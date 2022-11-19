655784fb6dd9dd2c68e4304f9d96b6b84849ea7c
Lan Tian <xuyh0120@outlook.com>
2022-11-19 16:41:39 -0600

    asterisk: add asty-crapper, add lenny infinite wait

---
+++ nixos/optional-apps/asterisk/apps/astycrapper.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, lib, ... }:
+
+let
+  LT = import ../../../helpers { inherit config pkgs; };
+
+  astyCrapper = pkgs.stdenvNoCC.mkDerivation rec {
+    pname = "asty-crapper";
+    version = "1.0";
+    src = pkgs.fetchurl {
+      url = "https://web.archive.org/web/20110217104551/http://www.linuxsystems.com.au/astycrapper/samples.tar.gz";
+      sha256 = "sha256-M/MjqHgSjktihIPLqZt1zSPMvYl2u63xsCVJb/HM3z0=";
+    };
+
+    buildPhase = ''
+      tar xf hellos.tar.gz
+      tar xf jordan.tar.gz
+    '';
+
+    installPhase = ''
+      mkdir -p $out/hellos $out/jordan
+      cp hellos/2.gsm $out/hellos/1.gsm
+      cp hellos/3.gsm $out/hellos/2.gsm
+      cp jordan/* $out/jordan/
+    '';
+  };
+in
+{
+  dialAstyCrapper = ''
+    [app-asty-crapper]
+    exten => b,1,Goto(app-asty-crapper-hello,b,1)
+
+    [app-asty-crapper-hello]
+    exten => b,1,Set(HELLO=''${IF($["0''${HELLO}"="03"]?1:$[0''${HELLO}+1])})
+    same => n,Playback(${astyCrapper}/hellos/''${HELLO})
+    same => n,BackgroundDetect(silence/10,1500)
+    same => n,Goto(app-asty-crapper-hello,b,1)
+    exten => talk,1,Goto(app-asty-crapper-loop,b,1)
+
+    [app-asty-crapper-loop]
+    exten => b,1,Set(LOOP=''${IF($["0''${LOOP}"="012"]?1:$[0''${LOOP}+1])})
+    same => n,Playback(${astyCrapper}/jordan/''${LOOP})
+    same => n,BackgroundDetect(silence/10,1500)
+    same => n,Goto(app-asty-crapper-hello,b,1)
+    exten => talk,1,Goto(app-asty-crapper-loop,b,1)
+  '';
+}
