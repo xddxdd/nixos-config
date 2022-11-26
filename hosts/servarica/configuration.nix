f93a3351056bff398b09d4d6e9ad59416653609f
Lan Tian <xuyh0120@outlook.com>
2022-11-25 18:39:00 -0600

    hosts: add servarica

---
+++ hosts/servarica/configuration.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, utils, inputs, ... }@args:
+
+let
+  LT = import ../../helpers args;
+in
+{
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  boot.initrd.systemd.enable = lib.mkForce false;
+
+  systemd.network.networks.eth0 = {
+    address = [ "104.152.209.126/24" ];
+    gateway = [ "104.152.209.1" ];
+    matchConfig.Name = "eth0";
+  };
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+
+  services.yggdrasil.regions = [ "united-states" "canada" ];
+}
