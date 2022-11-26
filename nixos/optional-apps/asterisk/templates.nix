37b5e9b89ffccfe9d99beaa6646524864454569a
Lan Tian <xuyh0120@outlook.com>
2022-11-26 05:22:27 -0600

    asterisk: reenable some codecs

--- nixos/optional-apps/asterisk/templates.nix
+++ nixos/optional-apps/asterisk/templates.nix
@@ -4,12 +4,59 @@ let
   LT = import ../../../helpers args;
 
   inherit (pkgs.callPackage ./common.nix args) dialRule enumerateList prefixZeros;
+
+  # http://www.cs.columbia.edu/~hgs/audio/codecs.html
+  # https://www.speex.org/comparison/
+  # https://en.wikipedia.org/wiki/Comparison_of_audio_coding_formats#Technical_details
+  codecs = [
+    # 48KHz
+    "opus"
+    "evs"
+
+    # 32KHz
+    "siren14" # 24/32/48Kbps
+    "speex32" # <44.2Kbps
+
+    # 16KHz
+    "amrwb"   # <23.85Kbps
+    "siren7"  # 16/24/32Kbps
+    "speex16" # <44.2Kbps
+    "g722"    # 64Kbps
+
+    # 8KHz
+    "g729"    # 8Kbps
+    "amr"     # <12.2Kbps
+    "gsm-efr" # 12.2Kbps
+    "gsm"     # 13Kbps
+    "g726"    # 16/24/32/40Kbps
+    "speex"   # <24.6Kbps
+    "ilbc"    # 13.3/15.2Kbps
+    "alaw"    # 64Kbps
+    "ulaw"    # 64Kbps
+
+    # Obsolete
+    "silk24"
+    "silk16"
+    "silk12"
+    "silk8"
+
+    # Uncompressed
+    # "slin192"
+    # "slin96"
+    "slin48"
+    "slin44"
+    "slin32"
+    "slin24"
+    "slin16"
+    "slin12"
+    "slin"
+  ];
 in
 rec {
   templates = ''
     [template-endpoint-common](!)
     type=endpoint
-    allow=opus,g722,alaw,ulaw,speex32,speex16,g729,g726,ilbc,speex
+    allow=${builtins.concatStringsSep "," codecs}
     direct_media=no
     rtp_symmetric=yes
     force_rport=yes
