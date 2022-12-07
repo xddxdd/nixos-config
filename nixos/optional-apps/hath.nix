e541b232c3125165b95424fd1793c030656d2ab2
Lan Tian <xuyh0120@outlook.com>
2022-12-06 19:04:28 -0600

    hosts: move data on oneprovider

--- nixos/optional-apps/hath.nix
+++ nixos/optional-apps/hath.nix
@@ -1,53 +1,93 @@
-{ pkgs, lib, config, utils, inputs, ... }@args:
+{ pkgs, lib, config, options, utils, inputs, ... }@args:
 
 let
   LT = import ../../helpers args;
-
-  hathCacheDir = "/var/cache/hath";
-  hathDataDir = "/var/lib/hath";
-  hathDownloadDir = "/var/lib/hath";
-  hathLogDir = "/var/log/hath";
-  hathTempDir = "/run/hath";
 in
 {
-  systemd.services.hath = {
-    description = "Hentai@Home";
-    after = [ "network.target" ];
-    wants = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    script = ''
-      exec ${pkgs.jre_headless}/bin/java \
-        -Xms16m -Xmx128m \
-        -jar ${pkgs.hath}/opt/HentaiAtHome.jar \
-        --cache-dir=${hathCacheDir} \
-        --data-dir=${hathDataDir} \
-        --download-dir=${hathDownloadDir} \
-        --log-dir=${hathLogDir} \
-        --temp-dir=${hathTempDir} \
-        --use-less-memory
-    '';
-    serviceConfig = LT.serviceHarden // {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "3";
-
-      MemoryDenyWriteExecute = false;
-      RuntimeDirectory = "hath";
-      StateDirectory = "hath";
-      CacheDirectory = "hath";
-      LogsDirectory = "hath";
-      User = "hath";
-      Group = "hath";
-
-      # Disable logging
-      StandardOutput = "null";
-      StandardError = "null";
+  options.lantian.hath = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+    cacheDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/cache/hath";
+    };
+    dataDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/hath";
+    };
+    downloadDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/hath";
+    };
+    logDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/log/hath";
+    };
+    tempDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/run/hath";
     };
   };
 
-  users.users.hath = {
-    group = "hath";
-    isSystemUser = true;
+  config = lib.mkIf config.lantian.hath.enable {
+    systemd.services.hath = {
+      description = "Hentai@Home";
+      after = [ "network.target" ];
+      wants = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      script = ''
+        exec ${pkgs.jre_headless}/bin/java \
+          -Xms16m -Xmx128m \
+          -jar ${pkgs.hath}/opt/HentaiAtHome.jar \
+          --cache-dir=${config.lantian.hath.cacheDir} \
+          --data-dir=${config.lantian.hath.dataDir} \
+          --download-dir=${config.lantian.hath.downloadDir} \
+          --log-dir=${config.lantian.hath.logDir} \
+          --temp-dir=${config.lantian.hath.tempDir} \
+          --use-less-memory
+      '';
+      serviceConfig = LT.serviceHarden // {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "3";
+
+        MemoryDenyWriteExecute = false;
+        RuntimeDirectory = "hath";
+        StateDirectory = "hath";
+        CacheDirectory = "hath";
+        LogsDirectory = "hath";
+        User = "hath";
+        Group = "hath";
+
+        ReadWritePaths = [
+          config.lantian.hath.cacheDir
+          config.lantian.hath.dataDir
+          config.lantian.hath.downloadDir
+          config.lantian.hath.logDir
+          config.lantian.hath.tempDir
+        ];
+
+        # Disable logging
+        StandardOutput = "null";
+        StandardError = "null";
+      };
+    };
+
+
+    systemd.tmpfiles.rules = [
+      "d ${config.lantian.hath.cacheDir} 755 hath hath"
+      "d ${config.lantian.hath.dataDir} 755 hath hath"
+      "d ${config.lantian.hath.downloadDir} 755 hath hath"
+      "d ${config.lantian.hath.logDir} 755 hath hath"
+      "d ${config.lantian.hath.tempDir} 755 hath hath"
+    ];
+
+    users.users.hath = {
+      group = "hath";
+      isSystemUser = true;
+    };
+    users.groups.hath = { };
   };
-  users.groups.hath = { };
 }
