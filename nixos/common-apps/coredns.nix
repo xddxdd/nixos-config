33c50f4c6448229168928aab44107940ad97ba3b
Lan Tian <xuyh0120@outlook.com>
2022-12-18 23:57:14 -0600

    networking: fix coredns not using dns over tls

--- nixos/common-apps/coredns.nix
+++ nixos/common-apps/coredns.nix
@@ -25,7 +25,7 @@ in
             bufsize 1232
             loadbalance round_robin
 
-            forward . 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 {
+            forward . tls://8.8.8.8 tls://8.8.4.4 tls://2001:4860:4860::8888 tls://2001:4860:4860::8844 {
               tls_servername dns.google
             }
             cache
