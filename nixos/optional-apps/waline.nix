7b6ac99edc0d5a3dee87b73abc05236c2ce37b7c
Lan Tian <xuyh0120@outlook.com>
2022-12-21 02:02:02 -0600

    nginx: combine comments.lantian.pub into lantian.pub (2/2)

--- nixos/optional-apps/waline.nix
+++ nixos/optional-apps/waline.nix
@@ -3,6 +3,22 @@
 {
   age.secrets.waline-env.file = inputs.secrets + "/waline-env.age";
 
+  services.nginx.virtualHosts = {
+    "comments.lantian.pub" = {
+      listen = LT.nginx.listenHTTPS;
+      locations = LT.nginx.addCommonLocationConf { } {
+        "/".extraConfig = ''
+          proxy_pass http://${LT.this.ltnet.IPv4}:${LT.portStr.Waline};
+          proxy_set_header REMOTE-HOST $remote_addr;
+        '' + LT.nginx.locationProxyConf;
+        "= /".return = "302 /ui/";
+      };
+      extraConfig = LT.nginx.makeSSL "lantian.pub_ecc"
+        + LT.nginx.commonVhostConf true
+        + LT.nginx.noIndex true;
+    };
+  };
+
   virtualisation.oci-containers.containers = {
     waline = {
       extraOptions = [ "--pull" "always" ];
