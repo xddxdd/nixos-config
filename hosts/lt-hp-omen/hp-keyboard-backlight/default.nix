df80f2a35a4292b196d6880d6849f1f8f6a9cca9
Lan Tian <xuyh0120@outlook.com>
2022-12-26 18:40:54 -0600

    hosts: setup dynamic keyboard backlight on lt-hp-omen

---
+++ hosts/lt-hp-omen/hp-keyboard-backlight/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, LT, config, utils, inputs, ... }@args:
+
+{
+  systemd.services.hp-keyboard-backlight = {
+    description = "HP Omen Keyboard Backlight";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "3";
+      ExecStart = "${pkgs.python3}/bin/python3 ${./script.py}";
+    };
+  };
+}
