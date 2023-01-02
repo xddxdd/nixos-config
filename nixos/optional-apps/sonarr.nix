5bb539b92dd76cba7b1832a954bf8a7c71843118
Lan Tian <xuyh0120@outlook.com>
2023-01-02 04:19:09 -0600

    sonarr: move lt-hp-z220-sff specific permission from global definition to host specific

--- nixos/optional-apps/sonarr.nix
+++ nixos/optional-apps/sonarr.nix
@@ -25,18 +25,9 @@
   };
   systemd.services.sonarr = {
     serviceConfig = LT.serviceHarden // {
-      AmbientCapabilities = "CAP_DAC_READ_SEARCH";
-      CapabilityBoundingSet = "CAP_DAC_READ_SEARCH";
       RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" "AF_NETLINK" ];
       StateDirectory = "sonarr";
       MemoryDenyWriteExecute = false;
-    };
-  };
-
-  systemd.services.qbittorrent = {
-    serviceConfig = {
-      AmbientCapabilities = "CAP_DAC_READ_SEARCH";
-      CapabilityBoundingSet = "CAP_DAC_READ_SEARCH";
     };
   };
 }
