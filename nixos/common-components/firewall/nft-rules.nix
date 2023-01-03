c474a2238e3a89dd3f0b72ec9355bf63d8cf6e69
Lan Tian <xuyh0120@outlook.com>
2023-01-03 13:14:43 -0600

    networking: forward range to wg-lantian clients instead of discrete ports

--- nixos/common-components/firewall/nft-rules.nix
+++ nixos/common-components/firewall/nft-rules.nix
@@ -1,113 +1,122 @@
 { pkgs, lib, LT, config, utils, inputs, ... }@args:
 
-pkgs.writeText "nft.conf" ''
-  #${pkgs.nftables-fullcone}/bin/nft -f
-  table inet lantian
-  delete table inet lantian
+let
+  serverPortForwards = lib.optionalString (LT.this.role == LT.roles.server) ''
+    # network namespace coredns
+    fib daddr type local tcp dport ${LT.portStr.DNS} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.coredns-authoritative}:${LT.portStr.DNS}
+    fib daddr type local udp dport ${LT.portStr.DNS} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.coredns-authoritative}:${LT.portStr.DNS}
+    fib daddr type local tcp dport ${LT.portStr.DNS} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.coredns-authoritative}]:${LT.portStr.DNS}
+    fib daddr type local udp dport ${LT.portStr.DNS} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.coredns-authoritative}]:${LT.portStr.DNS}
 
-  table inet lantian {
-    chain FILTER_INPUT {
-      type filter hook input priority 5; policy accept;
+    # network namespace yggdrasil-alfis
+    fib daddr type local tcp dport ${LT.portStr.YggdrasilAlfis} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.yggdrasil-alfis}:${LT.portStr.YggdrasilAlfis}
+    fib daddr type local tcp dport ${LT.portStr.YggdrasilAlfis} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.yggdrasil-alfis}]:${LT.portStr.YggdrasilAlfis}
+  '';
 
-      # Drop timestamp ICMP pkts
-      meta l4proto icmp icmp type timestamp-reply drop
-      meta l4proto icmp icmp type timestamp-request drop
+  wg-lantian = (lib.optionalString (LT.this.public.IPv4 != "")
+    (lib.concatStrings
+      (lib.mapAttrsToList
+        (n: { index, ... }: ''
+          ip daddr ${LT.this.public.IPv4} tcp dport { ${builtins.toString (LT.port.WGLanTian.ForwardStart + (index - 1) * 10)}-${builtins.toString (LT.port.WGLanTian.ForwardStart + index * 10 - 1)} } dnat to 192.0.2.${builtins.toString index}
+          ip daddr ${LT.this.public.IPv4} udp dport { ${builtins.toString (LT.port.WGLanTian.ForwardStart + (index - 1) * 10)}-${builtins.toString (LT.port.WGLanTian.ForwardStart + index * 10 - 1)} } dnat to 192.0.2.${builtins.toString index}
+        '')
+        LT.hosts))
+  )
+  + (lib.optionalString (LT.this.public.IPv6Subnet != "")
+    (lib.concatStrings
+      (lib.mapAttrsToList
+        (n: v: ''
+          ip6 daddr ${LT.this.public.IPv6Subnet}${builtins.toString v.index} dnat to fc00::${builtins.toString v.index}
+        '')
+        LT.hosts))
+  );
 
-      # Block non-DN42 traffic in DN42
-      iifname "dn42*" jump DN42_INPUT
+  text = ''
+    #${pkgs.nftables-fullcone}/bin/nft -f
+    table inet lantian
+    delete table inet lantian
+
+    table inet lantian {
+      chain FILTER_INPUT {
+        type filter hook input priority 5; policy accept;
+
+        # Drop timestamp ICMP pkts
+        meta l4proto icmp icmp type timestamp-reply drop
+        meta l4proto icmp icmp type timestamp-request drop
+
+        # Block non-DN42 traffic in DN42
+        iifname "dn42*" jump DN42_INPUT
+      }
+
+      chain FILTER_FORWARD {
+        type filter hook forward priority 5; policy accept;
+
+        # DN42 firewall rules
+        ${lib.concatMapStringsSep "\n" (p: ''iifname "dn42*" oifname "${p}*" reject'') LT.constants.wanInterfacePrefixes}
+
+        # Block non-DN42 traffic in DN42
+        iifname "dn42*" jump DN42_INPUT
+        oifname "dn42*" jump DN42_OUTPUT
+      }
+
+      chain FILTER_OUTPUT {
+        type filter hook output priority 5; policy accept;
+        oifname "dn42*" jump DN42_OUTPUT
+      }
+
+      chain NAT_PREROUTING {
+        type nat hook prerouting priority -95; policy accept;
+
+        # nginx whois & gopher server
+        fib daddr type local tcp dport ${LT.portStr.Whois} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.nginx-proxy}:${LT.portStr.Whois}
+        fib daddr type local tcp dport ${LT.portStr.Gopher} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.nginx-proxy}:${LT.portStr.Gopher}
+        fib daddr type local tcp dport ${LT.portStr.Whois} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.nginx-proxy}]:${LT.portStr.Whois}
+        fib daddr type local tcp dport ${LT.portStr.Gopher} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.nginx-proxy}]:${LT.portStr.Gopher}
+
+        ${serverPortForwards}
+        ${wg-lantian}
+      }
+
+      chain NAT_INPUT {
+        type nat hook input priority 105; policy accept;
+      }
+
+      chain NAT_OUTPUT {
+        type nat hook output priority -95; policy accept;
+      }
+
+      chain NAT_POSTROUTING {
+        type nat hook postrouting priority 105; policy accept;
+
+        # wg-lantian
+        ${lib.optionalString (LT.this.public.IPv6Subnet != "")
+          (builtins.concatStringsSep "\n"
+            (lib.mapAttrsToList (n: v:
+              "ip6 saddr fc00::${builtins.toString v.index} snat to ${LT.this.public.IPv6Subnet}${builtins.toString v.index}"
+            ) (lib.filterAttrs (n: v: v.role != LT.roles.server) LT.hosts)))}
+
+        ${lib.optionalString (LT.this.role == LT.roles.server) ''
+          # give nixos containers access to DN42
+          ip saddr 172.18.0.0/16 oifname "dn42-*" snat to ${LT.this.dn42.IPv4}
+          ip saddr 172.18.0.0/16 oifname "neo-*" snat to ${LT.this.neonetwork.IPv4}
+        ''}
+
+        ${lib.concatMapStringsSep "\n" (p: ''oifname "${p}*" fullcone'') (LT.constants.wanInterfacePrefixes ++ [ "virbr" ])}
+      }
+
+      # Helper chains
+      chain DN42_INPUT {
+        ${lib.concatMapStringsSep "\n" (p: "ip saddr ${p} return") LT.constants.dn42.IPv4}
+        ${lib.concatMapStringsSep "\n" (p: "ip6 saddr ${p} return") LT.constants.dn42.IPv6}
+        reject with icmpx type admin-prohibited
+      }
+
+      chain DN42_OUTPUT {
+        ${lib.concatMapStringsSep "\n" (p: "ip daddr ${p} return") LT.constants.dn42.IPv4}
+        ${lib.concatMapStringsSep "\n" (p: "ip6 daddr ${p} return") LT.constants.dn42.IPv6}
+        reject with icmpx type admin-prohibited
+      }
     }
-
-    chain FILTER_FORWARD {
-      type filter hook forward priority 5; policy accept;
-
-      # DN42 firewall rules
-      ${lib.concatMapStringsSep "\n" (p: ''iifname "dn42*" oifname "${p}*" reject'') LT.constants.wanInterfacePrefixes}
-
-      # Block non-DN42 traffic in DN42
-      iifname "dn42*" jump DN42_INPUT
-      oifname "dn42*" jump DN42_OUTPUT
-    }
-
-    chain FILTER_OUTPUT {
-      type filter hook output priority 5; policy accept;
-      oifname "dn42*" jump DN42_OUTPUT
-    }
-
-    chain NAT_PREROUTING {
-      type nat hook prerouting priority -95; policy accept;
-
-      # nginx whois & gopher server
-      fib daddr type local tcp dport ${LT.portStr.Whois} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.nginx-proxy}:${LT.portStr.Whois}
-      fib daddr type local tcp dport ${LT.portStr.Gopher} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.nginx-proxy}:${LT.portStr.Gopher}
-      fib daddr type local tcp dport ${LT.portStr.Whois} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.nginx-proxy}]:${LT.portStr.Whois}
-      fib daddr type local tcp dport ${LT.portStr.Gopher} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.nginx-proxy}]:${LT.portStr.Gopher}
-
-      ${lib.optionalString (LT.this.role == LT.roles.server) ''
-        # network namespace coredns
-        fib daddr type local tcp dport ${LT.portStr.DNS} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.coredns-authoritative}:${LT.portStr.DNS}
-        fib daddr type local udp dport ${LT.portStr.DNS} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.coredns-authoritative}:${LT.portStr.DNS}
-        fib daddr type local tcp dport ${LT.portStr.DNS} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.coredns-authoritative}]:${LT.portStr.DNS}
-        fib daddr type local udp dport ${LT.portStr.DNS} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.coredns-authoritative}]:${LT.portStr.DNS}
-
-        # network namespace yggdrasil-alfis
-        fib daddr type local tcp dport ${LT.portStr.YggdrasilAlfis} dnat ip to ${LT.this.ltnet.IPv4Prefix}.${LT.containerIP.yggdrasil-alfis}:${LT.portStr.YggdrasilAlfis}
-        fib daddr type local tcp dport ${LT.portStr.YggdrasilAlfis} dnat ip6 to [${LT.this.ltnet.IPv6Prefix}::${LT.containerIP.yggdrasil-alfis}]:${LT.portStr.YggdrasilAlfis}
-      ''}
-
-      # wg-lantian
-      ${lib.optionalString (LT.this.public.IPv4 != "") ''
-        ip daddr ${LT.this.public.IPv4} tcp dport { ${LT.portStr.Deluge.P2P} } dnat to 192.0.2.100
-        ip daddr ${LT.this.public.IPv4} udp dport { ${LT.portStr.Deluge.P2P} } dnat to 192.0.2.100
-        ip daddr ${LT.this.public.IPv4} tcp dport { ${LT.portStr.Transmission.P2P} } dnat to 192.0.2.101
-        ip daddr ${LT.this.public.IPv4} udp dport { ${LT.portStr.Transmission.P2P} } dnat to 192.0.2.101
-        ip daddr ${LT.this.public.IPv4} tcp dport { ${LT.portStr.qBitTorrent.P2P} } dnat to 192.0.2.102
-        ip daddr ${LT.this.public.IPv4} udp dport { ${LT.portStr.qBitTorrent.P2P} } dnat to 192.0.2.102
-      ''}
-      ${lib.optionalString (LT.this.public.IPv6Subnet != "")
-        (builtins.concatStringsSep "\n"
-          (lib.mapAttrsToList (n: v:
-            "ip6 daddr ${LT.this.public.IPv6Subnet}${builtins.toString v.index} dnat to fc00::${builtins.toString v.index}"
-          ) (lib.filterAttrs (n: v: v.role != LT.roles.server) LT.hosts)))}
-    }
-
-    chain NAT_INPUT {
-      type nat hook input priority 105; policy accept;
-    }
-
-    chain NAT_OUTPUT {
-      type nat hook output priority -95; policy accept;
-    }
-
-    chain NAT_POSTROUTING {
-      type nat hook postrouting priority 105; policy accept;
-
-      # wg-lantian
-      ${lib.optionalString (LT.this.public.IPv6Subnet != "")
-        (builtins.concatStringsSep "\n"
-          (lib.mapAttrsToList (n: v:
-            "ip6 saddr fc00::${builtins.toString v.index} snat to ${LT.this.public.IPv6Subnet}${builtins.toString v.index}"
-          ) (lib.filterAttrs (n: v: v.role != LT.roles.server) LT.hosts)))}
-
-      ${lib.optionalString (LT.this.role == LT.roles.server) ''
-        # give nixos containers access to DN42
-        ip saddr 172.18.0.0/16 oifname "dn42-*" snat to ${LT.this.dn42.IPv4}
-        ip saddr 172.18.0.0/16 oifname "neo-*" snat to ${LT.this.neonetwork.IPv4}
-      ''}
-
-      ${lib.concatMapStringsSep "\n" (p: ''oifname "${p}*" fullcone'') (LT.constants.wanInterfacePrefixes ++ [ "virbr" ])}
-    }
-
-    # Helper chains
-    chain DN42_INPUT {
-      ${lib.concatMapStringsSep "\n" (p: "ip saddr ${p} return") LT.constants.dn42.IPv4}
-      ${lib.concatMapStringsSep "\n" (p: "ip6 saddr ${p} return") LT.constants.dn42.IPv6}
-      reject with icmpx type admin-prohibited
-    }
-
-    chain DN42_OUTPUT {
-      ${lib.concatMapStringsSep "\n" (p: "ip daddr ${p} return") LT.constants.dn42.IPv4}
-      ${lib.concatMapStringsSep "\n" (p: "ip6 daddr ${p} return") LT.constants.dn42.IPv6}
-      reject with icmpx type admin-prohibited
-    }
-  }
-''
+  '';
+in
+pkgs.writeText "nft.conf" text
