af0bdbe1499289f261fad3d087a708f32a383111
Lan Tian <xuyh0120@outlook.com>
2023-01-13 00:44:10 -0800

    dns: reenable wildcard support

--- dns/core/eval.nix
+++ dns/core/eval.nix
@@ -23,15 +23,25 @@ let
     , providers
     , defaultTTL ? "1d"
     , nameserverTTL ? "1d"
+      # HENET disables wildcard for normal domains (not reverse ones) by default
+    , enableWildcard ? !(!reverse && builtins.elem "henet" providers)
     , records
     }:
     let
       providerCommands = lib.concatMapStringsSep ", " (n: "DnsProvider(DNS_${n})") providers;
       formattedDomain = "${if reverse then "REV(" else ""}${formatArg domain}${if reverse then ")" else ""}";
+
+      filteredRecords =
+        if enableWildcard
+        then lib.flatten records
+        else
+          builtins.filter
+            (v: !((lib.hasInfix ''("*'' v) || (lib.hasInfix ''('*'' v)))
+            (lib.flatten records);
     in
     [
       "D(${formattedDomain}, REG_${registrar}, ${providerCommands}, DefaultTTL(${formatArg defaultTTL}), NAMESERVER_TTL(${formatArg nameserverTTL}));"
-    ] ++ (builtins.map (record: "D_EXTEND(${formattedDomain}, ${record});") (lib.flatten records));
+    ] ++ (builtins.map (record: "D_EXTEND(${formattedDomain}, ${record});") filteredRecords);
   mapDomains = builtins.map mapDomainFunc (lib.flatten domains);
 
 in
