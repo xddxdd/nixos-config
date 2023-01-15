7b27693e12d211a3516ee95358a1a44317360306
Lan Tian <xuyh0120@outlook.com>
2023-01-15 01:15:52 -0800

    ssh: add known pubkey entries for all hosts

--- nixos/common-components/ssh-harden.nix
+++ nixos/common-components/ssh-harden.nix
@@ -1,22 +1,43 @@
 { pkgs, lib, LT, config, utils, inputs, ... }@args:
 
 {
-  programs.ssh.package = pkgs.openssh_hpn.overrideAttrs (old: {
-    doCheck = false;
-    patches = (old.patches or [ ]) ++ [
-      # OpenSSH requires all directories in path to chroot be writable only for
-      # root, to avoid a security issue described in:
-      #
-      # https://lists.mindrot.org/pipermail/openssh-unix-dev/2009-May/027651.html
-      #
-      # It requires a suid binary to exist on the same partition as the chroot
-      # target, which isn't the case on NixOS (all suid binaries are in
-      # /run/wrappers).
-      #
-      # Disable this behavior so I can configure SFTP properly.
-      ../../patches/openssh-disable-chroot-permission-check.patch
-    ];
-  });
+  programs.ssh = {
+    package = pkgs.openssh_hpn.overrideAttrs (old: {
+      doCheck = false;
+      patches = (old.patches or [ ]) ++ [
+        # OpenSSH requires all directories in path to chroot be writable only for
+        # root, to avoid a security issue described in:
+        #
+        # https://lists.mindrot.org/pipermail/openssh-unix-dev/2009-May/027651.html
+        #
+        # It requires a suid binary to exist on the same partition as the chroot
+        # target, which isn't the case on NixOS (all suid binaries are in
+        # /run/wrappers).
+        #
+        # Disable this behavior so I can configure SFTP properly.
+        ../../patches/openssh-disable-chroot-permission-check.patch
+      ];
+    });
+
+    knownHosts = builtins.listToAttrs (lib.flatten (lib.mapAttrsToList
+      (n: v:
+        (lib.optional ((LT.hosts."${n}".ssh.rsa or "") != "") {
+          name = "${n}-rsa";
+          value = {
+            hostNames = [ "${n}.lantian.pub" "${n}.xuyh0120.win" ];
+            publicKey = LT.hosts."${n}".ssh.rsa;
+          };
+        })
+        ++ (lib.optional ((LT.hosts."${n}".ssh.ed25519 or "") != "") {
+          name = "${n}-ed25519";
+          value = {
+            hostNames = [ "${n}.lantian.pub" "${n}.xuyh0120.win" ];
+            publicKey = LT.hosts."${n}".ssh.ed25519;
+          };
+        })
+      )
+      LT.hosts));
+  };
 
   services.openssh = {
     enable = true;
