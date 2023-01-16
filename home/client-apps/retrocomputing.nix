f997d15e2c61f5b52dc56371c9386d82b8107dc9
Lan Tian <xuyh0120@outlook.com>
2023-01-15 23:50:39 -0800

    home: add retrocomputing start script for winnt-mips

---
+++ home/client-apps/retrocomputing.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, LT, config, utils, inputs, ... }@args:
+
+let
+  winntMips = pkgs.stdenvNoCC.mkDerivation {
+    pname = "winnt-mips";
+    version = "1.0";
+
+    nativeBuildInputs = with pkgs; [ unzip ];
+
+    src = pkgs.fetchurl {
+      url = "https://web.archive.org/web/20150809205748if_/http://hpoussineau.free.fr/qemu/firmware/magnum-4000/setup.zip";
+      sha256 = "0plwj3pg47jigi4bbps5mfgb0phbksqya05azmbv2dcgyfsyxx1l";
+    };
+
+    unpackPhase = ''
+      unzip $src
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/opt
+      mv NTPROM.RAW $out/opt/
+
+      cat > $out/bin/winnt-mips <<EOF
+      #!/bin/sh
+      set -e
+
+      ROOTDIR="\$HOME/.local/share/winnt-mips"
+      mkdir -p "\$ROOTDIR"
+      exec ${pkgs.qemu}/bin/qemu-system-mips64el \
+        -hda "\$ROOTDIR/ntmips.img" \
+        -M magnum \
+        -nic user,model=dp83932 \
+        -m 128 \
+        -global "ds1225y.filename=\$ROOTDIR/nvram" \
+        -global "ds1225y.size=8200" \
+        -bios $out/opt/NTPROM.RAW \
+        "\$@"
+      EOF
+
+      chmod +x $out/bin/winnt-mips
+    '';
+  };
+in
+{
+  home.packages = [
+    winntMips
+  ];
+}
