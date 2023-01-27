74807197fadd94002e955971e97e509e0bc6c20f
Lan Tian <xuyh0120@outlook.com>
2023-01-27 03:12:59 -0800

    helpers: combine functions into a scope

--- helpers/default.nix
+++ helpers/default.nix
@@ -6,39 +6,54 @@
 }:
 
 let
-  args = rec {
-    inherit config pkgs lib inputs;
-
-    constants = import ./constants.nix;
-    hosts = builtins.mapAttrs hostDefaults (import ./hosts.nix { inherit roles geo; });
-    hostDefaults = import ./host-defaults.nix { inherit lib roles; };
-    this = hosts."${config.networking.hostName}" or (hostDefaults config.networking.hostName { });
-    otherHosts = builtins.removeAttrs hosts [ config.networking.hostName ];
-
-    geo = import ./geo.nix { inherit pkgs lib sanitizeName inputs; };
-    roles = import ./roles.nix;
-    serverHosts = lib.filterAttrs (n: v: v.role == roles.server) hosts;
-    nixosHosts = lib.filterAttrs (n: v: v.role != roles.non-nixos) hosts;
-
-    containerIP = import ./container-ip.nix;
-    dnssecKeys = import ./dnssec-keys.nix;
-    port = import ./port.nix;
-    portStr = lib.mapAttrsRecursive (k: builtins.toString) port;
-
-    sanitizeName = callHelper ./sanitize-name.nix;
-    serviceHarden = import ./service-harden.nix { inherit lib; };
-    sources = pkgs.callPackage _sources/generated.nix { };
-  };
-  callHelper = f: lib.callPackageWith
-    (pkgs // args)
-    f
-    { };
+  mkScope = f:
+    let
+      # Modified from lib.callPackageWith
+      call = file:
+        let
+          f = import file;
+          callFn = f:
+            let
+              fargs = lib.functionArgs f;
+              allArgs = builtins.intersectAttrs fargs (pkgs // scope);
+              missingArgs = lib.attrNames
+                (lib.filterAttrs (name: value: ! value)
+                  (removeAttrs fargs (lib.attrNames allArgs)));
+            in
+            if missingArgs == [ ] then f allArgs else null;
+        in
+        if lib.isFunction f then callFn f else f;
+      scope = f call;
+    in
+    scope;
 in
-args // rec {
-  container = callHelper ./container.nix;
-  gui = callHelper ./gui.nix;
-  netns = callHelper ./netns.nix;
-  nginx = callHelper ./nginx.nix;
-  uuid = callHelper ./uuid.nix;
-  wrapNetns = callHelper ./wrap-netns.nix;
-}
+mkScope (call: rec {
+  inherit config pkgs lib inputs;
+
+  constants = call ./constants.nix;
+  hosts = builtins.mapAttrs hostDefaults (call ./hosts.nix);
+  hostDefaults = call ./host-defaults.nix;
+  this = hosts."${config.networking.hostName}" or (hostDefaults config.networking.hostName { });
+  otherHosts = builtins.removeAttrs hosts [ config.networking.hostName ];
+
+  geo = call ./geo.nix;
+  roles = call ./roles.nix;
+  serverHosts = lib.filterAttrs (n: v: v.role == roles.server) hosts;
+  nixosHosts = lib.filterAttrs (n: v: v.role != roles.non-nixos) hosts;
+
+  containerIP = call ./container-ip.nix;
+  dnssecKeys = call ./dnssec-keys.nix;
+  port = call ./port.nix;
+  portStr = lib.mapAttrsRecursive (k: builtins.toString) port;
+
+  sanitizeName = call ./sanitize-name.nix;
+  serviceHarden = call ./service-harden.nix;
+  sources = call _sources/generated.nix;
+
+  container = call ./container.nix;
+  gui = call ./gui.nix;
+  netns = call ./netns.nix;
+  nginx = call ./nginx.nix;
+  uuid = call ./uuid.nix;
+  wrapNetns = call ./wrap-netns.nix;
+})
