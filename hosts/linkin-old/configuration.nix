4166ab23be35a32a7f2300564c781cf833f54267
Lan Tian <xuyh0120@outlook.com>
2023-01-29 16:10:12 -0800

    hosts: remove henet tunnelbroker on linkin-old

--- hosts/linkin-old/configuration.nix
+++ hosts/linkin-old/configuration.nix
@@ -14,41 +14,16 @@
     address = [ "103.172.81.11/28" ];
     gateway = [ "103.172.81.1" ];
     matchConfig.Name = "eth0";
-    networkConfig.Tunnel = "henet";
   };
 
   systemd.network.networks.dummy0.address = [
     "fdbc:f9dc:67ad::8b:c606:ba01/128"
   ];
 
-  systemd.network.netdevs.henet = {
-    netdevConfig = {
-      Kind = "sit";
-      Name = "henet";
-    };
-    tunnelConfig = {
-      Local = LT.this.public.IPv4;
-      Remote = "216.218.221.6";
-      TTL = 255;
-    };
-  };
-
-  systemd.network.networks.henet = {
-    address = [
-      "2001:470:18:10bd::2/64"
-      "2001:470:19:10bd::1/64"
-      "2001:470:fa1d::1/48"
-    ];
-    gateway = [ "2001:470:18:10bd::1" ];
-    matchConfig.Name = "henet";
-  };
-
   services."route-chain" = {
     enable = true;
     routes = [
       "172.22.76.97/29"
-      "2001:470:19:10bd::1/120"
-      "2001:470:fa1d::1/120"
     ];
   };
 
