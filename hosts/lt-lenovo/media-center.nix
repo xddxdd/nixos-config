4c16efb349fa6abb5ca97316fe2c4a7cf1d9ee15
Lan Tian <xuyh0120@outlook.com>
2023-02-11 00:55:54 -0800

    hosts: move jellyfin from lt-hp-z220-sff to lt-lenovo

---
+++ hosts/lt-lenovo/media-center.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, LT, config, utils, inputs, ... }@args:
+
+let
+  netns = LT.netns {
+    name = "wg-lantian";
+    setupDefaultRoute = false;
+  };
+
+  transmissionDownloadPath = "/mnt/storage/downloads";
+  transmissionSonarrDownloadPath = "/mnt/storage/.downloads-tr";
+  qBitTorrentSonarrDownloadPath = "/mnt/storage/.downloads-qb";
+  flexgetAutoDownloadPath = "/mnt/storage/.downloads-auto";
+  radarrMediaPath = "/mnt/storage/media-radarr";
+  sonarrMediaPath = "/mnt/storage/media-sonarr";
+in
+{
+  imports = [
+    ../../nixos/client-components/xorg.nix
+
+    ../../nixos/optional-apps/jellyfin.nix
+  ];
+
+  services.xserver.enable = lib.mkForce false;
+
+  systemd.services.jellyfin = {
+    after = [ "mnt-storage.mount" ];
+    requires = [ "mnt-storage.mount" ];
+  };
+}
