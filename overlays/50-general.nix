d64e3263cef5c2fedad7d55801fe35892ad56e9e
Lan Tian <xuyh0120@outlook.com>
2023-02-11 03:05:49 -0800

    general: use src attribute for googleearth-pro

--- overlays/50-general.nix
+++ overlays/50-general.nix
@@ -31,11 +31,11 @@ rec {
       cloudscraper
     ];
   });
-  googleearth-pro = prev.googleearth-pro.overrideAttrs (old: rec {
+  googleearth-pro = prev.googleearth-pro.overrideAttrs (old: {
     inherit (sources.googleearth-pro) version src;
     unpackPhase = ''
       # deb file contains a setuid binary, so 'dpkg -x' doesn't work here
-      dpkg --fsys-tarfile ${src} | tar --extract
+      dpkg --fsys-tarfile $src | tar --extract
     '';
     meta = builtins.removeAttrs old.meta [ "knownVulnerabilities" ];
   });
