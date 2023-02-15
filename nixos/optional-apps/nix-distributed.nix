e50a32f517b3dd00b4e16420bd6f588f3142b9f5
Lan Tian <xuyh0120@outlook.com>
2023-02-14 23:43:46 -0800

    nix: do not add localhost to buildMachine

--- nixos/optional-apps/nix-distributed.nix
+++ nixos/optional-apps/nix-distributed.nix
@@ -6,24 +6,20 @@ let
       isLocal = n == config.networking.hostName;
     in
     assert v.cpuThreads > 0;
-    {
+    if isLocal then null else {
       inherit (v) system;
       hostName = v.hostname;
       maxJobs = v.cpuThreads;
-      speedFactor = v.cpuThreads;
-      supportedFeatures = [ "benchmark" "big-parallel" ];
-    } // (if isLocal then {
-      hostName = "localhost";
-      protocol = null;
-    } else {
       protocol = "ssh-ng";
+      speedFactor = v.cpuThreads;
       sshKey = "/home/lantian/.ssh/id_ed25519";
       sshUser = "root";
-    });
+      supportedFeatures = [ "benchmark" "big-parallel" ];
+    };
 in
 {
   nix = {
     distributedBuilds = true;
-    buildMachines = lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder);
+    buildMachines = lib.filter (v: v != null) (lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder));
   };
 }
