6c16c4c037e748b9c6e66a9576e7059e06b47c86
Lan Tian <xuyh0120@outlook.com>
2023-02-17 20:37:41 -0800

    asterisk: ban clients repeatedly trying to login as anonymous

--- nixos/common-apps/fail2ban/default.nix
+++ nixos/common-apps/fail2ban/default.nix
@@ -1,13 +1,17 @@
 { pkgs, lib, LT, config, utils, inputs, ... }@args:
 
 {
+  environment.etc = {
+    "fail2ban/action.d/nftables-lantian.conf".source = lib.mkForce ./action.d/nftables-lantian.conf;
+    "fail2ban/filter.d/asterisk-lantian.conf".source = lib.mkForce ./filter.d/asterisk-lantian.conf;
+  };
   environment.systemPackages = [ config.services.fail2ban.package ];
 
   services.fail2ban = {
     enable = true;
     maxretry = 5;
-    banaction = "nftables-multiport";
-    banaction-allports = "nftables-allports";
+    banaction = "nftables-lantian[type=allports]";
+    banaction-allports = "nftables-lantian[type=allports]";
     packageFirewall = pkgs.nftables-fullcone;
     ignoreIP = with LT.constants; (reserved.IPv4 ++ reserved.IPv6);
 
