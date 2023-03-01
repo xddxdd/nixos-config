13d80a420ed2a840b753e845db3fe4e7950e4ceb
Lan Tian <xuyh0120@outlook.com>
2023-02-28 19:47:41 -0800

    hardware: add ondemand ignore-nice-load support

---
+++ nixos/hardware/ignore-nice-load.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: {
+  boot.kernelParams = [
+    "cpufreq.default_governor=ondemand"
+    "intel_pstate=passive"
+  ];
+
+  powerManagement.cpuFreqGovernor = "ondemand";
+
+  systemd.services.cpufreq.postStart = ''
+    echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load || true
+  '';
+}
