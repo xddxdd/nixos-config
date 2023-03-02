8344bfa7fe44301a7052c33465f4a9fe28799a73
Lan Tian <xuyh0120@outlook.com>
2023-03-02 01:36:31 -0800

    gui(i915): let vfio-pci take over VFs

--- nixos/common-components/kernel/default.nix
+++ nixos/common-components/kernel/default.nix
@@ -193,7 +193,17 @@ in
       script = ''
         I915_PATH=/sys/devices/pci0000:00/0000:00:02.0
         NUMVFS=$(cat "$I915_PATH/sriov_totalvfs")
+        echo 0 > "$I915_PATH/sriov_drivers_autoprobe"
         echo "$NUMVFS" > "$I915_PATH/sriov_numvfs"
+
+        ${pkgs.kmod}/bin/modprobe -v vfio-pci
+
+        for VF in $I915_PATH/virtfn*; do
+          PCI_ADDR=$(readlink -f $VF)
+          PCI_ADDR=''${PCI_ADDR##*/}
+          echo vfio-pci > "$VF/driver_override"
+          echo "$PCI_ADDR" > /sys/bus/pci/drivers_probe
+        done
       '';
     };
 
