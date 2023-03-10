4b1c68b60c07ca1fe0ac88c0f2e3d4c680279bd8
Lan Tian <xuyh0120@outlook.com>
2023-03-09 20:12:23 -0800

    hosts(vultr-sea): add

---
+++ hosts/vultr-sea/configuration.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: {
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  systemd.network.networks.eth0 = {
+    address = ["144.202.83.81/24"];
+    gateway = ["144.202.83.1"];
+    matchConfig.Name = "eth0";
+  };
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+}
