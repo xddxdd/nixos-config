30efb18d4dbb87111d17441a611dfb27b720671c
Lan Tian <xuyh0120@outlook.com>
2023-03-11 02:29:21 -0800

    nginx: override some mime types with nginx builtin list

--- nixos/common-apps/nginx/nginx.nix
+++ nixos/common-apps/nginx/nginx.nix
@@ -88,6 +88,8 @@ in {
     sslCiphers = null;
 
     commonHttpConfig = ''
+      include ${config.services.nginx.package}/conf/mime.types;
+
       log_format main '$remote_addr $host $remote_user [$time_local] "$request" '
                       '$status $body_bytes_sent "$http_referer" "$http_user_agent"';
       access_log syslog:server=unix:/dev/log,nohostname main;
