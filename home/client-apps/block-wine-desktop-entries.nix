b89afa2ecaa53e39adeb8d61119fd42c447eecf8
Lan Tian <xuyh0120@outlook.com>
2023-03-15 01:37:23 -0700

    home: block access to wine desktop entries

---
+++ home/client-apps/block-wine-desktop-entries.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: let
+  filenames = [
+    "wine-extension-chm.desktop"
+    "wine-extension-hlp.desktop"
+    "wine-extension-htm.desktop"
+    "wine-extension-html.desktop"
+    "wine-extension-ini.desktop"
+    "wine-extension-msp.desktop"
+    "wine-extension-pdf.desktop"
+    "wine-extension-rtf.desktop"
+    "wine-extension-txt.desktop"
+    "wine-extension-vbs.desktop"
+    "wine-extension-wri.desktop"
+    "wine-extension-xml.desktop"
+  ];
+in {
+  home.activation.block-wine-desktop-entries = builtins.concatStringsSep "\n" (builtins.map (f: ''
+      mkdir -p "$HOME/.local/share/applications"
+      rm -f "$HOME/.local/share/applications/${f}"
+      touch "$HOME/.local/share/applications/${f}"
+      chmod 000 "$HOME/.local/share/applications/${f}"
+    '')
+    filenames);
+}
