173c69c2f606a4038516279463deca5095eeb509
Lan Tian <xuyh0120@outlook.com>
2023-03-18 22:25:58 -0700

    hosts(oneprovider): disable transmission

--- hosts/oneprovider/configuration.nix
+++ hosts/oneprovider/configuration.nix
@@ -32,7 +32,6 @@
     ../../nixos/optional-apps/resilio.nix
     ../../nixos/optional-apps/sftp-server.nix
     ../../nixos/optional-apps/tg-bot-cleaner-bot
-    ../../nixos/optional-apps/transmission-daemon.nix
     ../../nixos/optional-apps/vaultwarden.nix
     ../../nixos/optional-apps/vlmcsd.nix
     ../../nixos/optional-apps/waline.nix
