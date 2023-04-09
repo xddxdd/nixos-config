cb787bcb2c417589a4b02580f00aed875686b986
Lan Tian <xuyh0120@outlook.com>
2023-04-09 16:27:30 -0700

    ssh: rename settings following nixpkgs change

--- nixos/common-components/ssh-harden.nix
+++ nixos/common-components/ssh-harden.nix
@@ -62,22 +62,22 @@
     settings = {
       LogLevel = "ERROR";
       PermitRootLogin = lib.mkForce "prohibit-password";
+      Ciphers = [
+        "chacha20-poly1305@openssh.com"
+        "aes256-gcm@openssh.com"
+        "aes128-gcm@openssh.com"
+      ];
+      KexAlgorithms = [
+        "sntrup761x25519-sha512@openssh.com"
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+      ];
+      Macs = [
+        "hmac-sha2-512"
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256"
+        "hmac-sha2-256-etm@openssh.com"
+      ];
     };
-    ciphers = [
-      "chacha20-poly1305@openssh.com"
-      "aes256-gcm@openssh.com"
-      "aes128-gcm@openssh.com"
-    ];
-    kexAlgorithms = [
-      "sntrup761x25519-sha512@openssh.com"
-      "curve25519-sha256"
-      "curve25519-sha256@libssh.org"
-    ];
-    macs = [
-      "hmac-sha2-512"
-      "hmac-sha2-512-etm@openssh.com"
-      "hmac-sha2-256"
-      "hmac-sha2-256-etm@openssh.com"
-    ];
   };
 }
