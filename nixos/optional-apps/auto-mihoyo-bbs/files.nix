c647abbd756fb3469061989f352e46a0b39bbfa6
Lan Tian <xuyh0120@outlook.com>
2023-04-29 00:12:58 -0700

    auto-mihoyo-bbs: bump version

--- nixos/optional-apps/auto-mihoyo-bbs/files.nix
+++ nixos/optional-apps/auto-mihoyo-bbs/files.nix
@@ -8,8 +8,8 @@ stdenvNoCC.mkDerivation {
   pname = "auto-mihoyo-bbs";
   version = "1.0.0";
   src = fetchurl {
-    url = "https://backblaze.lantian.pub/auto-mihoyo-bbs.tar.zst";
-    sha256 = "0h06iyrnlrpbym2k4xdn4hbmywg64h00r7nddr7dgxv9xy7csgss";
+    url = "https://private.xuyh0120.win/auto-mihoyo-bbs.tar.zst";
+    sha256 = "0hgwf1fici8jnqs94f9q7alhszadqqm3ns7s1kqxd0g7fm4f27mf";
   };
 
   nativeBuildInputs = [zstd];
