cf7ec323628abf30105fb65324844d1e3e47567e
Lan Tian <xuyh0120@outlook.com>
2023-04-30 12:12:15 -0700

    hosts(hetzner-de): use as builder

--- hosts/hetzner-de/host.nix
+++ hosts/hetzner-de/host.nix
@@ -4,9 +4,9 @@
   ...
 } @ args: {
   index = 14;
-  tags = with tags; [public-facing server];
+  tags = with tags; [nix-builder public-facing server];
   system = "aarch64-linux";
-  cpuThreads = 8;
+  cpuThreads = 4;
   city = geo.cities."DE Falkenstein";
   ssh = {
     rsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4jwDVkKk17MC0ERMrNei8QQoeS32Uf1rRfemnFQeKyKovJqcIqtsqtbDH2D3pp+2HQmRcSKZ2tGeTCQYcZnOMQRUDeBQHnZbnoMCMH/Zbt2WUUayfwv/lvle6nzm94ggNx6Ups1RNJIs9sBp6l/a2pG8M78GjMy4qvnHrbqd2Yj0+aqQv9Lhf2wJzepp3Eh4aR8YMBE46bC5o/BACxQTpMBBY7Yz7UDO3FuMIOMdhM/zsSzIwyfZtIAKYRbhhnKPce2MMXTOByFd2TJKcwcreyVX75j0i2A0KmT/U/+JTsWtXSBQ1y5ywRXRuysri+HX8W5Abv3EtZaJwfHzDk7f2jJv5LT+qWe8iJVRWqMWV4UvoUgEWNOKlxKKs9m4A5AOP6dRexCHSnLl0C0NcSjwcCsr94PfT0rsjHJ7k7vF7Jf/cHtBByzrDaJtkXru9+WDOC8NsP7kBaTMAVx0w86XNomRuIRoNQzozP/Y5dlGtRxAHS+uSWgCPAix3Q3Y/r4skpaU4zjWs9hN4+qwmM3vJcV+b7zxqB6e0Bn1VQJScgKpv0+87rZ3P7xITbiHN8aqmAvY45WNJn/32nI+4N/LMPQmv05dDMt2bWIOtKcdEpF85slOUo8XdasA1GLeCgNkFMItrdGOsaDfGaSjLhe8nmLH/lqo92Tbdf86lTpaIMw==";
