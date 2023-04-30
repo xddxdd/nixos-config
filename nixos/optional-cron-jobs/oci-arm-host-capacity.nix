3d0bc070f3a4db7b5dd30f51cdb1429811bb5b74
Lan Tian <xuyh0120@outlook.com>
2023-04-29 22:36:45 -0700

    oci-arm-host-capacity: setup on lt-hp-z220-sff

---
+++ nixos/optional-cron-jobs/oci-arm-host-capacity.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: {
+  systemd.services.oci-arm-host-capacity = {
+    serviceConfig =
+      LT.serviceHarden
+      // {
+        Type = "oneshot";
+        ExecStart = "${pkgs.php}/bin/php ${pkgs.oci-arm-host-capacity}/index.php env";
+        StateDirectory = "oci-arm-host-capacity";
+        WorkingDirectory = "/var/lib/oci-arm-host-capacity";
+      };
+  };
+
+  systemd.timers.oci-arm-host-capacity = {
+    wantedBy = ["timers.target"];
+    partOf = ["oci-arm-host-capacity.service"];
+    timerConfig = {
+      OnCalendar = "*:0/2";
+      Unit = "oci-arm-host-capacity.service";
+    };
+  };
+}
