a6ec77fc4153ea2f61cab920e13e98d861bbef9f
Lan Tian <xuyh0120@outlook.com>
2023-05-02 00:28:22 -0700

    asterisk: retry registration indefinitely

--- nixos/optional-apps/asterisk/default.nix
+++ nixos/optional-apps/asterisk/default.nix
@@ -38,6 +38,11 @@ in {
 
     confFiles = {
       "pjsip.conf" = ''
+        ; Allow unlimited retries for registration
+        [registration]
+        auth_rejection_permanent=no
+        max_retries=0
+
         ; Transports
         ${transports}
 
