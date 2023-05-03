1a47ebda298fd753dee2c6e31805718f468c90a4
Lan Tian <xuyh0120@outlook.com>
2023-05-02 23:25:55 -0700

    networking: update firmware for mt7916

--- overlays/02-mt7916-ap-fix.nix
+++ overlays/02-mt7916-ap-fix.nix
@@ -9,17 +9,11 @@ in rec {
         CONFIG_IEEE80211AX=y
       '';
   });
-  linux-firmware = prev.linux-firmware.overrideAttrs (old: let
-    mt7916TestFirmware = final.fetchzip {
-      url = "https://github.com/openwrt/mt76/files/10567955/mt7916_test_fw.zip";
-      stripRoot = false;
-      hash = "sha256-KgMogC2RAbOS9SIU2wvx5CvQ5+KguxnHx/SH2fm+y7w=";
-    };
-  in {
+  linux-firmware = prev.linux-firmware.overrideAttrs (old: {
     preInstall =
       (old.preInstall or "")
       + ''
-        cp -r ${mt7916TestFirmware}/* mediatek/
+        cp -r ${sources.openwrt-mt76.src}/firmware/* mediatek/
       '';
     outputHash = null;
     outputHashAlgo = null;
