6e7608083220a6e9cd340b48ba3de2f22e0ca8c1
Lan Tian <xuyh0120@outlook.com>
2023-05-10 21:10:22 -0700

    resilio: set advanced preferences and stop using nixpkgs module

--- nixos/optional-apps/resilio.nix
+++ nixos/optional-apps/resilio.nix
@@ -6,7 +6,39 @@
   utils,
   inputs,
   ...
-} @ args: {
+} @ args: let
+  rslConfig = {
+    device_name = config.networking.hostName;
+    storage_path = "/var/lib/resilio-sync/";
+    listening_port = 0;
+    use_gui = false;
+    check_for_updates = false;
+    use_upnp = true;
+    download_limit = 0;
+    upload_limit = 0;
+    lan_encrypt_data = true;
+    directory_root = "/run/rslfiles";
+
+    # Power user preferences: https://help.resilio.com/hc/en-us/articles/207371636-Power-user-preferences
+    log_size = 4;
+    log_ttl = 1;
+    disk_low_priority = true;
+    disk_worker_per_job = true;
+    show_service_disk_thread = true;
+    parallel_indexing = true;
+
+    # Inotify doesn't work with bindfs
+    enable_file_system_notifications = false;
+    folder_rescan_interval = 60;
+
+    # Do not change! Hardcoded value for ip2unix and nginx reverse proxy
+    webui = {
+      listen = "[::1]:9000";
+      login = "user";
+      password = "pass";
+    };
+  };
+in {
   options.lantian.resilio.storage = lib.mkOption {
     type = lib.types.str;
     default = "/nix/persistent/media";
@@ -29,26 +61,40 @@
       ];
     };
 
-    services.resilio = {
-      enable = true;
-      enableWebUI = true;
-      checkForUpdates = false;
-      deviceName = config.networking.hostName;
-      directoryRoot = lib.mkForce "/run/rslfiles";
-      # Authentication is done by nginx
-      httpLogin = "user";
-      httpPass = "pass";
+    users.users.rslsync = {
+      description = "Resilio Sync Service user";
+      home = rslConfig.storage_path;
+      createHome = true;
+      uid = config.ids.uids.rslsync;
+      group = "rslsync";
     };
 
+    users.groups.rslsync = {};
+
     systemd.services.resilio = {
-      after = ["run-rslfiles.mount"];
-      requires = ["run-rslfiles.mount"];
-      environment = {
-        LD_PRELOAD = "${pkgs.mimalloc}/lib/libmimalloc.so";
-      };
+      description = "Resilio Sync Service";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target" "run-rslfiles.mount"];
+      requires = ["network.target" "run-rslfiles.mount"];
+
+      script = let
+        cfgFile = pkgs.writeText "config.json" (builtins.toJSON rslConfig);
+      in ''
+        exec ${pkgs.ip2unix}/bin/ip2unix -r in,tcp,port=9000,path=/run/rslsync/rslsync.sock \
+          ${pkgs.resilio-sync}/bin/rslsync --nodaemon --config ${cfgFile}
+      '';
+
       serviceConfig =
         LT.serviceHarden
         // {
+          Restart = "on-abort";
+          UMask = "0002";
+          RuntimeDirectory = "rslsync";
+          ExecStartPost = pkgs.writeShellScript "rslsync-post" ''
+            while [ ! -S /run/rslsync/rslsync.sock ]; do sleep 1; done
+            chmod 777 /run/rslsync/rslsync.sock
+          '';
+
           User = "rslsync";
           Group = "rslsync";
           ReadWritePaths = ["/run/rslfiles"];
