971198a6555c6b12c6ba782eb4f8227da869f1a4
Lan Tian <xuyh0120@outlook.com>
2023-05-12 00:53:39 -0700

    Revert "networking: bird: disable add paths extension and fix internal route filtering"
    This reverts commit 5f8848c154ff59c8abf9b3890cf96dbec0a20825.

--- nixos/common-apps/bird/ltnet.nix
+++ nixos/common-apps/bird/ltnet.nix
@@ -99,11 +99,8 @@ in {
     }
 
     function ltnet_export_filter_v4() {
-      # Static protocol routes are all defined as unreachable
-      if source != RTS_STATIC then {
-        if dest ~ [RTD_BLACKHOLE, RTD_UNREACHABLE, RTD_PROHIBIT] then reject;
-        if ifindex = 0 then reject;
-      }
+      if dest ~ [RTD_BLACKHOLE, RTD_UNREACHABLE, RTD_PROHIBIT] then reject;
+      if ifindex = 0 then reject;
       if net ~ RESERVED_IPv4 then accept;
       reject;
     }
@@ -114,11 +111,8 @@ in {
     }
 
     function ltnet_export_filter_v6() {
-      # Static protocol routes are all defined as unreachable
-      if source != RTS_STATIC then {
-        if dest ~ [RTD_BLACKHOLE, RTD_UNREACHABLE, RTD_PROHIBIT] then reject;
-        if ifindex = 0 then reject;
-      }
+      if dest ~ [RTD_BLACKHOLE, RTD_UNREACHABLE, RTD_PROHIBIT] then reject;
+      if ifindex = 0 then reject;
       if net ~ RESERVED_IPv6 then accept;
       reject;
     }
@@ -137,8 +131,7 @@ in {
         next hop self yes;
         import keep filtered;
         extended next hop yes;
-        # DO NOT ENABLE. Other nodes may prefer a secondary route from us.
-        # add paths yes;
+        add paths yes;
         import filter { ltnet_import_filter_v4(); };
         export filter { ltnet_export_filter_v4(); };
       };
@@ -146,8 +139,7 @@ in {
         next hop self yes;
         import keep filtered;
         extended next hop yes;
-        # DO NOT ENABLE. Other nodes may prefer a secondary route from us.
-        # add paths yes;
+        add paths yes;
         import filter { ltnet_import_filter_v6(); };
         export filter { ltnet_export_filter_v6(); };
       };
