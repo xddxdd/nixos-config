142809c8fb9f90706328d2601a6555c021b016b6
Lan Tian <xuyh0120@outlook.com>
2023-05-14 01:56:07 -0700

    hosts(lt-lenovo-vm): add

---
+++ hosts/lt-lenovo-vm/configuration.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: {
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  systemd.network.networks.eth0 = {
+    address = ["192.168.0.10/24"];
+    gateway = ["192.168.0.2"];
+    matchConfig.Name = "eth0";
+  };
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+}
