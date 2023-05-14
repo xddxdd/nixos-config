fb5fe04342aafc04eefd73a495ea50dd0cdf4af9
Lan Tian <xuyh0120@outlook.com>
2023-05-13 21:24:43 -0700

    Revert "general: try nixbuild.net again"
    This reverts commit 25721df0f22c9a21d054e937265fddc4e7d3aabf.

--- nixos/optional-apps/nix-distributed.nix
+++ nixos/optional-apps/nix-distributed.nix
@@ -23,20 +23,9 @@
         sshUser = "root";
         supportedFeatures = ["benchmark" "big-parallel"];
       };
-
-  nixBuildNet = {
-    hostName = "eu.nixbuild.net";
-    systems = ["x86_64-linux" "aarch64-linux"];
-    sshKey = "/home/lantian/.ssh/id_ed25519";
-    sshUser = "root";
-    maxJobs = 100;
-    speedFactor = 100;
-    supportedFeatures = ["benchmark" "big-parallel"];
-  };
 in {
   nix = {
     distributedBuilds = true;
-    buildMachines = [nixBuildNet];
-    # buildMachines = lib.filter (v: v != null) (lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder));
+    buildMachines = lib.filter (v: v != null) (lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder));
   };
 }
