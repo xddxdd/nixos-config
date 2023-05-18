2d6848cbd88974e42f5a54a3cb864bd5dc6989fc
Lan Tian <xuyh0120@outlook.com>
2023-05-17 19:45:48 -0700

    hosts(lt-hp-z220-sff): add SmartRent auto lock script

--- hosts/lt-hp-z220-sff/configuration.nix
+++ hosts/lt-hp-z220-sff/configuration.nix
@@ -29,6 +29,7 @@
     ../../nixos/optional-apps/nvidia/cuda-only.nix
     ../../nixos/optional-apps/resilio.nix
     ../../nixos/optional-apps/sftp-server.nix
+    ../../nixos/optional-apps/smartrent-auto-lock
     ../../nixos/optional-apps/vlmcsd.nix
 
     ../../nixos/optional-cron-jobs/oci-arm-host-capacity.nix
