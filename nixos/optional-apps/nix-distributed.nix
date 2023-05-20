3ec57c99b99b813138e05cf7841dea73ee929434
Lan Tian <xuyh0120@outlook.com>
2023-05-19 18:59:08 -0700

    Revert "general: use nixbuild.net only for arm builds"
    This reverts commit 14b2e6444a56c0d4ddfe27d5dc6abd11c54ffeed.

--- nixos/optional-apps/nix-distributed.nix
+++ nixos/optional-apps/nix-distributed.nix
@@ -23,22 +23,9 @@
         sshUser = "root";
         supportedFeatures = ["benchmark" "big-parallel"];
       };
-
-  nixBuildNet = {
-    hostName = "eu.nixbuild.net";
-    systems = ["aarch64-linux"];
-    sshKey = "/home/lantian/.ssh/id_ed25519";
-    sshUser = "root";
-    maxJobs = 100;
-    speedFactor = 100;
-    supportedFeatures = ["benchmark" "big-parallel"];
-  };
 in {
   nix = {
     distributedBuilds = true;
-    buildMachines =
-      [nixBuildNet]
-      ++ (lib.filter (v: v != null)
-        (lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder)));
+    buildMachines = lib.filter (v: v != null) (lib.mapAttrsToList mkBuildMachine (LT.hostsWithTag LT.tags.nix-builder));
   };
 }
