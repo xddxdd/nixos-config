a940d8cc7dfbefeb2438dde23d11b8894a122c35
Lan Tian <xuyh0120@outlook.com>
2023-05-23 01:26:41 -0700

    general: no longer print verbose information for colmena

--- scripts/colmena.nix
+++ scripts/colmena.nix
@@ -5,7 +5,7 @@
 }: ''
   ACTION=$1; shift;
   if [ "$ACTION" = "apply" ] || [ "$ACTION" = "build" ]; then
-    colmena $ACTION --keep-result --show-trace --verbose $*
+    colmena $ACTION --keep-result --show-trace --evaluator streaming $*
     exit $?
   else
     colmena $ACTION $*
