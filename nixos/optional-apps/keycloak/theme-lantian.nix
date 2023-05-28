5ffea8fca594affee4b004424f2e540f54093956
Lan Tian <xuyh0120@outlook.com>
2023-05-27 21:29:38 -0700

    keycloak: update custom theme to latest version

--- nixos/optional-apps/keycloak/theme-lantian.nix
+++ nixos/optional-apps/keycloak/theme-lantian.nix
@@ -5,19 +5,11 @@
   sources,
   nodejs,
   ...
-} @ args: let
-  package = callPackage (sources.keycloak-lantian.src + "/default.nix") {};
-  nodeModules = "${package.package}/lib/node_modules/keycloak-lantian/node_modules";
-in
-  stdenvNoCC.mkDerivation {
-    inherit (sources.keycloak-lantian) pname version src;
-    buildInputs = [nodejs];
-    buildPhase = ''
-      ln -s ${nodeModules} ./node_modules
-      node ${nodeModules}/webpack-cli/bin/cli.js
-    '';
-    installPhase = ''
-      mkdir -p $out
-      cp -r login $out/
-    '';
-  }
+} @ args:
+stdenvNoCC.mkDerivation {
+  inherit (sources.keycloak-lantian) pname version src;
+  installPhase = ''
+    mkdir -p $out
+    cp -r login $out/
+  '';
+}
