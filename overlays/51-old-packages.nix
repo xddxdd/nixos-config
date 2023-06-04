d4dbc0e909b5142dd6613b540fbe80d410667425
Lan Tian <xuyh0120@outlook.com>
2023-06-03 21:41:56 -0700

    plausible: use latest erlang runtime

--- overlays/51-old-packages.nix
+++ overlays/51-old-packages.nix
@@ -2,9 +2,6 @@
   pkgs-22-05 = inputs.nixpkgs-22-05.legacyPackages."${final.system}";
   pkgs-22-11 = inputs.nixpkgs-22-11.legacyPackages."${final.system}";
 in rec {
-  # plausible: crashes on oneprovider with latest erlang runtime
-  plausible = prev.plausible.override {beamPackages = pkgs-22-05.beamPackages;};
-
   inherit (pkgs-22-05) linuxPackages_6_0 mysql57;
   inherit (pkgs-22-11) clickhouse;
 }
