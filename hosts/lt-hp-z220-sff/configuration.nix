b67c0cd458d0ec64e09a5c2e130b6522b4a86b84
Lan Tian <xuyh0120@outlook.com>
2023-06-06 01:34:12 -0700

    Revert "oci-arm-host-capacity: disable"
    This reverts commit 1716e74234169fb2ec9832dcf1b4930585403429.

--- hosts/lt-hp-z220-sff/configuration.nix
+++ hosts/lt-hp-z220-sff/configuration.nix
@@ -33,6 +33,8 @@
     ../../nixos/optional-apps/smartrent-auto-lock
     ../../nixos/optional-apps/vlmcsd.nix
 
+    ../../nixos/optional-cron-jobs/oci-arm-host-capacity.nix
+
     "${inputs.secrets}/nixos-hidden-module/8eca84a1c0f3007b"
   ];
 
