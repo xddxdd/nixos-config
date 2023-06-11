a0c26ecdf2c7577b233192b5dc2174539c799a44
Lan Tian <xuyh0120@outlook.com>
2023-06-10 20:03:00 -0700

    auto-mihoyo-bbs: change captcha solver platform

--- nixos/optional-apps/auto-mihoyo-bbs/captcha.py
+++ nixos/optional-apps/auto-mihoyo-bbs/captcha.py
@@ -1,15 +1,16 @@
 import os
+import time
 from request import http
 from loghelper import log
 
-# 请自行到人人打码官网注册并充值后获取appkey填写到下面
 appkey = ""
 if os.getenv("AutoMihoyoBBS_appkey") is not None:
     appkey = os.getenv("AutoMihoyoBBS_appkey")
 
 
 def game_captcha(gt: str, challenge: str):
-    validate = geetest(gt, challenge, 'https://passport-api.mihoyo.com/account/ma-cn-passport/app/loginByPassword')
+    validate = geetest(
+        gt, challenge, 'https://passport-api.mihoyo.com/account/ma-cn-passport/app/loginByPassword')
     return validate  # 失败返回None 成功返回validate
 
 
@@ -21,32 +22,45 @@ def bbs_captcha(gt: str, challenge: str):
 
 
 def geetest(gt: str, challenge: str, referer: str):
-    if not query_score():
+    try:
+        log.info("提交打码任务")
+        response = http.post('http://2captcha.com/in.php', params={
+            'method': 'geetest',
+            'key': appkey,
+            'gt': gt,
+            'challenge': challenge,
+            'pageurl': referer,
+            'json': 1,
+        }, timeout=60)
+        data = response.json()
+        if data.get('status', 0) == 0:
+            log.error(f'打码出错 {response.text}')
+            return None
+
+        req_id = data.get('request')
+        log.info(f"打码任务编号 {req_id}，等待")
+
+        time.sleep(10)
+        while True:
+            time.sleep(5)
+            response = http.post('http://2captcha.com/res.php', params={
+                'key': appkey,
+                'action': 'get',
+                'id': req_id,
+                'json': 1,
+            }, timeout=60)
+            data = response.json()
+
+            if data.get('status', 0) == 0:
+                if data.get('request') == 'CAPCHA_NOT_READY':
+                    log.info("打码尚未完成，等待")
+                    continue
+                log.error(f'打码出错 {response.text}')
+                return None
+
+            validate = data['request']['geetest_validate']
+            log.info("打码成功")
+            return validate
+    except Exception as e:
+        log.error(f'打码出错 {repr(e)}')
         return None
-
-    response = http.post('http://api.rrocr.com/api/recognize.html', params={
-        'appkey': appkey,
-        'gt': gt,
-        'challenge': challenge,
-        'referer': referer,
-        'sharecode': '585dee4d4ef94e1cb95d5362a158ea54'
-    }, timeout=60)
-    data = response.json()
-    if data['status'] != 0:
-        log.error(data['msg'])  # 打码失败输出错误信息
-        return None
-    return data['data']['validate']  # 失败返回None 成功返回validate
-
-
-def query_score():
-    response = http.get("http://api.rrocr.com/api/integral.html?appkey=" + appkey)
-    data = response.json()
-    if data['status'] == -1:
-        log.error('查询积分失败')
-        return False
-
-    if int(data['integral']) < 10:
-        log.warning('积分不足')
-        return False
-    log.info('积分还剩' + data['integral'])
-    return True
