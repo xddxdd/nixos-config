42a3f45bd5f1f5e09452b407133036a38e5e148a
Lan Tian <xuyh0120@outlook.com>
2023-06-16 20:01:53 -0700

    hosts(lt-epyc-vm): reenable gpu

--- hosts/lt-epyc-vm/configuration.nix
+++ hosts/lt-epyc-vm/configuration.nix
@@ -21,6 +21,7 @@
     ../../nixos/optional-apps/grasscutter.nix
     ../../nixos/optional-apps/glauth.nix
     ../../nixos/optional-apps/netns-wg-lantian.nix
+    ../../nixos/optional-apps/nvidia/cuda-only.nix
     ../../nixos/optional-apps/resilio.nix
     ../../nixos/optional-apps/sftp-server.nix
     ../../nixos/optional-apps/vlmcsd.nix
