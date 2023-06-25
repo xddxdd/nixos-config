84eb4f4c7f430e321049c7fef15d84e1e8d7249e
Lan Tian <xuyh0120@outlook.com>
2023-06-24 19:21:21 -0700

    rebuild-nixos-config: setup on lt-home-builder

---
+++ nixos/optional-cron-jobs/rebuild-nixos-config.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: {
+  age.secrets.attic-upload-key.file = inputs.secrets + "/attic-upload-key.age";
+
+  systemd.services.rebuild-nixos-config = {
+    serviceConfig =
+      LT.serviceHarden
+      // {
+        Type = "oneshot";
+        RuntimeDirectory = "rebuild-nixos-config";
+        WorkingDirectory = "/run/rebuild-nixos-config";
+      };
+    unitConfig.OnFailure = "notify-email-fail@%n.service";
+    environment = {
+      HOME = "/run/rebuild-nixos-config";
+    };
+    path = with pkgs; [
+      attic-client
+      bash
+      colmena
+      git
+      gnumake
+      nix
+    ];
+
+    script = ''
+      attic login --set-default lantian https://attic.xuyh0120.win "$(cat ${config.age.secrets.attic-upload-key.path})"
+      git clone --depth 1 https://github.com/xddxdd/nixos-config.git nixos-config
+      cd nixos-config
+      make build
+      ls -alh .gcroots
+      attic push lantian $(readlink -f .gcroots/*)
+    '';
+  };
+
+  systemd.timers.rebuild-nixos-config = {
+    wantedBy = ["timers.target"];
+    partOf = ["rebuild-nixos-config.service"];
+    timerConfig = {
+      OnCalendar = "daily";
+      RandomizedDelaySec = "1h";
+      Unit = "rebuild-nixos-config.service";
+    };
+  };
+}
