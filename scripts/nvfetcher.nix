b554a1514a6eb7c5c91f7c5f2cf081ae786d52ea
Lan Tian <xuyh0120@outlook.com>
2023-07-01 12:13:16 -0700

    general: use nvfetcher from nixpkgs

--- scripts/nvfetcher.nix
+++ scripts/nvfetcher.nix
@@ -1,4 +1,4 @@
 {nvfetcher, ...}: ''
   [ -f "$HOME/Secrets/nvfetcher.toml" ] && KEY_FLAG="-k $HOME/Secrets/nvfetcher.toml" || KEY_FLAG=""
-  nvfetcher $KEY_FLAG -c nvfetcher.toml -o helpers/_sources "$@"
+  ${nvfetcher}/bin/nvfetcher $KEY_FLAG -c nvfetcher.toml -o helpers/_sources "$@"
 ''
