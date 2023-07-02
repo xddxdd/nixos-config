0c8d55a2259ccdd4232c00fcba310bf0a67f12bb
Lan Tian <xuyh0120@outlook.com>
2023-07-01 17:50:38 -0700

    hosts: update location for personal devices

--- hosts/lt-hp-omen/host.nix
+++ hosts/lt-hp-omen/host.nix
@@ -7,7 +7,7 @@
   # Temporarily remove i915-sriov for build failure
   tags = with tags; [client nix-builder];
   cpuThreads = 16;
-  city = geo.cities."US Chicago";
+  city = geo.cities."US Seattle";
   hostname = "127.0.0.1";
   ssh = {
     rsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDE4CLCfkSGU7NMz4n5yp8AEdds7br0Ehe0e/RFCBz7wmqC6JeFgwapZefDwdI5av5UnBP1dqmi602OBF9yTCQ0yL1FLYTgkfWjGZSpRWOW69rQGsSGHMF/BExNQnVzmIrlMHlWtyQvDsI6tycMAiPBh2e2jcCzjGQSsHIE2d8TNN3XaA1Mht9dC7pAQqT3QUeYbUud2xWh0jCLLX+fLe9F41O9//TLLX/Lergf8Nxlu9BBM8l3t7JQBFq0QmzFSL+ODnlMCq/yEkXTOTNUNo9hzuLIr3qTX3jmYb4iF+WmeAfTcA1i0JWeMo/TilWhKsoBkYanIBCz9tctpqB2xFOWlC8UKXlgRwDRftDXTIrHMu2RvshB6bKxfV54yYAkMp7p/Alfq+4aYDjE10e0IRxB3AGsyVDH7eGWxbdDJSblu2zNaZfFmIARVU7Kc1bJHe05NmAOSwIparSh/ge4Nv3+i+ZQr6GPUv8ILHJEyn1kladwX0tubqXyR8caETe0FL0=";
