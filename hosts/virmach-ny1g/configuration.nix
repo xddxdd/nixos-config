cff04f3473f603c05f240aefe01436ebbe4bd8a7
Lan Tian <xuyh0120@outlook.com>
2023-07-14 23:10:18 -0700

    hosts(virmach-ny1g, virmach-ny6g): prepare for IP switch

--- hosts/virmach-ny1g/configuration.nix
+++ hosts/virmach-ny1g/configuration.nix
@@ -15,8 +15,8 @@
   ];
 
   systemd.network.networks.eth0 = {
-    address = ["216.52.57.200/24"];
-    gateway = ["216.52.57.1"];
+    address = ["216.52.57.200/24" "47.87.186.200/24"];
+    gateway = ["47.87.186.1"];
     matchConfig.Name = "eth0";
     networkConfig.Tunnel = "henet";
   };
