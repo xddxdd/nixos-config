2b92162431dd40896659a4bc937204d171b7c22f
Lan Tian <xuyh0120@outlook.com>
2023-07-20 23:55:05 -0700

    gui: update netease-cloud-music

--- overlays/01-nixos-cn-fix.nix
+++
@@ -1,7 +0,0 @@
-{inputs, ...}: final: prev: rec {
-  nixos-cn =
-    prev.nixos-cn
-    // {
-      netease-cloud-music = prev.nixos-cn.netease-cloud-music.override {libusb = final.libusb1;};
-    };
-}
