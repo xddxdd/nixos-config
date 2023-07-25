ce23135b199c96f90ef7a1ea6a52afc9b04d623c
Lan Tian <xuyh0120@outlook.com>
2023-07-24 21:03:03 -0700

    nginx: disable nginx config verifier with overlay instead of patch

---
+++ overlays/40-nixpkgs-lib.nix
@@ -0,0 +1,15 @@
+{inputs, ...}: final: prev: {
+  writers =
+    prev.writers
+    // {
+      writeNginxConfig = name: text:
+        final.runCommandLocal name {
+          inherit text;
+          passAsFile = ["text"];
+        }
+        ''
+          # nginx-config-formatter has an error - https://github.com/1connect/nginx-config-formatter/issues/16
+          awk -f ${prev.writers.awkFormatNginx} "$textPath" | sed '/^\s*$/d' > $out
+        '';
+    };
+}
