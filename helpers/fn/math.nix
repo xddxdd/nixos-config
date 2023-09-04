0ff04426878fca95000479ba55c21dbf7aa27fe5
Lan Tian <xuyh0120@outlook.com>
2023-09-04 02:21:27 -0700

    helpers: simplify trigonometric function code

--- helpers/fn/math.nix
+++ helpers/fn/math.nix
@@ -2,6 +2,9 @@
   pi = 3.1415926;
   epsilon = 0.0000000001;
 
+  sum = builtins.foldl' builtins.add 0;
+  multiply = builtins.foldl' builtins.mul 1;
+
   parseFloat = builtins.fromJSON;
 
   floor = x: let
@@ -23,45 +26,18 @@
     then mod (b - mod (0 - a) b) b
     else a - b * (div a b);
 
-  pow = x: times: let
-    helper = tmp: times:
-      if times > 0
-      then helper (tmp * x) (times - 1)
-      else tmp;
-  in
-    helper 1.0 times;
+  pow = x: times: multiply (lib.replicate times x);
 
-  factorial = x: let
-    helper = tmp: times:
-      if times > 0
-      then helper (tmp * times) (times - 1)
-      else tmp;
-  in
-    helper 1.0 x;
+  factorial = x: multiply (lib.range 1 x);
 
   # Taylor series: for x >= 0, atan(x) = x - x^3/3! + x^5/5!
   sin = x: let
-    x_shrunk = mod (1.0 * x) (2 * pi);
-
-    iterations = 10;
-
-    step = i:
-      (
-        if mod i 2 == 0
-        then -1.0
-        else 1.0
-      )
-      * (pow x_shrunk (i * 2 - 1))
-      / (factorial (i * 2 - 1));
-
-    helper = tmp: times:
-      if times > 0
-      then helper (tmp + step times) (times - 1)
-      else tmp;
+    x' = mod (1.0 * x) (2 * pi);
+    step = i: (pow (0 - 1) (i - 1)) * multiply (lib.genList (j: x' / (j + 1)) (i * 2 - 1));
   in
     if x < 0
     then -sin (0 - x)
-    else helper 0 iterations;
+    else sum (lib.genList (i: step (i + 1)) 10);
 
   cos = x: sin (0.5 * pi - x);
 
@@ -70,41 +46,20 @@
   # https://stackoverflow.com/questions/42537957/fast-accurate-atan-arctan-approximation-algorithm
   # Taylor series: for 0 <= x <= 1, atan(x) = x - x^3/3 + x^5/5
   atan = x: let
-    iterations = 10;
-
-    step = i:
-      (
-        if mod i 2 == 0
-        then -1
-        else 1
-      )
-      * (pow (1.0 * x) (i * 2 - 1))
-      / (i * 2 - 1);
-
-    helper = tmp: times:
-      if times > 0
-      then helper (tmp + step times) (times - 1)
-      else tmp;
+    step = i: (pow (0 - 1) (i - 1)) * (pow (1.0 * x) (i * 2 - 1)) / (i * 2 - 1);
   in
     if x < 0
     then atan (0 - x)
     else if x > 1
     then pi / 2 - atan (1 / x)
-    else helper 0 iterations;
+    else sum (lib.genList (i: step (i + 1)) 10);
 
   deg2rad = x: x * pi / 180;
 
-  sqrt = x: let
-    iterations = 10;
-    step = tmp: (tmp + x / tmp) / 2;
-    helper = tmp: times:
-      if times > 0
-      then helper (step tmp) (times - 1)
-      else tmp;
-  in
+  sqrt = x:
     if x < epsilon
     then 0
-    else helper (1.0 * x) iterations;
+    else builtins.foldl' (i: _: (i + 1.0 * x / i) / 2) (1.0 * x) (lib.range 1 10);
 
   # https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
   haversine = lat1: lon1: lat2: lon2: let
