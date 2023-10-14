68c19e3c3ece33f716d299be6f6adc90b6e05627
Lan Tian <xuyh0120@outlook.com>
2023-10-13 22:37:45 -0700

    general: use NVIDIA GRID driver from my NUR

--- nixos/common-components/kernel/nvidia-x11/grid_16_0.nix
+++
@@ -1,8 +0,0 @@
-import lib/grid_base.nix {
-  version = "535.54.03";
-  sha256_64bit = "0wb675s4acyjq0id0i14d1h84yw3hw7ijsf4bqrjqrclxjky63w4";
-  settingsSha256 = "sha256-ck7ra8y8nn5kA3L9/VcRR2W2RaWvfVbgBiOh2dRJr/8=";
-  settingsVersion = "535.54.03";
-  persistencedSha256 = "sha256-R5WCh09BSPjfifui0ODkCsdIXTowceNjLDq5XHwda08=";
-  persistencedVersion = "535.54.03";
-}
