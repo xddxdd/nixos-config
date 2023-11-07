1a981a0e525c999282845c52dcecfa4e0ba698ed
Lan Tian <xuyh0120@outlook.com>
2023-11-06 21:24:10 -0800

    iperf3: setup

---
+++ nixos/server-apps/iperf.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args:
+lib.mkIf (!(builtins.elem LT.tags.low-ram LT.this.tags)) {
+  services.iperf3 = {
+    enable = true;
+    bind = LT.this.ltnet.IPv4;
+    port = LT.port.IPerf;
+  };
+}
