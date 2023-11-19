09b316071738f680073f0f2170a98a87bd7ea519
Lan Tian <xuyh0120@outlook.com>
2023-11-18 18:34:49 -0800

    netbox: oauth with nginx

--- nixos/optional-apps/netbox.nix
+++ nixos/optional-apps/netbox.nix
@@ -25,16 +25,22 @@
     secretKeyFile = config.age.secrets.netbox-secret.path;
     settings = {
       CSRF_TRUSTED_ORIGINS = ["https://netbox.xuyh0120.win"];
+      REMOTE_AUTH_ENABLED = true;
+      REMOTE_AUTH_BACKEND = "netbox.authentication.RemoteUserBackend";
+      REMOTE_AUTH_HEADER = "HTTP_X_USER";
+      REMOTE_AUTH_AUTO_CREATE_USER = true;
     };
   };
 
   services.nginx.virtualHosts."netbox.xuyh0120.win" = {
     listen = LT.nginx.listenHTTPS;
     locations = LT.nginx.addCommonLocationConf {} {
-      "/" = {
-        proxyPass = "http://${config.services.netbox.listenAddress}:${LT.portStr.Netbox}";
-        extraConfig = LT.nginx.locationProxyConf;
-      };
+      "/".extraConfig =
+        LT.nginx.locationOauthConf
+        + ''
+          proxy_pass http://${config.services.netbox.listenAddress}:${LT.portStr.Netbox};
+        ''
+        + LT.nginx.locationProxyConf;
       "/static/".alias = config.services.netbox.settings.STATIC_ROOT + "/";
     };
     extraConfig =
