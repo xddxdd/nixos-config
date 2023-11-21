1d7942b50a0cba3a5d8d911ba523f2e09f8b05c1
Lan Tian <xuyh0120@outlook.com>
2023-11-20 22:36:31 -0800

    bgp-graph: package systemd units into single closure

--- nixos/optional-apps/bgp-graph/edges.nix
+++ nixos/optional-apps/bgp-graph/edges.nix
@@ -7,80 +7,85 @@
   inputs,
   ...
 } @ args: let
-  svcForEdge = instance: instanceCfg: edge: let
-    sanitizeName = n: builtins.replaceStrings ["_"] ["-"] (lib.toLower (LT.sanitizeName "${instance}-${n}"));
-  in {
-    name = "bgp-graph-edge-${instance}-${builtins.toString edge.fromId}-${builtins.toString edge.toId}";
-    value = let
+  edgeServices = let
+    ipbin = "${pkgs.iproute2}/bin/ip";
+    create-conf = pkgs.writeShellScriptBin "create-conf" ''
+      FROM_NETNS=$1
+      TO_NETNS=$2
+      FROM_ID=$3
+      TO_ID=$4
+      FROM_LINK_LOCAL=$5
+      TO_LINK_LOCAL=$6
+      FROM_SERVICE=$7
+      TO_SERVICE=$8
+      OUTPUT=$9
+
+      cat > $OUTPUT <<EOF
+      [Unit]
+      After=network-pre.target $FROM_SERVICE $TO_SERVICE
+      Requires=$FROM_SERVICE $TO_SERVICE
+
+      [Service]
+      # Setup veth pair
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${ipbin} link add v$TO_ID type veth peer t$FROM_ID$TO_ID
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${ipbin} link set t$FROM_ID$TO_ID netns $TO_NETNS
+      ExecStart=${ipbin} netns exec $TO_NETNS ${ipbin} link set t$FROM_ID$TO_ID name v$FROM_ID
+
+      # https://serverfault.com/questions/935366/why-does-arp-ignore-1-break-arp-on-pointopoint-interfaces-kvm-guest
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.v$TO_ID.arp_ignore=0
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.v$TO_ID.arp_announce=0
+      ExecStart=${ipbin} netns exec $TO_NETNS ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.v$FROM_ID.arp_ignore=0
+      ExecStart=${ipbin} netns exec $TO_NETNS ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.v$FROM_ID.arp_announce=0
+
+      # Setup LL IP
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${ipbin} link set v$TO_ID up
+      ExecStart=${ipbin} netns exec $FROM_NETNS ${ipbin} addr add $FROM_LINK_LOCAL/10 dev v$TO_ID
+      ExecStart=${ipbin} netns exec $TO_NETNS ${ipbin} link set v$FROM_ID up
+      ExecStart=${ipbin} netns exec $TO_NETNS ${ipbin} addr add $TO_LINK_LOCAL/10 dev v$FROM_ID
+
+      ExecStartPre=-${ipbin} netns exec $FROM_NETNS ${ipbin} link del v$TO_ID
+      ExecStartPre=-${ipbin} netns exec $TO_NETNS ${ipbin} link del v$FROM_ID
+      ExecStopPost=${ipbin} netns exec $FROM_NETNS ${ipbin} link del v$TO_ID
+      ExecStopPost=${ipbin} netns exec $TO_NETNS ${ipbin} link del v$FROM_ID
+      RemainAfterExit=true
+      Type=oneshot
+      EOF
+    '';
+
+    forEachEdge = instance: instanceCfg: edge: let
+      sanitizeName = n: builtins.replaceStrings ["_"] ["-"] (lib.toLower (LT.sanitizeName "${instance}-${n}"));
+
       fromNode = instanceCfg.nodeIdMap."${builtins.toString edge.fromId}";
       toNode = instanceCfg.nodeIdMap."${builtins.toString edge.toId}";
-
-      fromNetns = "bgp-graph-${sanitizeName fromNode.name}";
-      toNetns = "bgp-graph-${sanitizeName toNode.name}";
 
       fromLinkLocal = LT.net.cidr.host fromNode.id "fe80::/10";
       toLinkLocal = LT.net.cidr.host toNode.id "fe80::/10";
 
-      ipbin = "${pkgs.iproute2}/bin/ip";
-      fromIpNs = "${ipbin} netns exec ${fromNetns} ${ipbin}";
-      toIpNs = "${ipbin} netns exec ${toNetns} ${ipbin}";
-      fromSysctl = "${ipbin} netns exec ${fromNetns} ${pkgs.procps}/bin/sysctl";
-      toSysctl = "${ipbin} netns exec ${toNetns} ${pkgs.procps}/bin/sysctl";
-    in {
-      wantedBy = ["multi-user.target" "network.target"];
-      after = [
-        "network-pre.target"
-        "bgp-graph-netns-${sanitizeName fromNode.name}.service"
-        "bgp-graph-netns-${sanitizeName toNode.name}.service"
-      ];
-      requires = [
-        "bgp-graph-netns-${sanitizeName fromNode.name}.service"
-        "bgp-graph-netns-${sanitizeName toNode.name}.service"
-      ];
-      script = ''
-        set -x
-
-        # Setup veth pair
-        FROM_IF=v${builtins.toString toNode.id}
-        TO_IF=v${builtins.toString fromNode.id}
-        TEMP_IF=t${builtins.toString fromNode.id}${builtins.toString toNode.id}
-
-        ${fromIpNs} link add $FROM_IF type veth peer $TEMP_IF
-        ${fromIpNs} link set $TEMP_IF netns ${toNetns}
-        ${toIpNs} link set $TEMP_IF name $TO_IF
-
-        # https://serverfault.com/questions/935366/why-does-arp-ignore-1-break-arp-on-pointopoint-interfaces-kvm-guest
-        ${fromSysctl} -w net.ipv4.conf.$FROM_IF.arp_ignore=0
-        ${fromSysctl} -w net.ipv4.conf.$FROM_IF.arp_announce=0
-        ${toSysctl} -w net.ipv4.conf.$TO_IF.arp_ignore=0
-        ${toSysctl} -w net.ipv4.conf.$TO_IF.arp_announce=0
-
-        # Setup LL IP
-        ${fromIpNs} link set $FROM_IF up
-        ${fromIpNs} addr add ${fromLinkLocal}/10 dev $FROM_IF
-        ${toIpNs} link set $TO_IF up
-        ${toIpNs} addr add ${toLinkLocal}/10 dev $TO_IF
-      '';
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-
-        ExecStartPre = [
-          "-${fromIpNs} link del v${builtins.toString toNode.id}"
-          "-${toIpNs} link del v${builtins.toString fromNode.id}"
-        ];
-        ExecStopPost = [
-          "${fromIpNs} link del v${builtins.toString toNode.id}"
-          "${toIpNs} link del v${builtins.toString fromNode.id}"
-        ];
-      };
-    };
-  };
+      unitName = "bgp-graph-edge-${instance}-${builtins.toString edge.fromId}-${builtins.toString edge.toId}.service";
+    in ''
+      create-conf \
+        bgp-graph-${sanitizeName fromNode.name} \
+        bgp-graph-${sanitizeName toNode.name} \
+        ${builtins.toString edge.fromId} \
+        ${builtins.toString edge.toId} \
+        ${fromLinkLocal} \
+        ${toLinkLocal} \
+        bgp-graph-netns-${sanitizeName fromNode.name}.service \
+        bgp-graph-netns-${sanitizeName toNode.name}.service \
+        $out/etc/systemd/system/${unitName}
+    '';
+  in
+    pkgs.runCommand "bgp-graph-edges" {
+      nativeBuildInputs = [create-conf];
+    } (lib.concatStringsSep "\n" (lib.flatten ([
+        ''
+          mkdir -p $out/etc/systemd/system
+        ''
+      ]
+      ++ lib.mapAttrsToList (
+        instance: cfg: (builtins.map (forEachEdge instance cfg) cfg.edges)
+      )
+      config.lantian.bgp-graph)));
 in {
-  systemd.services = builtins.listToAttrs (lib.flatten (lib.mapAttrsToList (
-      instance: cfg:
-        builtins.map (svcForEdge instance cfg) cfg.edges
-    )
-    config.lantian.bgp-graph));
+  systemd.packages = [edgeServices];
 }
--- nixos/optional-apps/bgp-graph/nodes.nix
+++ nixos/optional-apps/bgp-graph/nodes.nix
@@ -7,62 +7,71 @@
   inputs,
   ...
 } @ args: let
-  svcForNode = instance: instanceCfg: node: let
-    sanitizeName = n: builtins.replaceStrings ["_"] ["-"] (lib.toLower (LT.sanitizeName "${instance}-${n}"));
-  in {
-    name = "bgp-graph-netns-${sanitizeName node.name}";
-    value = let
+  nodeServices = let
+    ipbin = "${pkgs.iproute2}/bin/ip";
+    create-conf = pkgs.writeShellScriptBin "create-conf" ''
+      NETNS_NAME=$1
+      NETNS_IP=$2
+      OUTPUT=$3
+
+      cat > $OUTPUT <<EOF
+      [Unit]
+      After=network-pre.target
+
+      [Service]
+      # Setup namespace
+      ExecStart=${ipbin} netns add $NETNS_NAME
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${ipbin} link set lo up
+      # Enable forwarding
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.default.forwarding=1
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.all.forwarding=1
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.forwarding=1
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.forwarding=1
+      # Disable auto generated IPv6 link local address
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.autoconf=0
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.autoconf=0
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.accept_ra=0
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.accept_ra=0
+      ExecStart=${pkgs.bash}/bin/bash -c "${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.addr_gen_mode=1 || true"
+      ExecStart=${pkgs.bash}/bin/bash -c "${ipbin} netns exec $NETNS_NAME ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.addr_gen_mode=1 || true"
+      # Setup self IP
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${ipbin} link add dummy0 type dummy
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${ipbin} link set dummy0 up
+      ExecStart=${ipbin} netns exec $NETNS_NAME ${ipbin} addr add $NETNS_IP dev dummy0
+
+      ExecStartPre=-${ipbin} netns delete $NETNS_NAME
+      ExecStopPost=${ipbin} netns exec $NETNS_NAME ${ipbin} link del dummy0
+      ExecStopPost=${ipbin} netns delete $NETNS_NAME
+
+      RemainAfterExit=true
+      Type=oneshot
+      EOF
+    '';
+
+    forEachNode = instance: instanceCfg: node: let
+      sanitizeName = n: builtins.replaceStrings ["_"] ["-"] (lib.toLower (LT.sanitizeName "${instance}-${n}"));
+
       netnsName = "bgp-graph-${sanitizeName node.name}";
       ip = LT.net.cidr.host node.id instanceCfg.baseCidr;
-
-      ipbin = "${pkgs.iproute2}/bin/ip";
-      ipns = "${ipbin} netns exec ${netnsName} ${ipbin}";
-      sysctl = "${ipbin} netns exec ${netnsName} ${pkgs.procps}/bin/sysctl";
-    in {
-      wantedBy = ["multi-user.target" "network.target"];
-      after = ["network-pre.target"];
-      script = ''
-        set -x
-
-        # Setup namespace
-        ${ipbin} netns add ${netnsName}
-        ${ipns} link set lo up
-        # Enable forwarding
-        ${sysctl} -w net.ipv4.conf.default.forwarding=1
-        ${sysctl} -w net.ipv4.conf.all.forwarding=1
-        ${sysctl} -w net.ipv6.conf.default.forwarding=1
-        ${sysctl} -w net.ipv6.conf.all.forwarding=1
-        # Disable auto generated IPv6 link local address
-        ${sysctl} -w net.ipv6.conf.default.autoconf=0
-        ${sysctl} -w net.ipv6.conf.all.autoconf=0
-        ${sysctl} -w net.ipv6.conf.default.accept_ra=0
-        ${sysctl} -w net.ipv6.conf.all.accept_ra=0
-        ${sysctl} -w net.ipv6.conf.default.addr_gen_mode=1 || true
-        ${sysctl} -w net.ipv6.conf.all.addr_gen_mode=1 || true
-        # Setup self IP
-        ${ipns} link add dummy0 type dummy
-        ${ipns} link set dummy0 up
-        ${ipns} addr add ${ip} dev dummy0
-      '';
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-
-        ExecStartPre = [
-          "-${ipbin} netns delete ${netnsName}"
-        ];
-        ExecStopPost = [
-          "${ipns} link del dummy0"
-          "${ipbin} netns delete ${netnsName}"
-        ];
-      };
-    };
-  };
+      unitName = "bgp-graph-netns-${sanitizeName node.name}.service";
+    in ''
+      create-conf \
+        ${netnsName} \
+        ${ip} \
+        $out/etc/systemd/system/${unitName}
+    '';
+  in
+    pkgs.runCommand "bgp-graph-nodes" {
+      nativeBuildInputs = [create-conf];
+    } (lib.concatStringsSep "\n" (lib.flatten ([
+        ''
+          mkdir -p $out/etc/systemd/system
+        ''
+      ]
+      ++ lib.mapAttrsToList (
+        instance: cfg: (builtins.map (forEachNode instance cfg) cfg.nodes)
+      )
+      config.lantian.bgp-graph)));
 in {
-  systemd.services = builtins.listToAttrs (lib.flatten (lib.mapAttrsToList (
-      instance: cfg:
-        builtins.map (svcForNode instance cfg) cfg.nodes
-    )
-    config.lantian.bgp-graph));
+  systemd.packages = [nodeServices];
 }
