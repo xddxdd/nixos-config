76105c08fa9d551391d565e8d9d2225c6a6cdfa6
Lan Tian <xuyh0120@outlook.com>
2023-12-01 01:00:31 -0800

    gui: add packages

--- home/client-apps/packages.nix
+++ home/client-apps/packages.nix
@@ -56,6 +56,7 @@ in {
     appimage-run
     aria
     audacious
+    baidunetdisk
     baidupcs-go
     bilibili
     brotli
@@ -65,6 +66,7 @@ in {
     cloudpan189-go
     colmena
     dbeaver
+    dingtalk
     discord
     discord-canary
     distrobox
