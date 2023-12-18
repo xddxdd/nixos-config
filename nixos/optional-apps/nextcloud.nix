f00be402c82cfd2c86114885b081f2f7e1196a03
Lan Tian <xuyh0120@outlook.com>
2023-12-17 21:54:10 -0800

    nextcloud: upgrade to 28

--- nixos/optional-apps/nextcloud.nix
+++ nixos/optional-apps/nextcloud.nix
@@ -11,7 +11,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     autoUpdateApps.enable = true;
     caching = {
       apcu = true;
