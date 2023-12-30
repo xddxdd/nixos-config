8867dff19efdd48d37e76b13c6b365ea6cc1397e
Lan Tian <xuyh0120@outlook.com>
2023-12-29 21:39:17 -0800

    home: use lanczos scaling algorithm for mpv

--- home/client-apps/mpv.nix
+++ home/client-apps/mpv.nix
@@ -11,11 +11,13 @@
     enable = true;
     package = pkgs.svp-mpv;
     config = {
-      hwdec = "auto-copy";
-      hwdec-codecs = "all";
+      cscale = "ewa_lanczossharp";
       gpu-context = "wayland";
       hr-seek-framedrop = false;
+      hwdec = "auto-copy";
+      hwdec-codecs = "all";
       no-resume-playback = true;
+      scale = "ewa_lanczossharp";
     };
   };
 
