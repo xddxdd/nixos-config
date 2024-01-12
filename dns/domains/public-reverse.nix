dd56b455a57fd4911a1670849482800612597fef
Lan Tian <xuyh0120@outlook.com>
2024-01-11 23:11:32 -0800

    dns: add zones

--- dns/domains/public-reverse.nix
+++ dns/domains/public-reverse.nix
@@ -35,6 +35,15 @@
     })
 
     (config.common.reverse {
+      prefix = "2001:470:e997::/48";
+      target = "lt-home-vm.lantian.pub.";
+    })
+    (config.common.reverse {
+      prefix = "2001:470:b:3af::/64";
+      target = "lt-home-vm.lantian.pub.";
+    })
+
+    (config.common.reverse {
       prefix = "2605:6400:cac6::/48";
       target = "buyvm.lantian.pub.";
     })
