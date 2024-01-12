bbaadb757ce92a1d79c18ea92b02eb22d74e5ccf
Lan Tian <xuyh0120@outlook.com>
2024-01-11 23:11:32 -0800

    dns: add zones

---
+++ dns/domains/lantian.best.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  config,
+  lib,
+  LT,
+  inputs,
+  ...
+} @ args: {
+  domains = [
+    rec {
+      domain = "lantian.best";
+      providers = ["henet"];
+      records = lib.flatten [
+        {
+          recordType = "ALIAS";
+          name = "${domain}.";
+          target = config.common.records.GeoDNSTarget;
+          ttl = "10m";
+        }
+        {
+          recordType = "CNAME";
+          name = "www.${domain}.";
+          target = config.common.records.GeoDNSTarget;
+          ttl = "10m";
+        }
+
+        config.common.hostRecs.CAA
+        (config.common.hostRecs.Normal domain)
+        (config.common.hostRecs.SSHFP domain)
+        (config.common.records.Autoconfig domain)
+        config.common.records.MXRoute
+        config.common.records.Libravatar
+        config.common.records.ProxmoxCluster
+        config.common.records.SIP
+        (config.common.hostRecs.LTNet "ltnet.${domain}")
+        (config.common.hostRecs.DN42 "dn42.${domain}")
+        (config.common.hostRecs.NeoNetwork "neo.${domain}")
+      ];
+    }
+  ];
+}
