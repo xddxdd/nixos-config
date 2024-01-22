e0aa4a0b8e2bb6c0bf05bb38ad9f9ab1abf04863
Lan Tian <xuyh0120@outlook.com>
2024-01-22 01:33:21 -0800

    palworld: setup

--- hosts/lt-home-vm/configuration.nix
+++ hosts/lt-home-vm/configuration.nix
@@ -26,6 +26,7 @@
     ../../nixos/optional-apps/netns-wg-lantian.nix
     ../../nixos/optional-apps/nvidia/cuda-only.nix
     ../../nixos/optional-apps/nvidia/grid-extension.nix
+    ../../nixos/optional-apps/palworld.nix
     ../../nixos/optional-apps/pterodactyl-panel.nix
     ../../nixos/optional-apps/pterodactyl-wings.nix
     ../../nixos/optional-apps/sftp-server.nix
