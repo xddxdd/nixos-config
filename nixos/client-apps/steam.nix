4d98d29b75f82b3dec5433b61958b2bbea29ea07
Lan Tian <xuyh0120@outlook.com>
2024-01-22 17:51:50 -0800

    gui: setup mangohud

---
+++ nixos/client-apps/steam.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+} @ args: let
+  mangohudForCurrentKernel = pkgs.mangohud.override {
+    libXNVCtrl = config.boot.kernelPackages.nvidia_x11.settings.libXNVCtrl;
+  };
+in {
+  programs.steam.enable = true;
+  environment.systemPackages = [mangohudForCurrentKernel];
+  # https://www.reddit.com/r/NixOS/comments/11d76bb/comment/jaqzwyt
+  hardware.opengl = {
+    extraPackages = [mangohudForCurrentKernel];
+    extraPackages32 = [mangohudForCurrentKernel];
+  };
+}
