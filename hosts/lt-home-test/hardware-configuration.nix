762ba7c0b8a578ef389aa71ba5a203f2d81f5f41
Lan Tian <xuyh0120@outlook.com>
2024-02-17 21:53:42 -0800

    hosts: remove duplicate kernel module definitions

--- hosts/lt-home-test/hardware-configuration.nix
+++ hosts/lt-home-test/hardware-configuration.nix
@@ -9,11 +9,6 @@
     ../../nixos/hardware/disable-watchdog.nix
   ];
 
-  boot.initrd.availableKernelModules = ["uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-
   boot.loader.grub = {
     efiSupport = true;
     device = "nodev";
