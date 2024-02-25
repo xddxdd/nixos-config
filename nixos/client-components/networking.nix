4d66f298c858e688fb09f14c97653b9bde0f3ab3
Lan Tian <xuyh0120@outlook.com>
2024-02-24 16:48:46 -0800

    networking: update default TTL for IPv6 as well

--- nixos/client-components/networking.nix
+++ nixos/client-components/networking.nix
@@ -8,5 +8,10 @@
   ...
 } @ args: {
   # Bypass hotspot restrictions for certain ISPs
-  boot.kernel.sysctl."net.ipv4.ip_default_ttl" = 65;
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_default_ttl" = 65;
+    "net.ipv6.conf.all.hop_limit" = 65;
+    "net.ipv6.conf.default.hop_limit" = 65;
+    "net.ipv6.conf.*.hop_limit" = 65;
+  };
 }
