c3c4da799eb30ce0c7dc6e016e8526c7d5b92edd
Lan Tian <xuyh0120@outlook.com>
2024-03-02 06:03:49 -0800

    Revert "rsshub: run as systemd service instead of docker container"
    This reverts commit 2f2fc7b9f5334dd54d7e190c90ee84222d192cba.

--- nixos/optional-apps/rsshub.nix
+++ nixos/optional-apps/rsshub.nix
@@ -7,10 +7,21 @@
   inputs,
   ...
 } @ args: {
+  virtualisation.oci-containers.containers.rsshub = {
+    extraOptions = ["--pull" "always"];
+    image = "diygod/rsshub:chromium-bundled";
+    environment = {
+      CACHE_CONTENT_EXPIRE = "600";
+    };
+    ports = [
+      "127.0.0.1:${LT.portStr.RSSHub}:1200"
+    ];
+  };
+
   lantian.nginxVhosts."rsshub.xuyh0120.win" = {
     locations = {
       "/" = {
-        proxyPass = "http://unix:/run/rsshub/rsshub.sock";
+        proxyPass = "http://127.0.0.1:${LT.portStr.RSSHub}";
       };
     };
 
@@ -18,49 +29,4 @@
     noIndex.enable = true;
     accessibleBy = "private";
   };
-
-  systemd.services.rsshub = {
-    description = "RSSHub";
-    wantedBy = ["multi-user.target"];
-    after = ["network-online.target"];
-    requires = ["network-online.target"];
-
-    environment = {
-      CACHE_CONTENT_EXPIRE = "600";
-      CHROMIUM_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
-      HOME = "/var/cache/rsshub";
-      LISTEN_INADDR_ANY = "0";
-      PORT = "0";
-      SOCKET = "/run/rsshub/rsshub.sock";
-    };
-
-    path = with pkgs; [gitMinimal];
-
-    serviceConfig =
-      LT.serviceHarden
-      // {
-        EnvironmentFile = config.age.secrets.waline-env.path;
-        ExecStartPre = "${pkgs.yarn}/bin/yarn add rsshub";
-        ExecStart = "${pkgs.nodejs}/bin/node node_modules/rsshub/lib/index.ts";
-
-        Restart = "always";
-        RestartSec = "3";
-
-        CacheDirectory = "rsshub";
-        WorkingDirectory = "/var/cache/rsshub";
-        RuntimeDirectory = "rsshub";
-        RuntimeDirectoryMode = lib.mkForce "0775";
-        UMask = "002";
-
-        User = "rsshub";
-        Group = "rsshub";
-        MemoryDenyWriteExecute = lib.mkForce false;
-      };
-  };
-
-  users.users.rsshub = {
-    group = "rsshub";
-    isSystemUser = true;
-  };
-  users.groups.rsshub.members = ["nginx"];
 }
