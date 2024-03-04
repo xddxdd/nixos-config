d4e27da2db44f4bc5d3feb53abc696705772f1d3
Lan Tian <xuyh0120@outlook.com>
2024-03-03 16:05:32 -0800

    hosts: add client role to lt-dell-wyse

--- hosts/lt-dell-wyse/host.nix
+++ hosts/lt-dell-wyse/host.nix
@@ -4,7 +4,7 @@
   ...
 } @ args: {
   index = 104;
-  tags = with tags; [x86_64-v1];
+  tags = with tags; [client x86_64-v1];
   cpuThreads = 4;
   city = geo.cities."US Seattle";
   hostname = "192.168.0.207";
