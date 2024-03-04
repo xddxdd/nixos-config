85498d9358f2881d969b355d8112a64fda1cd3e5
Lan Tian <xuyh0120@outlook.com>
2024-03-03 19:04:14 -0800

    hosts: fix cpu governor for lt-dell-wyse

--- hosts/lt-dell-wyse/hardware-configuration.nix
+++ hosts/lt-dell-wyse/hardware-configuration.nix
@@ -28,7 +28,7 @@
     options = ["compress-force=zstd" "nosuid" "nodev"];
   };
 
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableRedistributableFirmware = true;
 }
