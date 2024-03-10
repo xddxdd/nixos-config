e468e7573a28df8e24d00548d989e335d58c9ec8
Lan Tian <xuyh0120@outlook.com>
2024-03-09 22:44:54 -0800

    gui: remove svp from lt-dell-wyse

--- home/client-apps/packages.nix
+++ home/client-apps/packages.nix
@@ -65,95 +65,97 @@
         --add-flags "--disable-gpu"
     '');
 in {
-  home.packages = with pkgs; [
-    # error: collision between `/nix/store/2vkk2dnf693fzhlx7v2wn2kcvflgkih9-qqmusic-1.1.5/opt/LICENSE.electron.txt' and `/nix/store/zwgihw847calnxy6ff341l1qkilmn8hm-qq-3.2.2-18394/opt/LICENSE.electron.txt'
-    (lib.hiPrio qq)
-    (LT.wrapNetns "wg-lantian" amule-dlp)
-    (LT.wrapNetns "wg-lantian" deluge)
-    (LT.wrapNetns "wg-lantian" qbittorrent-enhanced-edition)
-    (lutris.override {extraPkgs = p: with p; [xdelta];})
-    appimage-run
-    aria
-    audacious
-    baidunetdisk
-    baidupcs-go
-    bilibili
-    brotli
-    bzip2
-    calibre
-    calibre-override-desktop
-    cloudpan189-go
-    colmena
-    comma
-    dbeaver
-    dingtalk
-    discord
-    discord-canary
-    distrobox
-    element-desktop
-    exiftool
-    ffmpeg-full
-    filezilla
-    gcdemu
-    gedit
-    gimp
-    google-earth-pro
-    gopherus
-    imagemagick
-    jamesdsp
-    jamesdsp-toggle
-    jellyfin-media-player
-    jellyfin-media-player-wrapped
-    kdenlive
-    lantianCustomized.attic-telnyx-compatible
-    lbzip2
-    libfaketime
-    librewolf
-    libsForQt5.ark
-    libsForQt5.kpat
-    linphone
-    macchanger
-    matrix-synapse-tools.synadm
-    mediainfo
-    megatools
-    newsflash
-    nvfetcher
-    openvpn
-    p7zip
-    parsec-bin
-    payload-dumper-go
-    powertop
-    prismlauncher
-    prismlauncher-wrapped
-    pwgen
-    qqmusic
-    quasselClient
-    rar
-    remmina
-    space-cadet-pinball-full-tilt
-    steam-run
-    step-cli-wrapped
-    svp
-    tdesktop
-    tigervnc
-    transmission-qt
-    transmission-remote-gtk
-    ulauncher
-    unar
-    vagrant
-    virt-manager
-    vopono
-    vscode
-    vscode-wrapped
-    wechat-uos
-    winetricks
-    wineWowPackages.stable
-    wpsoffice
-    xca
-    yt-dlp
-    yubikey-manager-qt
-    zoom-us
-  ];
+  home.packages = with pkgs; ([
+      # error: collision between `/nix/store/2vkk2dnf693fzhlx7v2wn2kcvflgkih9-qqmusic-1.1.5/opt/LICENSE.electron.txt' and `/nix/store/zwgihw847calnxy6ff341l1qkilmn8hm-qq-3.2.2-18394/opt/LICENSE.electron.txt'
+      (lib.hiPrio qq)
+      (LT.wrapNetns "wg-lantian" amule-dlp)
+      (LT.wrapNetns "wg-lantian" deluge)
+      (LT.wrapNetns "wg-lantian" qbittorrent-enhanced-edition)
+      (lutris.override {extraPkgs = p: with p; [xdelta];})
+      appimage-run
+      aria
+      audacious
+      baidunetdisk
+      baidupcs-go
+      bilibili
+      brotli
+      bzip2
+      calibre
+      calibre-override-desktop
+      cloudpan189-go
+      colmena
+      comma
+      dbeaver
+      dingtalk
+      discord
+      discord-canary
+      distrobox
+      element-desktop
+      exiftool
+      ffmpeg-full
+      filezilla
+      gcdemu
+      gedit
+      gimp
+      google-earth-pro
+      gopherus
+      imagemagick
+      jamesdsp
+      jamesdsp-toggle
+      jellyfin-media-player
+      jellyfin-media-player-wrapped
+      kdenlive
+      lantianCustomized.attic-telnyx-compatible
+      lbzip2
+      libfaketime
+      librewolf
+      libsForQt5.ark
+      libsForQt5.kpat
+      linphone
+      macchanger
+      matrix-synapse-tools.synadm
+      mediainfo
+      megatools
+      newsflash
+      nvfetcher
+      openvpn
+      p7zip
+      parsec-bin
+      payload-dumper-go
+      powertop
+      prismlauncher
+      prismlauncher-wrapped
+      pwgen
+      qqmusic
+      quasselClient
+      rar
+      remmina
+      space-cadet-pinball-full-tilt
+      steam-run
+      step-cli-wrapped
+      tdesktop
+      tigervnc
+      transmission-qt
+      transmission-remote-gtk
+      ulauncher
+      unar
+      vagrant
+      virt-manager
+      vopono
+      vscode
+      vscode-wrapped
+      wechat-uos
+      winetricks
+      wineWowPackages.stable
+      wpsoffice
+      xca
+      yt-dlp
+      yubikey-manager-qt
+      zoom-us
+    ]
+    ++ lib.optionals (osConfig.networking.hostName != "lt-dell-wyse") [
+      svp
+    ]);
 
   xdg.configFile = LT.gui.autostart ((lib.optionals (osConfig.networking.hostName == "lt-hp-omen") [
       {
