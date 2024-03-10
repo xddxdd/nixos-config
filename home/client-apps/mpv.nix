3e3c6fdf01d6280296ae2d276ba7921d745b8f0a
Lan Tian <xuyh0120@outlook.com>
2024-03-09 22:36:52 -0800

    mpv: use cuda acceleration for lt-dell-wyse

--- home/client-apps/mpv.nix
+++ home/client-apps/mpv.nix
@@ -5,20 +5,26 @@
   config,
   utils,
   inputs,
+  osConfig,
   ...
 } @ args: {
   programs.mpv = {
     enable = true;
     package = pkgs.svp-mpv;
-    config = {
-      cscale = "ewa_lanczossharp";
-      gpu-context = "wayland";
-      hr-seek-framedrop = false;
-      hwdec = "auto-copy";
-      hwdec-codecs = "all";
-      no-resume-playback = true;
-      scale = "ewa_lanczossharp";
-    };
+    config =
+      {
+        cscale = "ewa_lanczossharp";
+        gpu-context = "wayland";
+        hr-seek-framedrop = false;
+        hwdec = "auto-copy";
+        hwdec-codecs = "all";
+        no-resume-playback = "";
+        scale = "ewa_lanczossharp";
+      }
+      // (lib.optionalAttrs (osConfig.networking.hostName == "lt-dell-wyse") {
+        hwdec = "cuda";
+        scale = "lanczos";
+      });
   };
 
   xdg.configFile."mpv/scripts".source = LT.sources.mpv-sockets.src;
