c5e9790641347d2e5858bcb9fad9227c0bf62263
Lan Tian <xuyh0120@outlook.com>
2024-04-05 23:16:51 -0700

    gui: set xdg portal config

--- nixos/client-components/xorg.nix
+++ nixos/client-components/xorg.nix
@@ -85,6 +85,7 @@
   xdg = {
     portal = {
       enable = true;
+      config.common.default = "*";
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
