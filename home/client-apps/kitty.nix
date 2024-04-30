9c0033966d405a6314d75e4d14e8f5b324cfa970
Lan Tian <xuyh0120@outlook.com>
2024-04-29 21:33:23 -0700

    gui: disable kitty close window confirmation

--- home/client-apps/kitty.nix
+++ home/client-apps/kitty.nix
@@ -31,6 +31,7 @@
       initial_window_height = "25c";
       resize_in_steps = true;
       enabled_layouts = "splits";
+      confirm_os_window_close = 0;
 
       wheel_scroll_multiplier = 10;
       wheel_scroll_min_lines = 1;
