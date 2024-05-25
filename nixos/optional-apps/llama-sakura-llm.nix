9e627c91189d30f965cc77df98e6e1f180f8f9e6
Lan Tian <xuyh0120@outlook.com>
2024-05-24 22:30:43 -0700

    llama-sakura-llm: disable nginx timeout

--- nixos/optional-apps/llama-sakura-llm.nix
+++ nixos/optional-apps/llama-sakura-llm.nix
@@ -34,7 +34,10 @@ in
 
   lantian.nginxVhosts = {
     "llama-cpp.${config.networking.hostName}.xuyh0120.win" = {
-      locations."/".proxyPass = "http://127.0.0.1:${LT.portStr.LlamaCpp}";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${LT.portStr.LlamaCpp}";
+        proxyNoTimeout = true;
+      };
 
       sslCertificate = "${config.networking.hostName}.xuyh0120.win_ecc";
       noIndex.enable = true;
@@ -43,7 +46,10 @@ in
       listenHTTP.enable = true;
       listenHTTPS.enable = false;
 
-      locations."/".proxyPass = "http://127.0.0.1:${LT.portStr.LlamaCpp}";
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${LT.portStr.LlamaCpp}";
+        proxyNoTimeout = true;
+      };
 
       noIndex.enable = true;
       accessibleBy = "localhost";
