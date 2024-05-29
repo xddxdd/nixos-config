2f2f9a0d8ab4d78da906a26b34ed47165a11117f
Lan Tian <xuyh0120@outlook.com>
2024-05-28 19:08:46 -0700

    llama-sakura-llm: switch to 14b model

--- nixos/optional-apps/llama-sakura-llm.nix
+++ nixos/optional-apps/llama-sakura-llm.nix
@@ -6,23 +6,48 @@
 }:
 let
   # https://github.com/SakuraLLM/Sakura-13B-Galgame
-  model = {
-    "sakura-32b-qwen2beta-v0.9-iq4xs" = pkgs.fetchurl {
+  model = pkgs.linkFarm "llama-model" {
+    "sakura-14b-qwen2beta-v0.9-iq4_xs_ver2.gguf" = pkgs.fetchurl {
+      name = "sakura-14b-qwen2beta-v0.9-iq4_xs_ver2.gguf";
+      url = "https://huggingface.co/SakuraLLM/Sakura-14B-Qwen2beta-v0.9-GGUF/resolve/main/sakura-14b-qwen2beta-v0.9-iq4_xs_ver2.gguf?download=true";
+      sha256 = "0g6jmm10jmc3hr6zqh00flg988ij4hspgac1ppmanas7mbmbj0nr";
+    };
+    "sakura-14b-qwen2beta-v0.9.1-iq4xs.gguf" = pkgs.fetchurl {
+      name = "sakura-14b-qwen2beta-v0.9.1-iq4xs.gguf";
+      url = "https://huggingface.co/SakuraLLM/Sakura-14B-Qwen2beta-v0.9.1-GGUF/resolve/main/sakura-14b-qwen2beta-v0.9.1-iq4xs.gguf?download=true";
+      sha256 = "0wykq0lxms47xnj0nv5r7x79bfg0gm26z97ngi9h3b32a8dwqx9l";
+    };
+    "sakura-14b-qwen2beta-v0.9.1-q4km.gguf" = pkgs.fetchurl {
+      name = "sakura-14b-qwen2beta-v0.9.1-q4km.gguf";
+      url = "https://huggingface.co/SakuraLLM/Sakura-14B-Qwen2beta-v0.9.1-GGUF/resolve/main/sakura-14b-qwen2beta-v0.9.1-q4km.gguf?download=true";
+      sha256 = "0psrg53ms90ccwiq136yg9s8mn5xg0z2lv40xhwbpcfr5ca8l6sk";
+    };
+    "sakura-14b-qwen2beta-v0.9.2-iq4xs.gguf" = pkgs.fetchurl {
+      name = "sakura-14b-qwen2beta-v0.9.2-iq4xs.gguf";
+      url = "https://huggingface.co/SakuraLLM/Sakura-14B-Qwen2beta-v0.9.2-GGUF/resolve/main/sakura-14b-qwen2beta-v0.9.2-iq4xs.gguf?download=true";
+      sha256 = "0rzd7jd0ifl9c4qx9cns2nb7h2ha5fxmaphlf6ixm9g2wnbpwji5";
+    };
+    "sakura-14b-qwen2beta-v0.9.2-q4km.gguf" = pkgs.fetchurl {
+      name = "sakura-14b-qwen2beta-v0.9.2-q4km.gguf";
+      url = "https://huggingface.co/SakuraLLM/Sakura-14B-Qwen2beta-v0.9.2-GGUF/resolve/main/sakura-14b-qwen2beta-v0.9.2-q4km.gguf?download=true";
+      sha256 = "198180v585ay0bi7v0v0sm3ih1qsp2ay8flg79ygl9vkbgiimblb";
+    };
+    "sakura-32b-qwen2beta-v0.9-iq4xs.gguf" = pkgs.fetchurl {
       name = "sakura-32b-qwen2beta-v0.9-iq4xs.gguf";
       url = "https://huggingface.co/SakuraLLM/Sakura-32B-Qwen2beta-v0.9-GGUF/resolve/main/sakura-32b-qwen2beta-v0.9-iq4xs.gguf?download=true";
       sha256 = "0zr7b9fqlflgw40lzxfrd3sal08y3x86ghy4d9zby0m2zbcbd01s";
     };
-    "sakura-32b-qwen2beta-v0.9-q4km" = pkgs.fetchurl {
+    "sakura-32b-qwen2beta-v0.9-q4km.gguf" = pkgs.fetchurl {
       name = "sakura-32b-qwen2beta-v0.9-q4km.gguf";
       url = "https://huggingface.co/SakuraLLM/Sakura-32B-Qwen2beta-v0.9-GGUF/resolve/main/sakura-32b-qwen2beta-v0.9-q4km.gguf?download=true";
       sha256 = "15p63khy3b7z15kk97a794d6agjzkalkalvizajlmz9jfh7fh03k";
     };
-    "sakura-32b-qwen2beta-v0.9.1-iq4xs" = pkgs.fetchurl {
+    "sakura-32b-qwen2beta-v0.9.1-iq4xs.gguf" = pkgs.fetchurl {
       name = "sakura-32b-qwen2beta-v0.9.1-iq4xs.gguf";
       url = "https://huggingface.co/SakuraLLM/Sakura-32B-Qwen2beta-v0.9.1-GGUF/resolve/main/sakura-32b-qwen2beta-v0.9.1-iq4xs.gguf?download=true";
       sha256 = "1i1qkc8yy9ijp4hfhnvzwbkkii52f190l82wadxry1l11whppb35";
     };
-    "sakura-32b-qwen2beta-v0.10pre1-iq4xs" = pkgs.fetchurl {
+    "sakura-32b-qwen2beta-v0.10pre1-iq4xs.gguf" = pkgs.fetchurl {
       name = "sakura-32b-qwen2beta-v0.10pre1-iq4xs.gguf";
       url = "https://huggingface.co/SakuraLLM/Sakura-32B-Qwen2beta-v0.10pre1-GGUF/resolve/main/sakura-32b-qwen2beta-v0.10pre1-iq4xs.gguf?download=true";
       sha256 = "0r7v5bdlf8qm0zv0yqi8w641zh8yvpbhyqm765hm28f55ycsw14i";
@@ -35,7 +60,7 @@ in
     package = pkgs.lantianCustomized.llama-cpp;
     host = "127.0.0.1";
     port = LT.port.LlamaCpp;
-    model = model."sakura-32b-qwen2beta-v0.9-iq4xs";
+    model = "${model}/sakura-14b-qwen2beta-v0.9.1-iq4xs.gguf";
     extraFlags =
       let
         concurrency = 4;
