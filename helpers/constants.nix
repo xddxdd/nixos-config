bff62cfb2761c19b9e482b66e80e14e9b04533fc
Lan Tian <xuyh0120@outlook.com>
2024-06-21 21:54:46 -0700

    gui: replace wine with wine-ge from nix-gaming

--- helpers/constants.nix
+++ helpers/constants.nix
@@ -60,9 +60,13 @@ rec {
   };
 
   nix = {
-    substituters = [ "https://cuda-maintainers.cachix.org" ];
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+      "https://nix-gaming.cachix.org"
+    ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
 
