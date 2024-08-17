f9c25d6076757653612a155f7026be848340de38
Lan Tian <xuyh0120@outlook.com>
2024-08-16 20:07:17 -0700

    hosts(lt-home-vm): use flaresolverr module from nixpkgs

--- nixos/optional-apps/flaresolverr.nix
+++ nixos/optional-apps/flaresolverr.nix
@@ -6,31 +6,16 @@
   ...
 }:
 {
-  systemd.services.flaresolverr = {
-    description = "FlareSolverr";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    wants = [ "network.target" ];
-    path = with pkgs; [ xorg.xorgserver ];
-    environment = {
-      HOME = "/run/flaresolverr";
-      HOST = "127.0.0.1";
-      PORT = LT.portStr.FlareSolverr;
-      LOG_LEVEL = "warn";
-      TZ = config.time.timeZone;
-      LANG = config.i18n.defaultLocale;
-      TEST_URL = "https://www.example.com";
-    };
-    serviceConfig = LT.serviceHarden // {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "3";
-      ExecStart = "${pkgs.flaresolverr}/bin/flaresolverr";
-      RuntimeDirectory = "flaresolverr";
-      WorkingDirectory = "/run/flaresolverr";
+  services.flaresolverr = {
+    enable = true;
+    port = LT.port.FlareSolverr;
+  };
 
-      MemoryDenyWriteExecute = false;
-      SystemCallFilter = lib.mkForce [ ];
-    };
+  systemd.services.flaresolverr.environment = {
+    HOST = "127.0.0.1";
+    LOG_LEVEL = "warn";
+    TZ = config.time.timeZone;
+    LANG = config.i18n.defaultLocale;
+    TEST_URL = "https://www.example.com";
   };
 }
