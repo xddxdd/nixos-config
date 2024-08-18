b4b663a621406b153fcddeacc249e5c723281b1f
Lan Tian <xuyh0120@outlook.com>
2024-08-17 20:32:36 -0700

    home: improve Steam download speed

---
+++ home/client-apps/steam-download-speedup.nix
@@ -0,0 +1,9 @@
+_:
+{
+  # https://www.reddit.com/r/linux_gaming/comments/16e1l4h/slow_steam_downloads_try_this/
+  xdg.dataFile."Steam/steam_dev.cfg".text = ''
+    @nClientDownloadEnableHTTP2PlatformLinux 0
+    @fDownloadRateImprovementToAddAnotherConnection 1.1
+    @cMaxInitialDownloadSources 15
+  '';
+}
