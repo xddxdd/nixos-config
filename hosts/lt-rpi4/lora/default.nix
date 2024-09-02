8e688468d1c3b8d10351b7290f4f778892f64b8e
Lan Tian <xuyh0120@outlook.com>
2024-09-02 14:46:21 -0700

    hosts(lt-rpi4): connect LoRa to The Things Network

--- hosts/lt-rpi4/lora/default.nix
+++ hosts/lt-rpi4/lora/default.nix
@@ -1,27 +1,46 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  utils,
+  ...
+}:
 let
-  sx1302HalConfig = pkgs.writeText "sx1302_local_conf.json" (
-    builtins.toJSON {
-      "gateway_conf" = {
-        "gps_i2c_path" = "/dev/i2c-1";
-        "gateway_ID" = "AA555A0000000000";
-        # change with default server address/ports
-        "server_address" = "localhost";
-        "serv_port_up" = 1680;
-        "serv_port_down" = 1680;
-        # adjust the following parameters for your network
-        "keepalive_interval" = 10;
-        "stat_interval" = 30;
-        "push_timeout_ms" = 100;
-        # forward only valid packets
-        "forward_crc_valid" = true;
-        "forward_crc_error" = false;
-        "forward_crc_disabled" = false;
+  sx1302HalConfig = {
+    "gateway_conf" = {
+      "gateway_ID" = {
+        _secret = config.age.secrets.lora-euid.path;
       };
-    }
-  );
+      "server_address" = "nam1.cloud.thethings.network";
+      "serv_port_up" = 1700;
+      "serv_port_down" = 1700;
+      "servers" = [
+        {
+          "gateway_ID" = {
+            _secret = config.age.secrets.lora-euid.path;
+          };
+          "server_address" = "nam1.cloud.thethings.network";
+          "serv_port_up" = 1700;
+          "serv_port_down" = 1700;
+          "serv_enabled" = true;
+        }
+      ];
+
+      "gps_i2c_path" = "/dev/i2c-1";
+      # adjust the following parameters for your network
+      "keepalive_interval" = 10;
+      "stat_interval" = 30;
+      "push_timeout_ms" = 100;
+      # forward only valid packets
+      "forward_crc_valid" = true;
+      "forward_crc_error" = false;
+      "forward_crc_disabled" = false;
+    };
+  };
 in
 {
+  age.secrets.lora-euid.file = inputs.secrets + "/lora-euid.age";
+
   systemd.services.lora-sx1302-hal = {
     description = "LoRa SX1302 HAL";
     wantedBy = [ "multi-user.target" ];
@@ -29,7 +48,7 @@ in
     # reset_lgw.sh modified according to https://github.com/Lora-net/sx1302_hal/issues/67
     preStart = ''
       install -Dm755 ${./reset_lgw.sh} reset_lgw.sh
-      install -Dm644 ${sx1302HalConfig} local_conf.json
+      ${utils.genJqSecretsReplacementSnippet sx1302HalConfig "local_conf.json"}
     '';
 
     serviceConfig = {
