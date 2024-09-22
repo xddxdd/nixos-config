a5006aeb8ffff3543568c0a5654fac1996aea22c
Lan Tian <xuyh0120@outlook.com>
2024-09-21 21:17:34 -0700

    hosts(pve-epyc): limit power usage of NVIDIA GPUs

--- hosts/pve-epyc/hardware-configuration.nix
+++ hosts/pve-epyc/hardware-configuration.nix
@@ -50,4 +50,13 @@
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableRedistributableFirmware = true;
+
+  systemd.services.nvidia-power-limit = {
+    description = "Set Power Limit for NVIDIA GPUs";
+    wantedBy = [ "multi-user.target" ];
+    path = [ config.hardware.nvidia.package ];
+    script = ''
+      nvidia-smi -pl 125
+    '';
+  };
 }
