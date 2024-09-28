d928343f17255efc995fd1e25a12876033e5156c
Lan Tian <xuyh0120@outlook.com>
2024-09-28 16:14:06 -0700

    hosts(lt-home-vm): move peerbanhelper into netns

--- hosts/lt-home-vm/media-center.nix
+++ hosts/lt-home-vm/media-center.nix
@@ -48,6 +48,7 @@ in
   systemd.services.bitmagnet-http = netns.bind { };
   systemd.services.bitmagnet-queue = netns.bind { };
   systemd.services.bitmagnet-dht = netns.bind { };
+  systemd.services.peerbanhelper = netns.bind { };
 
   systemd.services.radarr = {
     after = [ "mnt-storage.mount" ];
@@ -101,6 +102,8 @@ in
     "qbittorrent.localhost".locations."/".proxyPass = lib.mkForce "http://${netns.ipv4}:${LT.portStr.qBitTorrent.WebUI}";
     "bitmagnet.${config.networking.hostName}.xuyh0120.win".locations."/".proxyPass = lib.mkForce "http://${netns.ipv4}:${LT.portStr.Bitmagnet}";
     "bitmagnet.localhost".locations."/".proxyPass = lib.mkForce "http://${netns.ipv4}:${LT.portStr.Bitmagnet}";
+    "peerbanhelper.${config.networking.hostName}.xuyh0120.win".locations."/".proxyPass = lib.mkForce "http://${netns.ipv4}:${LT.portStr.PeerBanHelper}";
+    "peerbanhelper.localhost".locations."/".proxyPass = lib.mkForce "http://${netns.ipv4}:${LT.portStr.PeerBanHelper}";
   };
 
   ########################################
