9051368d707d78c72bd2658291f9d6c6b3eb657a
Lan Tian <xuyh0120@outlook.com>
2024-10-05 23:32:01 -0700

    Reapply "general: set system language to Chinese"
    This reverts commit 93824b87c8d75a52c8de05e3718c42f965b4f47a.

--- nixos/minimal-components/environment.nix
+++ nixos/minimal-components/environment.nix
@@ -66,7 +66,7 @@ in
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.supportedLocales = [
     "C.UTF-8/UTF-8"
     "en_US.UTF-8/UTF-8"
