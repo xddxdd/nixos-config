a6078376ba6d4426f1a64d4e3550e81fb846dcfd
Lan Tian <xuyh0120@outlook.com>
2024-10-20 14:26:08 -0700

    hosts(hetzner-de): add

---
+++ hosts/hetzner-de/configuration.nix
@@ -0,0 +1,75 @@
+{
+  ...
+}:
+{
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+
+    # ../../nixos/optional-apps/attic.nix
+    # ../../nixos/optional-apps/bepasty.nix
+    # ../../nixos/optional-apps/bird-lg-go.nix
+    # ../../nixos/optional-apps/calibre-cops.nix
+    # ../../nixos/optional-apps/drone-ci.nix
+    # ../../nixos/optional-apps/gitea.nix
+    # ../../nixos/optional-apps/glauth.nix
+    # ../../nixos/optional-apps/grafana.nix
+    # ../../nixos/optional-apps/keycloak
+    # ../../nixos/optional-apps/lemmy.nix
+    # ../../nixos/optional-apps/matrix-synapse.nix
+    # ../../nixos/optional-apps/miniflux.nix
+    # ../../nixos/optional-apps/netbox.nix
+    # ../../nixos/optional-apps/nextcloud.nix
+    # ../../nixos/optional-apps/nginx-lab
+    # ../../nixos/optional-apps/nginx-private.nix
+    # ../../nixos/optional-apps/plausible
+    # ../../nixos/optional-apps/prometheus.nix
+    # ../../nixos/optional-apps/quassel.nix
+    # ../../nixos/optional-apps/rsshub.nix
+    # ../../nixos/optional-apps/step-ca.nix
+    # ../../nixos/optional-apps/tg-bot-cleaner-bot
+    # ../../nixos/optional-apps/vaultwarden.nix
+    # ../../nixos/optional-apps/waline.nix
+    # ../../nixos/optional-apps/yourls.nix
+
+    # ../../nixos/optional-cron-jobs/ssl-certs.nix
+    # ../../nixos/optional-cron-jobs/testssl.nix
+
+    # "${inputs.secrets}/nixos-hidden-module/11116c7374949a7a"
+    # "${inputs.secrets}/nixos-hidden-module/35c68fea6f2bde77"
+    # "${inputs.secrets}/nixos-hidden-module/ca877276fe06bd79"
+  ];
+
+  systemd.network.networks.eth0 = {
+    address = [
+      "159.69.30.238/24"
+      "2a01:4f8:c2c:8bbf::1/64"
+    ];
+    matchConfig.Name = "eth0";
+    networkConfig = {
+      IPv6AcceptRA = false;
+    };
+    routes = [
+      {
+        routeConfig = {
+          Gateway = "172.31.1.1";
+          GatewayOnLink = true;
+        };
+      }
+      {
+        routeConfig = {
+          Gateway = "fe80::1";
+          GatewayOnLink = true;
+        };
+      }
+    ];
+  };
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+}
