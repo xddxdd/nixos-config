3898e15db97a663fb78926d16fdb5903eaf13126
Lan Tian <xuyh0120@outlook.com>
2024-10-20 03:42:39 -0700

    dns: manage GCore GeoDNS directly from DNSControl

--- scripts/dnscontrol.nix
+++ scripts/dnscontrol.nix
@@ -5,7 +5,30 @@
   ...
 }:
 let
-  inherit (pkgs) dnscontrol;
+  # inherit (pkgs) dnscontrol;
+  dnscontrol = pkgs.buildGoModule rec {
+    pname = "dnscontrol";
+    version = "2b7756e51f0f3eac95af286fa19094f58554ec32";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "xddxdd";
+      repo = "dnscontrol";
+      rev = version;
+      sha256 = "sha256-50J89sELkSEgyzk1SUDMN6xsXwm7K2ZDidBg0/TaZMg=";
+    };
+
+    vendorHash = "sha256-6ePEgHVFPtkW+C57+cPLj5yc9YaCRKrnBFo2Y1pcglM=";
+
+    ldflags = [
+      "-s"
+      "-w"
+    ];
+
+    preCheck = ''
+      # requires network
+      rm pkg/spflib/flatten_test.go pkg/spflib/parse_test.go
+    '';
+  };
 in
 ''
   set -euxo pipefail
