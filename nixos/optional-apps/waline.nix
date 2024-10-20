cdd7dc857d670a55a063641b5247589a84588f51
Lan Tian <xuyh0120@outlook.com>
2024-10-20 16:14:22 -0700

    Revert "waline: run as systemd service instead of docker container"
    This reverts commit ec0abec458b9a5438co1e7cbbe89b2d19614d73351.

--- nixos/optional-apps/waline.nix
+++ nixos/optional-apps/waline.nix
@@ -1,6 +1,4 @@
 {
-  pkgs,
-  lib,
   LT,
   config,
   inputs,
@@ -26,44 +24,15 @@
     };
   };
 
-  systemd.services.waline = {
-    description = "Waline Comment System";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network-online.target" ];
-    requires = [ "network-online.target" ];
-
-    preStart = ''
-      ${pkgs.yarn}/bin/yarn add @waline/vercel
-
-      mkdir -p node_modules/@waline/vercel/src/config
-      cat > node_modules/@waline/vercel/src/config/config.production.js <<EOF
-      module.exports = {
-        host: "${LT.this.ltnet.IPv4}",
-        port: ${LT.portStr.Waline},
-      };
-      EOF
-    '';
-
-    serviceConfig = LT.serviceHarden // {
-      EnvironmentFile = config.age.secrets.waline-env.path;
-      ExecStart = "${pkgs.nodejs}/bin/node node_modules/@waline/vercel/vanilla.js";
-
-      Restart = "always";
-      RestartSec = "3";
-      TimeoutStartSec = "900";
-
-      CacheDirectory = "waline";
-      WorkingDirectory = "/var/cache/waline";
-
-      User = "waline";
-      Group = "waline";
-      MemoryDenyWriteExecute = lib.mkForce false;
+  virtualisation.oci-containers.containers = {
+    waline = {
+      extraOptions = [
+        "--pull"
+        "always"
+      ];
+      image = "lizheming/waline";
+      ports = [ "${LT.this.ltnet.IPv4}:${LT.portStr.Waline}:8360" ];
+      environmentFiles = [ config.age.secrets.waline-env.path ];
     };
   };
-
-  users.users.waline = {
-    group = "waline";
-    isSystemUser = true;
-  };
-  users.groups.waline = { };
 }
