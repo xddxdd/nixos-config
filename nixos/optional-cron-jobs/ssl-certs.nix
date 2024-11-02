dc124a9182b7913daa9b60bb1ebdfc149f38d61b
Lan Tian <xuyh0120@outlook.com>
2024-11-02 00:37:47 -0700

    ssl-certs: periodically run acme.sh-auto

--- nixos/optional-cron-jobs/ssl-certs.nix
+++ nixos/optional-cron-jobs/ssl-certs.nix
@@ -1,5 +1,55 @@
-{ pkgs, LT, ... }:
 {
+  pkgs,
+  lib,
+  LT,
+  ...
+}:
+let
+  homeDir = "/nix/persistent/sync-servers/acme.sh";
+
+  subdomains = builtins.concatStringsSep " " (lib.mapAttrsToList (n: _v: n) LT.hosts);
+
+  acme-sh-wrapped = pkgs.stdenv.mkDerivation {
+    name = "acme-sh";
+    version = "0.0.1";
+
+    phases = [ "installPhase" ];
+    buildInputs = [ pkgs.makeWrapper ];
+    installPhase = ''
+      makeWrapper "${pkgs.acme-sh}/bin/acme.sh" "$out/bin/acme.sh" \
+        --argv0 "acme.sh" \
+        --run "rm -rf ${homeDir}/deploy" \
+        --run "ln -sf ${pkgs.acme-sh}/libexec/deploy ${homeDir}/deploy" \
+        --run "rm -rf ${homeDir}/dnsapi" \
+        --run "ln -sf ${pkgs.acme-sh}/libexec/dnsapi ${homeDir}/dnsapi" \
+        --run "rm -rf ${homeDir}/notify" \
+        --run "ln -sf ${pkgs.acme-sh}/libexec/notify ${homeDir}/notify" \
+        --add-flags "--home ${homeDir}" \
+        --add-flags "--auto-upgrade 0"
+    '';
+  };
+
+  acme-sh-auto = pkgs.writeShellScriptBin "acme.sh-auto" ''
+    set -x
+
+    for SUBDOMAIN in ${subdomains}; do
+      if [ ! -f "${homeDir}/$SUBDOMAIN.xuyh0120.win/$SUBDOMAIN.xuyh0120.win.cer" ]; then
+        acme.sh --issue --dns dns_gcore -d $SUBDOMAIN.xuyh0120.win -d \*.$SUBDOMAIN.xuyh0120.win -k 2048
+      fi
+      if [ ! -f "${homeDir}/$SUBDOMAIN.xuyh0120.win_ecc/$SUBDOMAIN.xuyh0120.win.cer" ]; then
+        acme.sh --issue --dns dns_gcore -d $SUBDOMAIN.xuyh0120.win -d \*.$SUBDOMAIN.xuyh0120.win -k ec-256
+      fi
+    done
+
+    find ${homeDir}/ -type f -exec chmod 644 -- {} +
+  '';
+in
+{
+  environment.systemPackages = [
+    acme-sh-auto
+    acme-sh-wrapped
+  ];
+
   systemd.services.ssl-certs = {
     serviceConfig = LT.serviceHarden // {
       Type = "oneshot";
@@ -7,10 +57,8 @@
     };
     unitConfig.OnFailure = "notify-email-fail@%n.service";
     script = ''
-      exec ${pkgs.acme-sh}/bin/acme.sh \
-        --config-home /nix/persistent/sync-servers/acme.sh \
-        --auto-upgrade 0 \
-        --cron
+      ${pkgs.acme-sh-wrapped}/bin/acme.sh --cron
+      ${pkgs.acme-sh-auto}/bin/acme.sh-auto
     '';
   };
 
