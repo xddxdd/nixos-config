9434a37879bd1763d38fc7370312bfff5ec6d1b6
Lan Tian <xuyh0120@outlook.com>
2024-11-10 00:33:13 -0800

    hosts(pve-epyc): workaround issue of setting fan to max on startup

--- hosts/pve-epyc/smfc/default.nix
+++ hosts/pve-epyc/smfc/default.nix
@@ -3,11 +3,17 @@
   systemd.services.smfc = {
     description = "Super Micro Fan Control";
     wantedBy = [ "multi-user.target" ];
+
+    script = ''
+      # Workaround issue of setting fan to max on startup
+      timeout 10 ${pkgs.nur-xddxdd.smfc}/bin/smfc -c ${./smfc.conf} -l 3 || true
+      exec ${pkgs.nur-xddxdd.smfc}/bin/smfc -c ${./smfc.conf} -l 3
+    '';
+
     serviceConfig = {
       Type = "simple";
       Restart = "always";
       RestartSec = "3";
-      ExecStart = "${pkgs.nur-xddxdd.smfc}/bin/smfc -c ${./smfc.conf} -l 3";
 
       ProcSubset = "all";
       ProtectKernelTunables = false;
