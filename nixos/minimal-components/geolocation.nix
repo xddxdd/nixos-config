4fca17a0264a79338d643a191591920d489f2e3d
Lan Tian <xuyh0120@outlook.com>
2024-11-16 20:36:42 -0800

    general: disable geoclue for now due to Mozilla location service shutdown

--- nixos/minimal-components/geolocation.nix
+++ nixos/minimal-components/geolocation.nix
@@ -1,17 +1,27 @@
 {
   LT,
+  config,
   ...
 }:
 {
   location = {
-    provider = if LT.this.hasTag LT.tags.client then "geoclue2" else "manual";
+    # provider = if LT.this.hasTag LT.tags.client then "geoclue2" else "manual";
+    provider = "manual";
     latitude = LT.this.city.lat;
     longitude = LT.this.city.lng;
   };
 
   # Default geoclue2 server from Mozilla has been shut down
   services.geoclue2 = {
-    enable = LT.this.hasTag LT.tags.client;
+    enable = config.location.provider == "geoclue2";
+    enableDemoAgent = true;
+
+    enable3G = true;
+    enableCDMA = true;
+    enableModemGPS = true;
+    enableNmea = true;
+    enableWifi = true;
+
     geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
     submitData = false;
     submissionUrl = "https://api.beacondb.net/v2/geosubmit";
