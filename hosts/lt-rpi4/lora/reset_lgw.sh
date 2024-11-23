ffa3e797307fedec91f88ab0f37f68f618072e20
Lan Tian <xuyh0120@outlook.com>
2024-11-23 02:29:46 -0800

    general: rerun formatter

--- hosts/lt-rpi4/lora/reset_lgw.sh
+++ hosts/lt-rpi4/lora/reset_lgw.sh
@@ -18,93 +18,93 @@ SX1261_RESET_PIN=22    # SX1261 reset (LBT / Spectral Scan)
 AD5338R_RESET_PIN=13   # AD5338R reset (full-duplex CN490 reference design)
 
 WAIT_GPIO() {
-    sleep 0.1
+  sleep 0.1
 }
 
 init() {
-    # setup GPIOs
-    echo "$SX1302_RESET_PIN" >/sys/class/gpio/export
-    WAIT_GPIO
-    echo "$SX1261_RESET_PIN" >/sys/class/gpio/export
-    WAIT_GPIO
-    echo "$SX1302_POWER_EN_PIN" >/sys/class/gpio/export
-    WAIT_GPIO
-    echo "$AD5338R_RESET_PIN" >/sys/class/gpio/export
-    WAIT_GPIO
+  # setup GPIOs
+  echo "$SX1302_RESET_PIN" >/sys/class/gpio/export
+  WAIT_GPIO
+  echo "$SX1261_RESET_PIN" >/sys/class/gpio/export
+  WAIT_GPIO
+  echo "$SX1302_POWER_EN_PIN" >/sys/class/gpio/export
+  WAIT_GPIO
+  echo "$AD5338R_RESET_PIN" >/sys/class/gpio/export
+  WAIT_GPIO
 
-    # set GPIOs as output
-    echo "out" >/sys/class/gpio/gpio$SX1302_RESET_PIN/direction
-    WAIT_GPIO
-    echo "out" >/sys/class/gpio/gpio$SX1261_RESET_PIN/direction
-    WAIT_GPIO
-    echo "out" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction
-    WAIT_GPIO
-    echo "out" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/direction
-    WAIT_GPIO
+  # set GPIOs as output
+  echo "out" >/sys/class/gpio/gpio$SX1302_RESET_PIN/direction
+  WAIT_GPIO
+  echo "out" >/sys/class/gpio/gpio$SX1261_RESET_PIN/direction
+  WAIT_GPIO
+  echo "out" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction
+  WAIT_GPIO
+  echo "out" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/direction
+  WAIT_GPIO
 }
 
 reset() {
-    echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
-    echo "SX1261 reset through GPIO$SX1302_RESET_PIN..."
-    echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
-    echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
+  echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
+  echo "SX1261 reset through GPIO$SX1302_RESET_PIN..."
+  echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
+  echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
 
-    # write output for SX1302 CoreCell power_enable and reset
-    echo "0" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value
-    WAIT_GPIO
-    echo "1" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value
-    WAIT_GPIO
+  # write output for SX1302 CoreCell power_enable and reset
+  echo "0" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value
+  WAIT_GPIO
+  echo "1" >/sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value
+  WAIT_GPIO
 
-    echo "1" >/sys/class/gpio/gpio$SX1302_RESET_PIN/value
-    WAIT_GPIO
-    echo "0" >/sys/class/gpio/gpio$SX1302_RESET_PIN/value
-    WAIT_GPIO
+  echo "1" >/sys/class/gpio/gpio$SX1302_RESET_PIN/value
+  WAIT_GPIO
+  echo "0" >/sys/class/gpio/gpio$SX1302_RESET_PIN/value
+  WAIT_GPIO
 
-    echo "0" >/sys/class/gpio/gpio$SX1261_RESET_PIN/value
-    WAIT_GPIO
-    echo "1" >/sys/class/gpio/gpio$SX1261_RESET_PIN/value
-    WAIT_GPIO
+  echo "0" >/sys/class/gpio/gpio$SX1261_RESET_PIN/value
+  WAIT_GPIO
+  echo "1" >/sys/class/gpio/gpio$SX1261_RESET_PIN/value
+  WAIT_GPIO
 
-    echo "0" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/value
-    WAIT_GPIO
-    echo "1" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/value
-    WAIT_GPIO
+  echo "0" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/value
+  WAIT_GPIO
+  echo "1" >/sys/class/gpio/gpio$AD5338R_RESET_PIN/value
+  WAIT_GPIO
 }
 
 term() {
-    # cleanup all GPIOs
-    if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]; then
-        echo "$SX1302_RESET_PIN" >/sys/class/gpio/unexport
-        WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$SX1261_RESET_PIN ]; then
-        echo "$SX1261_RESET_PIN" >/sys/class/gpio/unexport
-        WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]; then
-        echo "$SX1302_POWER_EN_PIN" >/sys/class/gpio/unexport
-        WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$AD5338R_RESET_PIN ]; then
-        echo "$AD5338R_RESET_PIN" >/sys/class/gpio/unexport
-        WAIT_GPIO
-    fi
+  # cleanup all GPIOs
+  if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]; then
+    echo "$SX1302_RESET_PIN" >/sys/class/gpio/unexport
+    WAIT_GPIO
+  fi
+  if [ -d /sys/class/gpio/gpio$SX1261_RESET_PIN ]; then
+    echo "$SX1261_RESET_PIN" >/sys/class/gpio/unexport
+    WAIT_GPIO
+  fi
+  if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]; then
+    echo "$SX1302_POWER_EN_PIN" >/sys/class/gpio/unexport
+    WAIT_GPIO
+  fi
+  if [ -d /sys/class/gpio/gpio$AD5338R_RESET_PIN ]; then
+    echo "$AD5338R_RESET_PIN" >/sys/class/gpio/unexport
+    WAIT_GPIO
+  fi
 }
 
 case "$1" in
 start)
-    term # just in case
-    init
-    reset
-    ;;
+  term # just in case
+  init
+  reset
+  ;;
 stop)
-    reset
-    term
-    ;;
+  reset
+  term
+  ;;
 *)
-    echo "Usage: $0 {start|stop}"
-    exit 1
-    ;;
+  echo "Usage: $0 {start|stop}"
+  exit 1
+  ;;
 esac
 
 exit 0
--- nixos/pve-components/isolcpus-affinity-fix/isolcpus-affinity-fix.sh
+++ nixos/pve-components/isolcpus-affinity-fix/isolcpus-affinity-fix.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 while true; do
-    for VM in $(find /etc/pve/qemu-server/ -name \*.conf); do
-        grep "^affinity: " "$VM" >/dev/null 2>/dev/null || continue
-        VMFILE=$(basename "$VM")
-        VMID=${VMFILE%.conf}
-        VMPID=$(cat "/run/qemu-server/$VMID.pid") || continue
-        echo "Set SCHED_RR for VM ${VMID} PID ${VMPID}"
-        chrt --rr -a -p 1 "$VMPID"
-    done
-    echo "Wait 10s for next cycle..."
-    sleep 10
+  for VM in $(find /etc/pve/qemu-server/ -name \*.conf); do
+    grep "^affinity: " "$VM" >/dev/null 2>/dev/null || continue
+    VMFILE=$(basename "$VM")
+    VMID=${VMFILE%.conf}
+    VMPID=$(cat "/run/qemu-server/$VMID.pid") || continue
+    echo "Set SCHED_RR for VM ${VMID} PID ${VMPID}"
+    chrt --rr -a -p 1 "$VMPID"
+  done
+  echo "Wait 10s for next cycle..."
+  sleep 10
 done
