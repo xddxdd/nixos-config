a17a9bb44f23c2619adfdb138189f735249b51ba
Lan Tian <xuyh0120@outlook.com>
2024-11-23 04:37:10 -0800

    Revert "ssh: move keys to /nix/persistent"
    This reverts commit b6c5482790fe1045b872a3cb6636c0265f5426b5.

--- nixos/minimal-components/impermanence.nix
+++ nixos/minimal-components/impermanence.nix
@@ -13,6 +13,10 @@ in
     ];
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
     ];
   };
 
