82eabca7b97117c30fa2de9cd2542f36e54be928
Lan Tian <xuyh0120@outlook.com>
2024-12-19 02:00:35 -0800

    networking: add public IPs of peers to zerotier config

--- nixos/minimal-apps/zerotier/default.nix
+++ nixos/minimal-apps/zerotier/default.nix
@@ -7,16 +7,6 @@
 let
   whitelistToBlacklist = pkgs.callPackage ./whitelist_to_blacklist.nix { };
 
-  configFile = builtins.toJSON {
-    settings = {
-      interfacePrefixBlacklist = whitelistToBlacklist (
-        # Do not try to use network namespaces
-        with LT.constants.interfacePrefixes; (builtins.filter (v: v != "ns") (WAN ++ LAN))
-      );
-      softwareUpdate = "disable";
-    };
-  };
-
   ltnet = "91450bd87b000001";
 
   zerotier-default = pkgs.writeShellScriptBin "zerotier-default" ''
@@ -34,13 +24,27 @@ in
   services.zerotierone = {
     enable = true;
     joinNetworks = [ ltnet ];
+    localConf = {
+      virtual = lib.mapAttrs' (
+        _k: v:
+        lib.nameValuePair v.zerotier {
+          try =
+            (lib.optionals (v.public.IPv4 != "") [ "${v.public.IPv4}/9993" ])
+            ++ (lib.optionals (v.public.IPv6 != "") [ "${v.public.IPv6}/9993" ])
+            ++ (lib.optionals (v.public.IPv6Alt != "") [ "${v.public.IPv6Alt}/9993" ]);
+        }
+      ) (lib.filterAttrs (_k: v: v.zerotier != null) LT.otherHosts);
+      settings = {
+        interfacePrefixBlacklist = whitelistToBlacklist (
+          # Do not try to use network namespaces
+          with LT.constants.interfacePrefixes; (builtins.filter (v: v != "ns") (WAN ++ LAN))
+        );
+        softwareUpdate = "disable";
+      };
+    };
   };
 
   systemd.services.zerotierone = {
-    preStart = ''
-      mkdir -p /var/lib/zerotier-one
-      cat ${pkgs.writeText "local.conf" configFile} > /var/lib/zerotier-one/local.conf
-    '';
     serviceConfig = LT.serviceHarden // {
       AmbientCapabilities = [ "CAP_NET_ADMIN" ];
       CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
