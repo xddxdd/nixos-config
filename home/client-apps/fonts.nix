e1f2d52b3f6c2b5b08fda99cf6360b9cf9f0dd64
Lan Tian <xuyh0120@outlook.com>
2025-01-02 00:47:59 -0800

    general: use colmena direct flake eval for potentially faster evaluation

--- home/client-apps/fonts.nix
+++ home/client-apps/fonts.nix
@@ -1,6 +1,6 @@
-{ osConfig, ... }:
+{ osConfig, config, ... }:
 {
   # To make Steam recognize Chinese fonts
   fonts.fontconfig.defaultFonts = osConfig.fonts.fontconfig.defaultFonts;
-  xdg.dataFile."fonts".source = "/run/current-system/sw/share/X11/fonts";
+  xdg.dataFile."fonts".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/X11/fonts";
 }
