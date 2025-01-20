cdea66a074fbae42103bb9c1df3b6eaa97efbd91
Lan Tian <xuyh0120@outlook.com>
2025-01-20 12:33:27 -0800

    crowdsec: add attack detection scenarios

--- nixos/server-apps/crowdsec.nix
+++ nixos/server-apps/crowdsec.nix
@@ -62,9 +62,44 @@
     };
   };
 
-  systemd.services.crowdsec.postStart = ''
-    cscli bouncers add cs-firewall-bouncer --key cs-firewall-bouncer || true
-  '';
+  systemd.services.crowdsec = {
+    preStart = ''
+      cscli hub upgrade
+
+      # Asterisk
+      cscli parsers install crowdsecurity/asterisk-logs
+      cscli scenarios install crowdsecurity/asterisk_bf
+      cscli scenarios install crowdsecurity/asterisk_user_enum
+
+      # Nginx
+      cscli parsers install crowdsecurity/nginx-logs
+      cscli scenarios install crowdsecurity/http-admin-interface-probing
+      cscli scenarios install crowdsecurity/http-backdoors-attempts
+      cscli scenarios install crowdsecurity/http-bf-wordpress_bf_xmlrpc
+      cscli scenarios install crowdsecurity/http-cve-2021-42013
+      cscli scenarios install crowdsecurity/http-cve-probing
+      cscli scenarios install crowdsecurity/http-generic-bf
+      cscli scenarios install crowdsecurity/http-open-proxy
+      cscli scenarios install crowdsecurity/http-path-traversal-probing
+      cscli scenarios install crowdsecurity/http-sensitive-files
+      cscli scenarios install crowdsecurity/http-sqli-probing
+      cscli scenarios install crowdsecurity/http-wordpress_user-enum
+      cscli scenarios install crowdsecurity/http-wordpress_wpconfig
+      cscli scenarios install crowdsecurity/http-wordpress-scan
+      cscli scenarios install crowdsecurity/http-xss-probing
+
+      # SSH
+      cscli parsers install crowdsecurity/sshd-logs
+      cscli parsers install crowdsecurity/sshd-success-logs
+      cscli scenarios install crowdsecurity/ssh-bf
+      cscli scenarios install crowdsecurity/ssh-cve-2024-6387
+      cscli scenarios install crowdsecurity/ssh-slow-bf
+    '';
+    postStart = ''
+      cscli bouncers add cs-firewall-bouncer --key cs-firewall-bouncer || true
+    '';
+    serviceConfig.StateDirectory = "crowdsec";
+  };
 
   systemd.services.crowdsec-firewall-bouncer = {
     after = [ "crowdsec.service" ];
