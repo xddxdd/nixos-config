91644090598514d445a0b67a97df99cbeacad2d5
Lan Tian <xuyh0120@outlook.com>
2025-01-20 16:12:02 -0800

    crowdsec: fix startup

--- nixos/server-apps/crowdsec.nix
+++ nixos/server-apps/crowdsec.nix
@@ -1,6 +1,8 @@
 {
   config,
   LT,
+  lib,
+  pkgs,
   inputs,
   ...
 }:
@@ -63,46 +65,62 @@
   };
 
   systemd.services.crowdsec = {
-    preStart = ''
-      cscli hub upgrade
-
-      # Asterisk
-      cscli parsers install crowdsecurity/asterisk-logs
-      cscli scenarios install crowdsecurity/asterisk_bf
-      cscli scenarios install crowdsecurity/asterisk_user_enum
-
-      # Nginx
-      cscli parsers install crowdsecurity/nginx-logs
-      cscli scenarios install crowdsecurity/http-admin-interface-probing
-      cscli scenarios install crowdsecurity/http-backdoors-attempts
-      cscli scenarios install crowdsecurity/http-bf-wordpress_bf_xmlrpc
-      cscli scenarios install crowdsecurity/http-cve-2021-42013
-      cscli scenarios install crowdsecurity/http-cve-probing
-      cscli scenarios install crowdsecurity/http-generic-bf
-      cscli scenarios install crowdsecurity/http-open-proxy
-      cscli scenarios install crowdsecurity/http-path-traversal-probing
-      cscli scenarios install crowdsecurity/http-sensitive-files
-      cscli scenarios install crowdsecurity/http-sqli-probing
-      cscli scenarios install crowdsecurity/http-wordpress_user-enum
-      cscli scenarios install crowdsecurity/http-wordpress_wpconfig
-      cscli scenarios install crowdsecurity/http-wordpress-scan
-      cscli scenarios install crowdsecurity/http-xss-probing
-
-      # SSH
-      cscli parsers install crowdsecurity/sshd-logs
-      cscli parsers install crowdsecurity/sshd-success-logs
-      cscli scenarios install crowdsecurity/ssh-bf
-      cscli scenarios install crowdsecurity/ssh-cve-2024-6387
-      cscli scenarios install crowdsecurity/ssh-slow-bf
-    '';
     postStart = ''
+      while ! cscli lapi status; do
+        echo "Waiting for CrowdSec daemon to be ready"
+        sleep 5
+      done
+
       cscli bouncers add cs-firewall-bouncer --key cs-firewall-bouncer || true
     '';
-    serviceConfig.StateDirectory = "crowdsec";
+    serviceConfig = {
+      ExecStartPre = lib.mkAfter [
+        (pkgs.writeShellScript "crowdsec-packages" ''
+          cscli hub upgrade
+
+          # Asterisk
+          cscli parsers install crowdsecurity/asterisk-logs
+          cscli scenarios install crowdsecurity/asterisk_bf
+          cscli scenarios install crowdsecurity/asterisk_user_enum
+
+          # Nginx
+          cscli parsers install crowdsecurity/nginx-logs
+          cscli scenarios install crowdsecurity/http-admin-interface-probing
+          cscli scenarios install crowdsecurity/http-backdoors-attempts
+          cscli scenarios install crowdsecurity/http-bf-wordpress_bf_xmlrpc
+          cscli scenarios install crowdsecurity/http-cve-2021-42013
+          cscli scenarios install crowdsecurity/http-cve-probing
+          cscli scenarios install crowdsecurity/http-generic-bf
+          cscli scenarios install crowdsecurity/http-open-proxy
+          cscli scenarios install crowdsecurity/http-path-traversal-probing
+          cscli scenarios install crowdsecurity/http-sensitive-files
+          cscli scenarios install crowdsecurity/http-sqli-probing
+          cscli scenarios install crowdsecurity/http-wordpress_user-enum
+          cscli scenarios install crowdsecurity/http-wordpress_wpconfig
+          cscli scenarios install crowdsecurity/http-wordpress-scan
+          cscli scenarios install crowdsecurity/http-xss-probing
+
+          # SSH
+          cscli parsers install crowdsecurity/sshd-logs
+          cscli parsers install crowdsecurity/sshd-success-logs
+          cscli scenarios install crowdsecurity/ssh-bf
+          cscli scenarios install crowdsecurity/ssh-cve-2024-6387
+          cscli scenarios install crowdsecurity/ssh-slow-bf
+        '')
+      ];
+
+      StateDirectory = "crowdsec";
+      Restart = lib.mkForce "always";
+      RestartSec = lib.mkForce "5";
+    };
   };
 
   systemd.services.crowdsec-firewall-bouncer = {
     after = [ "crowdsec.service" ];
     requires = [ "crowdsec.service" ];
+    serviceConfig = {
+      Restart = lib.mkForce "always";
+      RestartSec = lib.mkForce "5";
+    };
   };
 }
