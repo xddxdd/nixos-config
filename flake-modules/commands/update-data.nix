60f6e32f2d201e7e6464231c1ef4b11e8700070c
Lan Tian <xuyh0120@outlook.com>
2025-02-02 16:43:36 +0800

    ci: auto update data files

---
+++ flake-modules/commands/update-data.nix
@@ -0,0 +1,20 @@
+{
+  inputs,
+  python3,
+  ...
+}:
+let
+  py = python3.withPackages (p: with p; [ requests ]);
+in
+''
+  export SECRET_BASE=${inputs.secrets}
+
+  for S in $(find . -name update.sh); do
+    echo "Executing $S"
+    bash "$S"
+  done
+  for S in $(find . -name update.py); do
+    echo "Executing $S"
+    ${py}/bin/python3 "$S"
+  done
+''
