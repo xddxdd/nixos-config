0beb873081262421ae38189dbb11684f0ed828a7
Lan Tian <xuyh0120@outlook.com>
2025-02-16 00:14:01 -0800

    iodine: relax source IP restriction

--- nixos/optional-apps/iodine.nix
+++ nixos/optional-apps/iodine.nix
@@ -4,7 +4,7 @@
     enable = true;
     ip = "203.0.113.1/24";
     domain = "dt.56631131.xyz";
-    extraConfig = "-l ${LT.this.ltnet.IPv4} -p ${LT.portStr.Iodine}";
+    extraConfig = "-l ${LT.this.ltnet.IPv4} -p ${LT.portStr.Iodine} -c -n ${LT.this.public.IPv4}";
     passwordFile = config.age.secrets.default-pw.path;
   };
 }
