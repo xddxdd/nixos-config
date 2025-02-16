831f0255ec2e6faaceeab0ef80d6bee8aaad9d73
Lan Tian <xuyh0120@outlook.com>
2025-02-16 00:06:14 -0800

    iodine: fix ip address

--- nixos/optional-apps/iodine.nix
+++ nixos/optional-apps/iodine.nix
@@ -2,7 +2,7 @@
 {
   services.iodine.server = {
     enable = true;
-    ip = "203.0.113.0/24";
+    ip = "203.0.113.1/24";
     domain = "dt.56631131.xyz";
     extraConfig = "-l ${LT.this.ltnet.IPv4} -p ${LT.portStr.Iodine}";
     passwordFile = config.age.secrets.default-pw.path;
