e0c9eafe0884e4123a115189f354a8866e4b55a9
Lan Tian <xuyh0120@outlook.com>
2025-02-18 01:33:28 -0800

    hosts(pve-epyc): dynamically control GPU fan speed

--- hosts/pve-epyc/smfc/default.nix
+++ hosts/pve-epyc/smfc/default.nix
@@ -1,13 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   systemd.services.smfc = {
     description = "Super Micro Fan Control";
     wantedBy = [ "multi-user.target" ];
+    after = [ "nvidia-tempmon.service" ];
+    requires = [ "nvidia-tempmon.service" ];
+    path = with pkgs; [
+      python3
+      ipmitool
+    ];
 
     script = ''
       # Workaround issue of setting fan to max on startup
-      timeout 10 ${pkgs.nur-xddxdd.smfc}/bin/smfc -c ${./smfc.conf} -l 3 || true
-      exec ${pkgs.nur-xddxdd.smfc}/bin/smfc -c ${./smfc.conf} -l 3
+      timeout 10 python3 ${./smfc.py} -c ${./smfc.conf} -l 3 || true
+      exec python3 ${./smfc.py} -c ${./smfc.conf} -l 3
     '';
 
     serviceConfig = {
@@ -17,6 +23,30 @@
 
       ProcSubset = "all";
       ProtectKernelTunables = false;
+    };
+  };
+
+  systemd.services.nvidia-tempmon = {
+    description = "Monitor NVIDIA GPU temperature";
+    wantedBy = [ "multi-user.target" ];
+    path = [
+      config.hardware.nvidia.package
+    ];
+    script = ''
+      while true; do
+        for i in $(seq 0 1); do
+          echo $(nvidia-smi -q -i "$i" | grep "GPU Current Temp" | grep -E -o "[0-9]+" | sort -rn | head -n1)000 > /run/nvidia-tempmon/gpu$i
+        done
+        sleep 2
+      done
+    '';
+
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "3";
+
+      RuntimeDirectory = "nvidia-tempmon";
     };
   };
 }
