8ceee5ed1f5b7f41dddf19e9f99335329b066d12
Lan Tian <xuyh0120@outlook.com>
2025-02-19 11:13:48 -0800

    general: add env vars to disable tracking for CLIs

---
+++ nixos/minimal-components/do-not-track.nix
@@ -0,0 +1,12 @@
+_: {
+  # https://consoledonottrack.com/
+  environment.variables = {
+    DO_NOT_TRACK = "1";
+    GATSBY_TELEMETRY_DISABLED = "1";
+    HOMEBREW_NO_ANALYTICS = "1";
+    STNOUPGRADE = "1";
+    DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+    SAM_CLI_TELEMETRY = "0";
+    AZURE_CORE_COLLECT_TELEMETRY = "0";
+  };
+}
