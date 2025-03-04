00374e9ec8e561d0ece50d248ccad693d794774c
Lan Tian <xuyh0120@outlook.com>
2025-03-03 21:01:44 -0800

    gui: auto adapt to block wine desktop entries

--- home/client-apps/block-wine-desktop-entries.nix
+++ home/client-apps/block-wine-desktop-entries.nix
@@ -1,27 +1,12 @@
-_:
-let
-  filenames = [
-    "wine-extension-chm.desktop"
-    "wine-extension-hlp.desktop"
-    "wine-extension-htm.desktop"
-    "wine-extension-html.desktop"
-    "wine-extension-ini.desktop"
-    "wine-extension-msp.desktop"
-    "wine-extension-pdf.desktop"
-    "wine-extension-rtf.desktop"
-    "wine-extension-txt.desktop"
-    "wine-extension-vbs.desktop"
-    "wine-extension-wri.desktop"
-    "wine-extension-xml.desktop"
-  ];
-in
-{
-  home.activation.block-wine-desktop-entries = builtins.concatStringsSep "\n" (
-    builtins.map (f: ''
-      mkdir -p "$HOME/.local/share/applications"
-      rm -f "$HOME/.local/share/applications/${f}"
-      touch "$HOME/.local/share/applications/${f}"
-      chmod 000 "$HOME/.local/share/applications/${f}"
-    '') filenames
-  );
+_: {
+  home.activation.block-wine-desktop-entries = ''
+    mkdir -p "$HOME/.local/share/applications"
+    shopt -s nullglob
+    for F in $HOME/.local/share/applications/wine-extension-*.desktop; do
+      rm -f "$F"
+      touch "$F"
+      chmod 000 "$F"
+    done
+    shopt -u nullglob
+  '';
 }
