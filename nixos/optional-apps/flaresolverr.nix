a17d4a719408b3f95633d17f345890d0f37f4145
Lan Tian <xuyh0120@outlook.com>
2025-03-05 00:22:27 -0800

    Revert "flaresolverr: enable logging"
    This reverts commit 72ac7dfab446be006c7d365b079ae461d94347d3.

--- nixos/optional-apps/flaresolverr.nix
+++ nixos/optional-apps/flaresolverr.nix
@@ -13,6 +13,7 @@
 
   systemd.services.flaresolverr.environment = {
     HOST = "127.0.0.1";
+    LOG_LEVEL = "warn";
     TZ = config.time.timeZone;
     LANG = config.i18n.defaultLocale;
     TEST_URL = "https://www.example.com";
