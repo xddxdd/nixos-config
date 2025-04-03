d7acb0eaca5d5588539c0e31c2783fc2557d300a
Lan Tian <xuyh0120@outlook.com>
2025-04-02 23:34:55 -0700

    general: bump packages

--- nixos/hardware/zfs.nix
+++ nixos/hardware/zfs.nix
@@ -1,6 +1,7 @@
-{ LT, ... }:
+{ LT, pkgs, ... }:
 {
   boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.package = pkgs.zfs_2_3;
   boot.zfs.forceImportAll = true;
   preservation.preserveAt."/nix/persistent" = {
     directories = builtins.map LT.preservation.mkFolder [
