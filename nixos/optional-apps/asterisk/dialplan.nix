f4958d8e53b619c2f90b5e83232ade230a4373c5
Lan Tian <xuyh0120@outlook.com>
2025-04-13 01:03:40 -0700

    asterisk: generate HTML dialplan webpage

--- nixos/optional-apps/asterisk/dialplan.nix
+++ nixos/optional-apps/asterisk/dialplan.nix
@@ -3,6 +3,9 @@ let
   inherit (pkgs.callPackage ./common.nix args) prefixZeros;
   inherit (pkgs.callPackage ./local-devices.nix args) destLocalDialPlan;
   inherit (pkgs.callPackage ./musics.nix args) destMusicDialPlan;
+  inherit (pkgs.callPackage ./apps/astycrapper.nix args) dialAstyCrapperDescription;
+  inherit (pkgs.callPackage ./apps/beverly.nix args) dialBeverlyDescription;
+  inherit (pkgs.callPackage ./apps/lenny.nix args) dialLennyDescription;
 
   dialPlan = lib.mergeAttrsList [
     {
@@ -11,23 +14,46 @@ let
       "02XX" = "Conference room XX";
       "1999" = "Ring group (1000 & 1001 & 1003)";
       "2000" = "Random between all call bots";
-      "2001" = "Lenny";
-      "2002" =
-        "Jordan (Asty-crapper, https://web.archive.org/web/20110517174427/http://www.linuxsystems.com.au/astycrapper/)";
-      "2003" = "Beverly (https://worldofprankcalls.com/beverly/)";
+      "2001" = dialLennyDescription;
+      "2002" = dialAstyCrapperDescription;
+      "2003" = dialBeverlyDescription;
     }
     destLocalDialPlan
     destMusicDialPlan
   ];
 
-  dialPlanDir = pkgs.writeTextDir "index.txt" (
-    lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: "${prefixZeros 4 n}: ${v}") dialPlan)
+  dialPlanRows = lib.concatStrings (
+    lib.mapAttrsToList (
+      n: v:
+      let
+        number = prefixZeros 4 n;
+        numberLink = "<a href=\"sip:${number}@lantian.pub\">sip:${number}@lantian.pub</a>";
+      in
+      "<tr><td>${numberLink}</td><td>${v}</td></tr>"
+    ) dialPlan
   );
+
+  dialPlanDir = pkgs.writeTextDir "index.html" ''
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>lantian.pub SIP dial plan</title>
+      </head>
+      <body>
+        <table>
+          <thead>
+            <tr><td>Number</td><td>Description</td></tr>
+          </thead>
+          <tbody>${dialPlanRows}</tbody>
+        </table>
+      </body>
+    </html>
+  '';
 in
 {
   lantian.nginxVhosts."sip.lantian.pub" = {
     root = dialPlanDir;
-    locations."/".index = "index.txt";
     sslCertificate = "lantian.pub_ecc";
     noIndex.enable = true;
   };
