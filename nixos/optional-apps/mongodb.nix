ad56c22a5604663be429b85da3a10c297c07a9f8
Lan Tian <xuyh0120@outlook.com>
2025-04-26 23:21:45 -0700

    mongodb: wait until data folder is mounted

---
+++ nixos/optional-apps/mongodb.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, ... }:
+{
+  services.mongodb = {
+    enable = true;
+    bind_ip = "127.0.0.1";
+    package = pkgs.mongodb-ce;
+  };
+  environment.systemPackages = [ pkgs.mongosh ];
+  preservation.preserveAt."/nix/persistent" = {
+    directories = [
+      {
+        directory = config.services.mongodb.dbpath;
+        user = "mongodb";
+        group = "mongodb";
+      }
+    ];
+  };
+
+  systemd.services.mongodb = {
+    after = [ "var-db-mongodb.mount" ];
+    requires = [ "var-db-mongodb.mount" ];
+
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = 5;
+    };
+  };
+}
--- nixos/optional-apps/open5gs/default.nix
+++ nixos/optional-apps/open5gs/default.nix
@@ -1,23 +1,14 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./services.nix
     ./webui.nix
+    ../mongodb.nix
   ];
 
   environment.etc."freeDiameter".source = ./freeDiameter;
   environment.etc."open5gs".source = ./config;
   environment.etc."open5gs-pkg".source = pkgs.open5gs;
-
-  services.mongodb = {
-    enable = true;
-    bind_ip = "127.0.0.1";
-    package = pkgs.mongodb-ce;
-  };
-  environment.systemPackages = [ pkgs.mongosh ];
-  preservation.preserveAt."/nix/persistent" = {
-    directories = [ config.services.mongodb.dbpath ];
-  };
 
   systemd.network.netdevs.open5gs = {
     netdevConfig = {
