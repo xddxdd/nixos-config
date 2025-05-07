f30a71482270ed425fc0861a21ef325982d549d2
Lan Tian <xuyh0120@outlook.com>
2025-05-06 23:39:13 -0700

    gui: remove console color override to fix systemd boot log color

--- nixos/common-apps/shell.nix
+++ nixos/common-apps/shell.nix
@@ -5,25 +5,6 @@
   ...
 }:
 {
-  console.colors = [
-    "000000"
-    "ff5370"
-    "c3e88d"
-    "ffcb6b"
-    "82aaff"
-    "c792ea"
-    "89ddff"
-    "ffffff"
-    "545454"
-    "ff5370"
-    "c3e88d"
-    "ffcb6b"
-    "82aaff"
-    "c792ea"
-    "89ddff"
-    "ffffff"
-  ];
-
   environment.pathsToLink = [ "/share/zsh" ];
   environment.shells = [ pkgs.zsh ];
   environment.variables = {
