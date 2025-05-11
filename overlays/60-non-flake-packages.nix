92c38549d0fce1ed7e538f6fb61079d743dc71ea
Lan Tian <xuyh0120@outlook.com>
2025-05-10 23:36:43 -0700

    general: remove unused overlays

--- overlays/60-non-flake-packages.nix
+++ overlays/60-non-flake-packages.nix
@@ -1,20 +1,5 @@
 { inputs, ... }:
-final: _prev:
-let
-  sources = final.callPackage ../helpers/_sources/generated.nix { };
-in
-{
+final: _prev: {
   composer2nix = final.callPackage inputs.composer2nix { };
-
   dwarffs = inputs.dwarffs.packages."${final.system}".default;
-
-  web-compressor = final.rustPlatform.buildRustPackage {
-    inherit (sources.web-compressor) pname version src;
-    useFetchCargoVendor = true;
-
-    nativeBuildInputs = with final; [ cmake ];
-    buildInputs = with final; [ zlib-ng ];
-
-    cargoHash = "sha256-BmbYzdeBxLk+ToHlKF/1gCyROR1HSWZIRvXwswCnjEs=";
-  };
 }
