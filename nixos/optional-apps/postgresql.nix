7a67e89b00ca826ff0725f79d349ee9d30005293
Lan Tian <xuyh0120@outlook.com>
2025-05-12 20:57:40 -0700

    postgresql: remove phppgadmin

--- nixos/optional-apps/postgresql.nix
+++ nixos/optional-apps/postgresql.nix
@@ -2,8 +2,6 @@
   pkgs,
   lib,
   LT,
-  config,
-  inputs,
   ...
 }:
 {
@@ -26,65 +24,6 @@
   };
 
   systemd.services.postgresql.serviceConfig = LT.serviceHarden;
-
-  services.phpfpm.pools.pga = {
-    phpPackage = pkgs.php.withExtensions (
-      { enabled, all }:
-      with all;
-      enabled
-      ++ [
-        pdo
-        pdo_pgsql
-        pgsql
-      ]
-    );
-    inherit (config.services.nginx) user;
-    settings = {
-      "listen.owner" = config.services.nginx.user;
-      "pm" = "ondemand";
-      "pm.max_children" = "8";
-      "pm.process_idle_timeout" = "10s";
-      "pm.max_requests" = "1000";
-      "pm.status_path" = "/php-fpm-status.php";
-      "ping.path" = "/ping.php";
-      "ping.response" = "pong";
-      "request_terminate_timeout" = "300";
-    };
-  };
-
-  age.secrets.phppgadmin-conf = {
-    file = inputs.secrets + "/phppgadmin-conf.age";
-    owner = "nginx";
-    group = "nginx";
-  };
-  systemd.tmpfiles.rules = [
-    "L+ /etc/phppgadmin/config.inc.php - - - - ${config.age.secrets.phppgadmin-conf.path}"
-  ];
-  lantian.nginxVhosts = {
-    "pga.${config.networking.hostName}.xuyh0120.win" = {
-      root = pkgs.nur-xddxdd.phppgadmin;
-      locations = {
-        "/".index = "index.php";
-      };
-
-      phpfpmSocket = config.services.phpfpm.pools.pga.socket;
-      sslCertificate = "${config.networking.hostName}.xuyh0120.win_ecc";
-      noIndex.enable = true;
-    };
-    "pga.localhost" = {
-      listenHTTP.enable = true;
-      listenHTTPS.enable = false;
-
-      root = pkgs.nur-xddxdd.phppgadmin;
-      locations = {
-        "/".index = "index.php";
-      };
-
-      phpfpmSocket = config.services.phpfpm.pools.pga.socket;
-      noIndex.enable = true;
-      accessibleBy = "localhost";
-    };
-  };
 
   services.prometheus.exporters.postgres = {
     enable = true;
