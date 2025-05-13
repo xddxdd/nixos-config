f76bb4c8a7a9342ff57220b9bacd32ac326cf60f
Lan Tian <xuyh0120@outlook.com>
2025-05-12 23:59:42 -0700

    hosts(lt-home-vm, pve-epyc): let lt-home-vm manage GPU power

--- hosts/pve-epyc/hardware-configuration.nix
+++ hosts/pve-epyc/hardware-configuration.nix
@@ -51,14 +51,4 @@
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableRedistributableFirmware = true;
-
-  systemd.services.nvidia-power-limit = {
-    description = "Set Power Limit for NVIDIA GPUs";
-    wantedBy = [ "multi-user.target" ];
-    path = [ config.hardware.nvidia.package ];
-    script = ''
-      nvidia-smi -pl 125
-    '';
-    serviceConfig.Type = "oneshot";
-  };
 }
