c402cc5f40926197cb4100d2434ca3ce8b071be6
Lan Tian <xuyh0120@outlook.com>
2025-05-15 21:25:26 -0700

    nix: revert back to nix for nixbuild.net compatibility issues

--- nixos/minimal-components/nix.nix
+++ nixos/minimal-components/nix.nix
@@ -18,7 +18,7 @@
   };
 
   nix = {
-    package = pkgs.lixPackageSets.latest.lix or pkgs.lixVersions.latest;
+    package = pkgs.nixVersions.latest;
     extraOptions = ''
       !include ${config.age.secrets.nix-access-token.path}
     '';
