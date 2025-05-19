09c7b2a1da683512debc5a0e8749a916ff527728
Lan Tian <xuyh0120@outlook.com>
2025-05-18 23:32:32 -0700

    proxmox: remove unneeded extra paths

--- nixos/pve-components/proxmox.nix
+++ nixos/pve-components/proxmox.nix
@@ -1,22 +1,6 @@
-{ pkgs, config, ... }:
-let
-  extraPath =
-    with pkgs;
-    (
-      [
-        util-linux
-        swtpm
-      ]
-      ++ (lib.optionals (config.boot.supportedFilesystems.zfs or false) [
-        zfs
-      ])
-    );
-in
-{
+_: {
   services.proxmox-ve.enable = true;
 
-  systemd.services.pvedaemon.path = extraPath;
-  systemd.services.pve-guests.path = extraPath;
   systemd.services.pvescheduler.serviceConfig = {
     Restart = "always";
     RestartSec = 5;
