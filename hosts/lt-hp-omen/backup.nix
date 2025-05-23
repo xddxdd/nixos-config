da04ec96d2e899de500f5f99ff5b76335d452600
Lan Tian <xuyh0120@outlook.com>
2025-05-23 00:41:14 -0700

    hosts(lt-hp-omen): make backup script easily rerunnable

--- hosts/lt-hp-omen/backup.nix
+++ hosts/lt-hp-omen/backup.nix
@@ -3,8 +3,12 @@ let
   backupScript = pkgs.writeShellScriptBin "backup" ''
     set -x
     DATE=$(date "+%Y%m%d")
-    sudo btrfs subvol snapshot -r /mnt/root/home /mnt/root/home-$DATE
-    sudo btrfs subvol snapshot -r /mnt/root/persistent /mnt/root/persistent-$DATE
+    if [ ! -d "/mnt/root/home-$DATE" ]; then
+      sudo btrfs subvol snapshot -r /mnt/root/home /mnt/root/home-$DATE
+    fi
+    if [ ! -d "/mnt/root/persistent-$DATE" ]; then
+      sudo btrfs subvol snapshot -r /mnt/root/persistent /mnt/root/persistent-$DATE
+    fi
     sudo btrfs send /mnt/root/home-$DATE | pv | ssh lt-home-vm.lantian.pub "btrfs receive /mnt/storage/backups"
     sudo btrfs send /mnt/root/persistent-$DATE | pv | ssh lt-home-vm.lantian.pub "btrfs receive /mnt/storage/backups"
   '';
