463a7da9d171a79e715eb007975014334e6d98be
Lan Tian <xuyh0120@outlook.com>
2025-05-24 01:03:08 -0700

    matrix-synapse: use official synapse compress state module

--- nixos/optional-apps/matrix-synapse/synapse-compress-state.nix
+++ nixos/optional-apps/matrix-synapse/synapse-compress-state.nix
@@ -1,30 +1,12 @@
-{ pkgs, ... }:
-{
+_: {
   imports = [ ../postgresql.nix ];
 
-  systemd.services.synapse-compress-state = {
-    after = [ "matrix-synapse.service" ];
-    requires = [ "matrix-synapse.service" ];
-    script = ''
-      exec ${pkgs.rust-synapse-state-compress}/bin/synapse_auto_compressor \
-        -p "host=/run/postgresql user=matrix-synapse dbname=matrix-synapse" \
-        -c 500 \
-        -n 100
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "matrix-synapse";
-    };
-  };
-
-  systemd.timers.synapse-compress-state = {
-    wantedBy = [ "timers.target" ];
-    partOf = [ "synapse-compress-state.service" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-      RandomizedDelaySec = "4h";
-      Unit = "synapse-compress-state.service";
+  services.synapse-auto-compressor = {
+    enable = true;
+    startAt = "daily";
+    settings = {
+      chunk_size = 500;
+      chunks_to_compress = 100;
     };
   };
 }
