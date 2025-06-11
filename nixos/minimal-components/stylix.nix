fb95807ea6d2a3cd0532fef94bf9781a94aa9b8c
Lan Tian <xuyh0120@outlook.com>
2025-06-10 22:49:30 -0700

    gui: use stylix to manage all GUI styles

---
+++ nixos/minimal-components/stylix.nix
@@ -0,0 +1,42 @@
+{ pkgs, LT, ... }:
+{
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/oceanicnext.yaml";
+
+    cursor = {
+      package = pkgs.nur-xddxdd.sam-toki-mouse-cursors;
+      name = "STMCS_601_Genshin_Furina";
+      size = LT.constants.gui.cursorSize;
+    };
+
+    fonts = {
+      serif = {
+        package = pkgs.nerd-fonts.noto;
+        name = "Noto Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.nerd-fonts.ubuntu;
+        name = "Ubuntu";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.ubuntu-mono;
+        name = "Ubuntu Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji-blob-bin;
+        name = "Blobmoji";
+      };
+
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 12;
+      };
+    };
+  };
+}
