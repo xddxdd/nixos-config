7abe02ed3e6f5b2a5ec86fa7aba32a58c173d5c1
Lan Tian <xuyh0120@outlook.com>
2025-06-11 21:53:03 -0700

    general: update nixpkgs-options module definition

---
+++ flake-modules/nixpkgs-options.nix
@@ -0,0 +1,55 @@
+{
+  self,
+  inputs,
+  ...
+}:
+let
+  inherit (inputs.nixpkgs) lib;
+  LT = import ../helpers {
+    inherit lib inputs;
+    inherit (inputs) self;
+  };
+in
+{
+  perSystem =
+    { system, ... }:
+    {
+      nixpkgs-options =
+        let
+          permittedInsecurePackages = [
+            "aspnetcore-runtime-6.0.36"
+            "aspnetcore-runtime-wrapped-6.0.36"
+            "dotnet-sdk-6.0.428"
+            "dotnet-sdk-wrapped-6.0.428"
+            "electron-32.3.3"
+            "olm-3.2.16"
+            "ventoy-1.1.05"
+          ];
+          overlays = [
+            inputs.agenix.overlays.default
+            inputs.colmena.overlay
+            inputs.nil.overlays.nil
+            inputs.nix-alien.overlays.default
+            inputs.nix-xilinx.overlay
+            inputs.nixd.overlays.default
+            inputs.nur-xddxdd.overlays.inSubTree-pinnedNixpkgsWithCuda
+            inputs.nur.overlays.default
+            inputs.nvfetcher.overlays.default
+            inputs.proxmox-nixos.overlays."${system}"
+            inputs.secrets.overlays.default
+          ] ++ (import ../overlays { inherit inputs; });
+        in
+        {
+          pkgs = {
+            sourceInput = inputs.nixpkgs;
+            patches = LT.ls ../patches/nixpkgs;
+            inherit permittedInsecurePackages overlays;
+          };
+          pkgs-stable = {
+            sourceInput = inputs.nixpkgs-stable;
+            patches = LT.ls ../patches/nixpkgs-stable;
+            inherit permittedInsecurePackages overlays;
+          };
+        };
+    };
+}
