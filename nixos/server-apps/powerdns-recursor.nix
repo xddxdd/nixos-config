d64a8850887ff74901c358f9a4956cee1efbe158
Lan Tian <xuyh0120@outlook.com>
2025-06-11 23:15:20 -0700

    powerdns-recursor: update config to YAML format

--- nixos/server-apps/powerdns-recursor.nix
+++ nixos/server-apps/powerdns-recursor.nix
@@ -26,67 +26,95 @@ lib.mkIf (!(LT.this.hasTag LT.tags.low-ram)) {
 
   services.pdns-recursor = {
     enable = true;
-    dns.address = "0.0.0.0, ::";
+    dns.address = [
+      "0.0.0.0"
+      "::"
+    ];
     dns.allowFrom = [
       "0.0.0.0/0"
       "::/0"
     ];
-    forwardZones =
-      let
-        authoritative =
-          lib.genAttrs
-            # NeoNetwork is covered by fwd-dn42-interconnect
-            (with LT.constants.zones; (DN42 ++ OpenNIC ++ CRXN ++ Ltnet))
-            (
-              _k:
-              builtins.concatStringsSep ";" [
-                "198.19.0.254"
-                "fdbc:f9dc:67ad:2547::54"
-              ]
-            );
-        emercoin = lib.genAttrs LT.constants.zones.Emercoin (
-          _k:
-          builtins.concatStringsSep ";" [
-            "185.122.58.37"
-            "2a06:8ec0:3::1:2c4e"
-            "172.106.88.242"
-            "2602:ffc5:30::1:5c47"
-          ]
-        );
-        yggdrasilAlfis = lib.genAttrs LT.constants.zones.YggdrasilAlfis (_k: "fdbc:f9dc:67ad:2547::52");
-      in
-      authoritative
-      // emercoin
-      // yggdrasilAlfis
-      // {
-        # DN42
-        "hack" = "172.31.0.5";
-      };
     luaConfig =
-      (lib.concatMapStringsSep "\n" (n: "addNTA(\"${n}\")") (
-        with LT.constants.zones; (OpenNIC ++ Emercoin ++ CRXN ++ YggdrasilAlfis ++ Ltnet)
-      ))
-      + ''
+      let
+        ntaRecords = lib.concatMapStringsSep "\n" (n: "addNTA(\"${n}\")") (
+          with LT.constants.zones; (DN42 ++ OpenNIC ++ Emercoin ++ CRXN ++ YggdrasilAlfis ++ Ltnet)
+        );
+      in
+      ''
+        ${ntaRecords}
         dofile("/nix/persistent/sync-servers/ltnet-scripts/pdns-recursor-conf/fwd-dn42-interconnect.lua")
       '';
     serveRFC1918 = false;
-    settings = {
-      any-to-tcp = "yes";
-      dont-query = "";
-      loglevel = 3;
-      qname-minimization = "no";
-      query-local-address = builtins.concatStringsSep ", " [
-        config.lantian.netns.powerdns-recursor.ipv4
-        config.lantian.netns.powerdns-recursor.ipv6
-      ];
-      reuseport = "yes";
-      server-id = "lantian";
-      tcp-fast-open = "128";
-      include-dir = "/nix/persistent/sync-servers/ltnet-scripts/pdns-recursor-conf";
-      "forward-zones-recurse+=." = builtins.concatStringsSep ";" [
-        "172.22.76.109:${LT.portStr.DNSUpstream}"
-        "[fdbc:f9dc:67ad:2547::54]:${LT.portStr.DNSUpstream}"
-      ];
+    yaml-settings = {
+      incoming = {
+        reuseport = true;
+        tcp_fast_open = 128;
+      };
+      recursor = {
+        any_to_tcp = true;
+        qname_minimization = false;
+        server_id = "lantian.${config.networking.hostName}";
+        forward_zones_file = "/nix/persistent/sync-servers/ltnet-scripts/pdns-recursor-conf/fwd-dn42-interconnect.yml";
+        forward_zones =
+          let
+            authoritative =
+              builtins.map
+                (k: {
+                  zone = k;
+                  forwarders = [
+                    "198.19.0.254"
+                    "fdbc:f9dc:67ad:2547::54"
+                  ];
+                })
+                # NeoNetwork is covered by fwd-dn42-interconnect
+                (with LT.constants.zones; (DN42 ++ OpenNIC ++ CRXN ++ Ltnet));
+
+            emercoin = builtins.map (k: {
+              zone = k;
+              forwarders = [
+                "185.122.58.37"
+                "2a06:8ec0:3::1:2c4e"
+                "172.106.88.242"
+                "2602:ffc5:30::1:5c47"
+              ];
+            }) LT.constants.zones.Emercoin;
+
+            yggdrasilAlfis = builtins.map (k: {
+              zone = k;
+              forwarders = [
+                "fdbc:f9dc:67ad:2547::52"
+              ];
+            }) LT.constants.zones.YggdrasilAlfis;
+
+            hack = [
+              {
+                zone = "hack";
+                forwarders = [ "172.31.0.5" ];
+              }
+            ];
+          in
+          authoritative ++ emercoin ++ yggdrasilAlfis ++ hack;
+        forward_zones_recurse = [
+          {
+            zone = ".";
+            forwarders = [
+              "172.22.76.109:${LT.portStr.DNSUpstream}"
+              "[fdbc:f9dc:67ad:2547::54]:${LT.portStr.DNSUpstream}"
+            ];
+          }
+        ];
+      };
+      outgoing = {
+        dont_query = [ ];
+        source_address = [
+          config.lantian.netns.powerdns-recursor.ipv4
+          config.lantian.netns.powerdns-recursor.ipv6
+        ];
+      };
+
+      # # Only enable when debugging!
+      # dnssec.log_bogus = true;
+      # logging.trace = "fail";
     };
   };
 
