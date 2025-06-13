620e278ba02f2c609355172da9a1a0f22046e27c
Lan Tian <xuyh0120@outlook.com>
2025-06-13 01:35:04 -0700

    gui: enable stylix color for firefox and thunderbird

--- home/client-apps/thunderbird.nix
+++ home/client-apps/thunderbird.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   userChrome = pkgs.writeText "userChrome.css" ''
     .titlebar-buttonbox-container {
       display:none;
     }
+
+    ${config.programs.firefox.profiles._stylix.userChrome}
+  '';
+
+  userContent = pkgs.writeText "userContent.css" ''
+    ${config.programs.firefox.profiles._stylix.userContent}
   '';
 in
 {
@@ -12,6 +18,7 @@ in
       for F in $(cat "$HOME/.thunderbird/profiles.ini" | grep Path | cut -d= -f2); do
         if [ -d "$HOME/.thunderbird/$F" ]; then
           ${pkgs.coreutils}/bin/install -Dm755 ${userChrome} "$HOME/.thunderbird/$F/chrome/userChrome.css"
+          ${pkgs.coreutils}/bin/install -Dm755 ${userContent} "$HOME/.thunderbird/$F/chrome/userContent.css"
         fi
       done
     fi
