b415985c16a618a891fa02f19e3d8b0888ef8c18
Lan Tian <xuyh0120@outlook.com>
2025-06-13 23:15:02 -0700

    gui: globally override base01 background color to same as base00

--- home/client-apps/stylix.nix
+++ home/client-apps/stylix.nix
@@ -1,12 +1,4 @@
-{ config, lib, ... }:
-let
-  mkColor = color: {
-    r = config.lib.stylix.colors."${color}-rgb-r";
-    g = config.lib.stylix.colors."${color}-rgb-g";
-    b = config.lib.stylix.colors."${color}-rgb-b";
-  };
-in
-{
+_: {
   stylix.targets = {
     # Having KDE is enough, QT module only supports qtct not kde6
     qt.enable = false;
@@ -20,10 +12,4 @@ in
       profileNames = [ "lantian" ];
     };
   };
-
-  programs = lib.genAttrs [ "firefox" "librewolf" ] (n: {
-    profiles.lantian.extensions.settings."FirefoxColor@mozilla.com".settings.theme.colors = {
-      frame = lib.mkForce (mkColor "base00");
-    };
-  });
 }
