2b8f98916515dc49ddb9529a961b78408f92a3c3
Lan Tian <xuyh0120@outlook.com>
2025-06-15 00:47:52 -0700

    gui: manage firefox settings with home-manager

--- nixos/client-apps/firefox.nix
+++ nixos/client-apps/firefox.nix
@@ -100,35 +100,5 @@
       };
       UseSystemPrintDialog = true;
     };
-    preferences = {
-      "browser.aboutConfig.showWarning" = false;
-      "gfx.webrender.all" = true;
-      # FIXME: enabling causes graphic glitches
-      "gfx.webrender.compositor.force-enabled" = false;
-      "gfx.x11-egl.force-enabled" = true;
-      "media.av1.enabled" = true;
-      "media.ffmpeg.vaapi.enabled" = true;
-      "media.hardware-video-decoding.force-enabled" = true;
-      "media.hls.enabled" = true;
-      "media.videocontrols.picture-in-picture.enabled" = false;
-      "security.insecure_connection_text.enabled" = true;
-      "security.insecure_connection_text.pbmode.enabled" = true;
-      "security.osclientcerts.autoload" = true;
-      "geo.provider.network.url" = "https://api.beacondb.net/v1/geolocate";
-      "geo.provider.use_geoclue" = config.services.geoclue2.enable;
-      "browser.ml.chat.provider" = "https://ai.xuyh0120.win";
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      "svg.context-properties.content.enabled" = true;
-
-      # Make home-manager extension config work
-      "extensions.webextensions.ExtensionStorageIDB.enabled" = false;
-
-      # Vertical tabs
-      "sidebar.verticalTabs" = true;
-      "browser.tabs.groups.enabled" = true;
-      "browser.tabs.groups.smart.enabled" = true;
-      "browser.tabs.unloadTabInContextMenu" = true;
-    };
-    preferencesStatus = "locked";
   };
 }
