835bd4c51a0d1e52aa68aa0e8fda174dab9a865e
Lan Tian <xuyh0120@outlook.com>
2025-06-16 17:11:42 -0700

    matrix-synapse: fix state compress panic issue

--- nixos/optional-apps/matrix-synapse/synapse-compress-state.nix
+++ nixos/optional-apps/matrix-synapse/synapse-compress-state.nix
@@ -1,4 +1,5 @@
-_: {
+{ config, ... }:
+{
   imports = [ ../postgresql.nix ];
 
   services.synapse-auto-compressor = {
@@ -8,5 +9,42 @@ _: {
       chunk_size = 500;
       chunks_to_compress = 100;
     };
+  };
+
+  systemd.services.synapse-auto-compressor = {
+    # https://github.com/matrix-org/rust-synapse-compress-state/issues/78#issuecomment-1409932869
+    path = [ config.services.postgresql.package ];
+    preStart = ''
+      psql <<_EOF
+      BEGIN;
+
+      DELETE
+      FROM state_compressor_state AS scs
+      WHERE NOT EXISTS
+          (SELECT *
+           FROM rooms AS r
+           WHERE r.room_id = scs.room_id);
+
+      DELETE
+      FROM state_compressor_state AS scs
+      WHERE scs.room_id in
+          (SELECT DISTINCT room_id
+           FROM state_compressor_state AS scs2
+           WHERE scs2.current_head IS NOT NULL
+             AND NOT EXISTS
+               (SELECT *
+                FROM state_groups AS sg
+                WHERE sg.id = scs2.current_head));
+
+      DELETE
+      FROM state_compressor_progress AS scp
+      WHERE NOT EXISTS
+          (SELECT *
+           FROM state_compressor_state AS scs
+           WHERE scs.room_id = scp.room_id);
+
+      COMMIT;
+      _EOF
+    '';
   };
 }
