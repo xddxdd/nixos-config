ce57684f30c8f35c8978962837934b7a4408d4be
Lan Tian <xuyh0120@outlook.com>
2025-06-17 19:35:18 -0700

    general: update secrets

--- helpers/constants.nix
+++ helpers/constants.nix
@@ -74,18 +74,6 @@ rec {
     IPv6 = [ "fd10:127::/32" ];
   };
 
-  dnssecKeys = [
-    "K0.1.0.0.7.2.1.0.0.1.d.f.ip6.arpa.+013+11807"
-    "K10.127.10.in-addr.arpa.+013+53292"
-    "K184_29.76.22.172.in-addr.arpa.+013+08709"
-    "K96_27.76.22.172.in-addr.arpa.+013+41969"
-    "Kasn.lantian.pub.+013+48539"
-    "Kd.a.7.6.c.d.9.f.c.b.d.f.ip6.arpa.+013+18344"
-    "Klantian.dn42.+013+20109"
-    "Klantian.eu.org.+013+37106"
-    "Klantian.neo.+013+47346"
-  ];
-
   matrixWellKnown = {
     server = builtins.toJSON { "m.server" = "matrix.lantian.pub:${portStr.Matrix.Public}"; };
     client = builtins.toJSON {
