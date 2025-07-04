9a51cb63b11db52cb011bf5cda201cef81ff84a0
Lan Tian <xuyh0120@outlook.com>
2025-07-04 14:42:33 -0700

    plasma: declaratively set IME

--- home/client-apps/plasma.nix
+++ home/client-apps/plasma.nix
@@ -27,6 +27,11 @@ _: {
       "kwinrulesrc"
     ];
 
+    configFile.kwinrc."Wayland".InputMethod = {
+      shellExpand = true;
+      value = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
+    };
+
     desktop = {
       icons = {
         alignment = "left";
