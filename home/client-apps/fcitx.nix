1ca58620b154ca55a6620a91c58dc3d5b68765a9
Lan Tian <xuyh0120@outlook.com>
2025-07-04 14:59:33 -0700

    fcitx: set env vars for some apps to fix IME issues

--- home/client-apps/fcitx.nix
+++ home/client-apps/fcitx.nix
@@ -1,6 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   xdg.dataFile = {
     "fcitx5/themes".source = "${pkgs.nur-xddxdd.fcitx5-breeze}/share/fcitx5/themes";
   };
+
+  # Apply IME fix wrapper
+  home.packages =
+    builtins.map
+      (
+        p:
+        lib.hiPrio (
+          pkgs.runCommand "${p.name}-wrapped" { nativeBuildInputs = with pkgs; [ makeWrapper ]; } ''
+            mkdir -p $out/bin
+            makeWrapper \
+              ${p}/bin/${p.meta.mainProgram} \
+              $out/bin/${p.meta.mainProgram} \
+              --set GTK_IM_MODULE wayland \
+              --set QT_IM_MODULE fcitx
+          ''
+        )
+      )
+      [
+        pkgs.audacious
+        pkgs.ghostty
+      ];
 }
