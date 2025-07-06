38d6c01b8101579cd1e9f6d800938ec148602329
Lan Tian <xuyh0120@outlook.com>
2025-07-05 22:09:32 -0700

    radicale: use LDAP for authentication

--- nixos/optional-apps/radicale.nix
+++ nixos/optional-apps/radicale.nix
@@ -1,10 +1,28 @@
-{ LT, ... }:
 {
+  LT,
+  config,
+  inputs,
+  ...
+}:
+{
+  age.secrets.glauth-bindpw = {
+    file = inputs.secrets + "/glauth-bindpw.age";
+    mode = "0444";
+  };
+
   services.radicale = {
     enable = true;
     settings = {
       server.hosts = [ "127.0.0.1:${LT.portStr.Radicale}" ];
-      auth.type = "http_x_remote_user";
+      auth = {
+        type = "ldap";
+        ldap_uri = "ldap://[fdbc:f9dc:67ad:2547::389]";
+        ldap_base = "dc=lantian,dc=pub";
+        ldap_reader_dn = "cn=serviceuser,dc=lantian,dc=pub";
+        ldap_secret_file = config.age.secrets.glauth-bindpw.path;
+        ldap_filter = "(&(cn={0})(objectClass=posixAccount)(!(ou=svcaccts)))";
+        ldap_user_attribute = "cn";
+      };
       storage.filesystem_folder = "/var/lib/radicale/collections";
     };
   };
@@ -16,10 +34,7 @@
 
   lantian.nginxVhosts."cal.xuyh0120.win" = {
     locations = {
-      "/" = {
-        proxyPass = "http://127.0.0.1:${LT.portStr.Radicale}";
-        enableBasicAuth = true;
-      };
+      "/".proxyPass = "http://127.0.0.1:${LT.portStr.Radicale}";
     };
 
     sslCertificate = "lets-encrypt-xuyh0120.win";
