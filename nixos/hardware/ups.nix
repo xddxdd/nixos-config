30aa5221372c98e3dc0bb8bf3ee5c3da9b339898
Lan Tian <xuyh0120@outlook.com>
2025-07-16 23:59:57 -0700

    general: consolidate files

--- nixos/hardware/ups.nix
+++ nixos/hardware/ups.nix
@@ -85,4 +85,17 @@ in
       ]) upsmonNotifyTypes;
     };
   };
+
+  systemd.services.upsd = lib.mkIf config.power.ups.enable {
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = 5;
+    };
+  };
+  systemd.services.upsmon = lib.mkIf config.power.ups.upsmon.enable {
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = 5;
+    };
+  };
 }
