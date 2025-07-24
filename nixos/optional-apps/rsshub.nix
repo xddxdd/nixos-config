e98e552fd9ee30d91ac443e749b07d7102d4371e
Lan Tian <xuyh0120@outlook.com>
2025-07-23 23:45:52 -0700

    rsshub: deploy directly without Docker

--- nixos/optional-apps/rsshub.nix
+++ nixos/optional-apps/rsshub.nix
@@ -1,14 +1,40 @@
-{ LT, config, ... }:
+{ LT, pkgs, ... }:
 {
-  virtualisation.oci-containers.containers.rsshub = {
-    extraOptions = [ "--pull=always" ];
-    image = "diygod/rsshub:chromium-bundled";
+  systemd.services.rsshub = {
+    description = "RSSHub";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
     environment = {
-      TZ = config.time.timeZone;
+      PORT = LT.portStr.RSSHub;
+      LISTEN_INADDR_ANY = "0";
       CACHE_CONTENT_EXPIRE = "600";
     };
-    ports = [ "127.0.0.1:${LT.portStr.RSSHub}:1200" ];
+
+    serviceConfig = LT.serviceHarden // {
+      User = "rsshub";
+      Group = "rsshub";
+
+      ExecStart = "${pkgs.rsshub}/bin/rsshub";
+
+      RestrictAddressFamilies = [
+        "AF_INET"
+        "AF_INET6"
+        "AF_UNIX"
+        "AF_NETLINK"
+      ];
+
+      MemoryDenyWriteExecute = false;
+      Restart = "always";
+      RestartSec = 5;
+    };
   };
+
+  users.users.rsshub = {
+    group = "rsshub";
+    isSystemUser = true;
+  };
+  users.groups.rsshub = { };
 
   lantian.nginxVhosts."rsshub.xuyh0120.win" = {
     locations = {
