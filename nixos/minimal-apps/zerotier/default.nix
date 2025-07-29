f748f463bcc7a7451923599f8df2d51444325992
Lan Tian <xuyh0120@outlook.com>
2025-07-28 19:33:51 -0700

    networking: enable trusted path on reserved IP ranges

--- nixos/minimal-apps/zerotier/default.nix
+++ nixos/minimal-apps/zerotier/default.nix
@@ -25,6 +25,9 @@ in
     enable = true;
     joinNetworks = [ ltnet ];
     localConf = {
+      physical = lib.genAttrs (LT.constants.reserved.IPv4 ++ LT.constants.reserved.IPv6) (net: {
+        trustedPathId = 2547;
+      });
       virtual = lib.mapAttrs' (
         k: v:
         lib.nameValuePair v.zerotier {
