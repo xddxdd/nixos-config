015f84c69b3e0ed921a66a45e22dc1a9feb89ba6
Lan Tian <xuyh0120@outlook.com>
2025-08-06 01:14:22 -0700

    acme: switch to ZeroSSL to workaround renewal issues with Let's Encrypt

--- nixos/optional-apps/acme/base-domains.nix
+++ nixos/optional-apps/acme/base-domains.nix
@@ -7,7 +7,7 @@
 }:
 let
   inherit (pkgs.callPackage ./common.nix { inherit config; })
-    mkLetsEncryptWildcardCert
+    mkZeroSSLWildcardCert
     ;
 
   hostSubdomains = lib.mapAttrsToList (n: v: "${n}.xuyh0120.win") LT.hosts;
@@ -15,11 +15,11 @@ in
 {
   security.acme.certs = lib.mergeAttrsList (
     [
-      (mkLetsEncryptWildcardCert "lantian.pub")
-      (mkLetsEncryptWildcardCert "xuyh0120.win")
-      (mkLetsEncryptWildcardCert "56631131.xyz")
-      (mkLetsEncryptWildcardCert "ltn.pw")
+      (mkZeroSSLWildcardCert "lantian.pub")
+      (mkZeroSSLWildcardCert "xuyh0120.win")
+      (mkZeroSSLWildcardCert "56631131.xyz")
+      (mkZeroSSLWildcardCert "ltn.pw")
     ]
-    ++ (builtins.map mkLetsEncryptWildcardCert hostSubdomains)
+    ++ (builtins.map mkZeroSSLWildcardCert hostSubdomains)
   );
 }
