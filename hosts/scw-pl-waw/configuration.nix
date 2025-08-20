6f18b18249a97a640e10111fc34c4b2e5b778b2b
Lan Tian <xuyh0120@outlook.com>
2025-08-19 20:49:58 -0700

    hosts(scw-*): add

---
+++ hosts/scw-pl-waw/configuration.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  lib,
+  LT,
+  config,
+  utils,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  systemd.network.networks.eth0 = {
+    networkConfig = {
+      DHCP = "yes";
+      IPv6AcceptRA = "yes";
+    };
+    ipv6AcceptRAConfig = {
+      Token = "::1";
+      DHCPv6Client = "no";
+    };
+    matchConfig.Name = "eth0";
+  };
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+
+  services.yggdrasil.regions = [
+    "germany"
+    "france"
+    "luxembourg"
+    "netherlands"
+    "united-kingdom"
+  ];
+}
