38ed0318d080f52efba803773ed6296106264baf
Lan Tian <xuyh0120@outlook.com>
2025-08-25 21:42:55 -0700

    podman: enforce auto update for all containers

--- nixos/minimal-components/podman.nix
+++ nixos/minimal-components/podman.nix
@@ -42,5 +42,11 @@
     users.users.lantian.extraGroups = [ "podman" ];
 
     virtualisation.oci-containers.backend = "podman";
+
+    # Make sure auto update is enabled for all containers
+    assertions = lib.mapAttrsToList (n: v: {
+      assertion = (v.labels."io.containers.autoupdate" or "") != "";
+      message = "Container ${n} does not have auto update enabled";
+    }) config.virtualisation.oci-containers.containers;
   };
 }
