73cce6cca40863f24c7b64972157ca44ac8caa71
Lan Tian <xuyh0120@outlook.com>
2025-09-03 00:37:31 -0700

    iyuuplus: run without docker container

--- nixos/optional-apps/iyuuplus.nix
+++ nixos/optional-apps/iyuuplus.nix
@@ -1,27 +1,103 @@
-{ LT, config, ... }:
 {
-  virtualisation.oci-containers.containers.iyuuplus = {
-    image = "docker.io/iyuucn/iyuuplus-dev:latest";
-    labels = {
-      "io.containers.autoupdate" = "registry";
+  LT,
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+let
+  php-iyuu = pkgs.php83.withExtensions (
+    { enabled, all }:
+    with all;
+    enabled
+    ++ [
+      curl
+      fileinfo
+      gd
+      mbstring
+      exif
+      mysqli
+      openssl
+      pdo_mysql
+      pdo_sqlite
+      sockets
+      sodium
+      sqlite3
+      zip
+    ]
+  );
+in
+{
+  imports = [ ./mysql.nix ];
+
+  age.secrets.iyuu-env = {
+    file = inputs.secrets + "/iyuu-env.age";
+    owner = "iyuu";
+    group = "iyuu";
+  };
+
+  systemd.services.iyuuplus = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "mysql.service" ];
+    requires = [ "mysql.service" ];
+
+    path = with pkgs; [ gitMinimal ];
+
+    preStart = ''
+      if [ -e .git ]; then
+        git fetch --all
+        git reset --hard origin/master
+      else
+        git clone https://github.com/ledccn/iyuuplus-dev.git .
+      fi
+
+      sed -E -i "s#'listen'.*#'listen' => 'http://127.0.0.1:${LT.portStr.IyuuPlus}',#g" config/server.php
+      install -Dm644 ${config.age.secrets.iyuu-env.path} .env
+    '';
+
+    serviceConfig = LT.serviceHarden // {
+      ExecStart = "${php-iyuu}/bin/php /var/lib/iyuu/start.php start";
+      StateDirectory = "iyuu";
+      WorkingDirectory = "/var/lib/iyuu";
+      User = "iyuu";
+      Group = "iyuu";
+      MemoryDenyWriteExecute = lib.mkForce false;
+
+      Restart = "always";
+      RestartSec = "5";
     };
-    ports = [ "127.0.0.1:${LT.portStr.IyuuPlus}:8780" ];
-    volumes = [
-      "/var/lib/iyuu/iyuu:/iyuu"
-      "/var/lib/iyuu/data:/data"
+  };
+
+  systemd.tmpfiles.rules = [ "d /var/lib/iyuu/runtime/crontab/observer 755 iyuu iyuu" ];
+
+  services.mysql = {
+    ensureDatabases = [ "iyuu" ];
+    ensureUsers = [
+      {
+        name = "iyuu";
+        ensurePermissions = {
+          "iyuu.*" = "ALL PRIVILEGES";
+        };
+      }
     ];
   };
 
-  systemd.tmpfiles.rules = [
-    "d /var/lib/iyuu/iyuu 755 root root"
-    "d /var/lib/iyuu/data 755 root root"
-  ];
+  users.users.iyuu = {
+    group = "iyuu";
+    isSystemUser = true;
+  };
+  users.groups.iyuu.members = [ "nginx" ];
 
   lantian.nginxVhosts = {
     "iyuu.${config.networking.hostName}.xuyh0120.win" = {
       locations = {
         "/" = {
           proxyPass = "http://127.0.0.1:${LT.portStr.IyuuPlus}";
+          proxyWebsockets = true;
+        };
+        "/app/d9422b72cffad23098ad301eea0f8419" = {
+          proxyPass = "http://127.0.0.1:3131";
           proxyWebsockets = true;
         };
       };
@@ -37,6 +113,10 @@
       locations = {
         "/" = {
           proxyPass = "http://127.0.0.1:${LT.portStr.IyuuPlus}";
+          proxyWebsockets = true;
+        };
+        "/app/d9422b72cffad23098ad301eea0f8419" = {
+          proxyPass = "http://127.0.0.1:3131";
           proxyWebsockets = true;
         };
       };
