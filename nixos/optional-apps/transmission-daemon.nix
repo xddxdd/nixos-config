76233f00896f16ed1237442f5c99c402b5c5cc46
Lan Tian <xuyh0120@outlook.com>
2025-09-04 01:06:18 -0700

    transmission-daemon: add patch to truncate long filenames

--- nixos/optional-apps/transmission-daemon.nix
+++ nixos/optional-apps/transmission-daemon.nix
@@ -8,7 +8,9 @@
 {
   services.transmission = {
     enable = true;
-    package = pkgs.nur-xddxdd.lantianCustomized.transmission-with-webui;
+    package = pkgs.nur-xddxdd.lantianCustomized.transmission-with-webui.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [ ../../patches/transmission-truncate-long-filename.patch ];
+    });
     user = "lantian";
     group = "users";
     downloadDirPermissions = "775";
