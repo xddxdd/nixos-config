18f365e6d6e88966a3f74224733c9b98cb472370
Lan Tian <xuyh0120@outlook.com>
2025-09-04 21:32:15 -0700

    firefox: enable HDR on wayland

--- home/client-apps/firefox.nix
+++ home/client-apps/firefox.nix
@@ -51,38 +51,36 @@ let
       };
 
       settings = {
+        # keep-sorted start
         "browser.aboutConfig.showWarning" = false;
+        # AI Sidebar
+        "browser.ml.chat.provider" = "https://ai.xuyh0120.win";
+        "browser.tabs.groups.enabled" = true;
+        "browser.tabs.groups.smart.enabled" = true;
+        "browser.tabs.unloadTabInContextMenu" = true;
+        "extensions.autoDisableScopes" = 0; # Auto enable installed extensions
+        "extensions.webextensions.ExtensionStorageIDB.enabled" = false; # Make home-manager extension config work
+        "geo.provider.network.url" = lib.mkForce "https://api.beacondb.net/v1/geolocate";
+        "geo.provider.use_geoclue" = osConfig.services.geoclue2.enable;
+        "gfx.wayland.hdr" = true;
         "gfx.webrender.all" = true;
-        # FIXME: enabling causes graphic glitches
-        "gfx.webrender.compositor.force-enabled" = false;
+        "gfx.webrender.compositor.force-enabled" = false; # FIXME: enabling causes graphic glitches
         "gfx.x11-egl.force-enabled" = true;
         "image.avif.enabled" = true;
         "image.jxl.enabled" = true;
         "media.av1.enabled" = true;
         "media.ffmpeg.vaapi.enabled" = true;
         "media.hardware-video-decoding.force-enabled" = true;
+        "media.hevc.enabled" = true;
         "media.hls.enabled" = true;
         "media.videocontrols.picture-in-picture.enabled" = false;
         "security.insecure_connection_text.enabled" = true;
         "security.insecure_connection_text.pbmode.enabled" = true;
         "security.osclientcerts.autoload" = true;
-        "geo.provider.network.url" = lib.mkForce "https://api.beacondb.net/v1/geolocate";
-        "geo.provider.use_geoclue" = osConfig.services.geoclue2.enable;
-        "browser.ml.chat.provider" = "https://ai.xuyh0120.win";
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "svg.context-properties.content.enabled" = true;
-
-        # Make home-manager extension config work
-        "extensions.webextensions.ExtensionStorageIDB.enabled" = false;
-
-        # Vertical tabs
         "sidebar.verticalTabs" = true;
-        "browser.tabs.groups.enabled" = true;
-        "browser.tabs.groups.smart.enabled" = true;
-        "browser.tabs.unloadTabInContextMenu" = true;
-
-        # Auto enable installed extensions
-        "extensions.autoDisableScopes" = 0;
+        "svg.context-properties.content.enabled" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        # keep-sorted end
       };
 
       userChrome = ''
