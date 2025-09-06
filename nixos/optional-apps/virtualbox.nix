97e0b98381c37614996f886264d611385da18892
Lan Tian <xuyh0120@outlook.com>
2025-09-06 02:59:06 -0700

    virtualbox: enable extension pack

--- nixos/optional-apps/virtualbox.nix
+++ nixos/optional-apps/virtualbox.nix
@@ -2,6 +2,7 @@ _: {
   virtualisation.virtualbox.host = {
     enable = true;
     addNetworkInterface = false;
+    enableExtensionPack = true;
     enableHardening = false;
     enableKvm = true;
   };
