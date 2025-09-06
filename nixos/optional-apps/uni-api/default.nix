dfadeb11399affd78f4482dd9ca8bff1cee2fd7d
Lan Tian <xuyh0120@outlook.com>
2025-09-06 02:02:38 -0700

    uni-api: move model config to secrets repo

--- nixos/optional-apps/uni-api/default.nix
+++ nixos/optional-apps/uni-api/default.nix
@@ -40,40 +40,7 @@ let
   };
 in
 {
-  imports = [ ./models.nix ];
-
-  age.secrets = builtins.listToAttrs (
-    builtins.map
-      (
-        f:
-        lib.nameValuePair "uni-api-${f}" {
-          file = inputs.secrets + "/uni-api/${f}.age";
-          owner = "uni-api";
-          group = "uni-api";
-        }
-      )
-      [
-        "admin-api-key"
-        "akash-networks-api-key"
-        "cerebras-api-key"
-        "chutes-ai-api-key"
-        "cloudflare-account-id"
-        "cloudflare-api-key"
-        "github-models-api-key"
-        "google-api-key"
-        "groq-api-key"
-        "lingyiwanwu-api-key"
-        "mistral-api-key"
-        "modelscope-api-key"
-        "nvidia-api-key"
-        "openrouter-api-key"
-        "siliconflow-api-key"
-        "v0-api-key"
-        "veloera-api-key"
-        "wbot-api-key"
-        "xai-api-key"
-      ]
-  );
+  imports = [ ./model-config.nix ];
 
   systemd.services.uni-api = {
     description = "Uni-API Server";
