b73e2f1a7dfb07a027ea7fb6340262188c270d2d
Lan Tian <xuyh0120@outlook.com>
2025-09-13 13:27:22 -0700

    hosts(azure-vm1): reenable server role

--- hosts/azure-vm1/configuration.nix
+++ hosts/azure-vm1/configuration.nix
@@ -1,15 +1,15 @@
 { LT, ... }:
 {
   imports = [
-    ../../nixos/minimal.nix
+    ../../nixos/server.nix
 
     ./hardware-configuration.nix
   ];
 
   systemd.network.networks.eth0 = LT.cloudLanNetworking "eth0";
 
-  # services."route-chain" = {
-  #   enable = true;
-  #   routes = [ "172.22.76.97/29" ];
-  # };
+  services."route-chain" = {
+    enable = true;
+    routes = [ "172.22.76.97/29" ];
+  };
 }
