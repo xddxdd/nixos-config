101fc2320c0869652e87289751e9394fb03445f1
Lan Tian <xuyh0120@outlook.com>
2025-09-23 00:30:56 -0700

    general: add secret module

--- nixos/optional-apps/elasticsearch.nix
+++ nixos/optional-apps/elasticsearch.nix
@@ -2,15 +2,23 @@
   LT,
   pkgs,
   config,
+  inputs,
   ...
 }:
 {
+  imports = [
+    "${inputs.secrets}/nixos-hidden-module/bd998f7ec298455a"
+  ];
+
   services.elasticsearch = {
     enable = true;
     port = LT.port.ElasticSearch;
     plugins = with pkgs.elasticsearchPlugins; [
       analysis-smartcn
     ];
+    extraConf = ''
+      xpack.security.enabled: false
+    '';
   };
 
   lantian.nginxVhosts."es.${config.networking.hostName}.xuyh0120.win" = {
