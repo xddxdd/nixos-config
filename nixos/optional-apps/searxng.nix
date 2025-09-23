d0e3ceb2206cff67fe192d1bd89c736a0fc736cb
Lan Tian <xuyh0120@outlook.com>
2025-09-22 21:41:04 -0700

    searxng: enable more engines by default

--- nixos/optional-apps/searxng.nix
+++ nixos/optional-apps/searxng.nix
@@ -1,4 +1,55 @@
 { config, inputs, ... }:
+let
+  enableBuiltinEngines =
+    builtins.map
+      (n: {
+        name = n;
+        disabled = false;
+      })
+      [
+        "bing"
+        "360search"
+        "baidu"
+        "quark"
+        "sogou"
+        "material icons"
+        "bilibili"
+        "sogou wechat"
+        "apple maps"
+        "crates.io"
+        "lib.rs"
+        "npm"
+        "pkg.go.dev"
+        "codeberg"
+        "gitea.com"
+        "gitlab"
+        "huggingface"
+        "huggingface datasets"
+        "ollama"
+        "sourcehut"
+        "nixos wiki"
+        "anaconda"
+        "cppreference"
+        "hackernews"
+        "lobste.rs"
+        "apk mirror"
+        "fdroid"
+        "google play apps"
+        "1337x"
+        "annas archive"
+        "library genesis"
+        "nyaa"
+        "z-library"
+        "imdb"
+        "rottentomatoes"
+        "tmdb"
+        "minecraft wiki"
+        "duckduckgo weather"
+        "openmeteo"
+        "openlibrary"
+        "steam"
+      ];
+in
 {
   age.secrets.searxng-env = {
     file = inputs.secrets + "/searxng-env.age";
@@ -19,12 +70,27 @@
     settings = {
       server.secret_key = "@SEARX_SERVER_SECRET_KEY@";
       search = {
+        favicon_resolver = "duckduckgo";
         formats = [
           "html"
           "csv"
           "json"
           "rss"
         ];
+      };
+      engines = enableBuiltinEngines;
+    };
+    faviconsSettings = {
+      favicons = {
+        cfg_schema = 1;
+        cache = {
+          db_url = "/var/cache/searx/faviconcache.db";
+          HOLD_TIME = 5184000;
+          LIMIT_TOTAL_BYTES = 2147483648;
+          BLOB_MAX_BYTES = 40960;
+          MAINTENANCE_MODE = "auto";
+          MAINTENANCE_PERIOD = 600;
+        };
       };
     };
   };
@@ -35,6 +101,7 @@
     serviceConfig = {
       Restart = "always";
       RestartSec = 5;
+      CacheDirectory = "searx";
     };
   };
 
