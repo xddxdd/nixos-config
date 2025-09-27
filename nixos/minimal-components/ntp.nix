7e7f66369b2ef2587c9e60b782550bae5ef77b93
Lan Tian <xuyh0120@outlook.com>
2025-09-27 00:24:27 -0700

    general: use ntpd-rs as NTP client

--- nixos/minimal-components/ntp.nix
+++ nixos/minimal-components/ntp.nix
@@ -1,10 +1,4 @@
-{
-  pkgs,
-  lib,
-  LT,
-  config,
-  ...
-}:
+_:
 let
   ntpServers = [ ];
   ntpPools = [
@@ -18,70 +12,19 @@ in
 {
   networking.timeServers = ntpServers;
 
-  services.timesyncd.enable = LT.this.hasTag LT.tags.low-ram;
-  services.chrony = rec {
-    enable = !(LT.this.hasTag LT.tags.low-ram);
-    servers = [ ];
-    # Use my custom makestep setting
-    initstepslew.enabled = false;
-    # https://github.com/mlichvar/chrony/blob/master/examples/chrony.conf.example3
-    extraConfig = ''
-      ${lib.concatMapStringsSep "\n" (k: "server ${k} ${serverOption}") ntpServers}
-      ${lib.concatMapStringsSep "\n" (k: "pool ${k} ${serverOption}") ntpPools}
-
-      cmdport 0
-      makestep 1.0 3
-      hwtimestamp *
-    '';
-    serverOption = "${lib.optionalString config.networking.networkmanager.enable "offline"} iburst";
-  };
-
-  systemd.services.chronyd = {
-    restartIfChanged = !config.networking.networkmanager.enable;
-    serviceConfig = {
-      Restart = "always";
-      RestartSec = 5;
+  services.ntpd-rs = {
+    enable = true;
+    useNetworkingTimeServers = false;
+    settings = {
+      source =
+        (builtins.map (n: {
+          mode = "server";
+          address = n;
+        }) ntpServers)
+        ++ (builtins.map (n: {
+          mode = "pool";
+          address = n;
+        }) ntpPools);
     };
   };
-
-  networking.networkmanager.dispatcherScripts = [
-    {
-      type = "basic";
-      source = pkgs.writeShellScript "chrony" ''
-        INTERFACE=$1
-        STATUS=$2
-
-        # Make sure we're always getting the standard response strings
-        LANG='C'
-
-        CHRONY=${config.services.chrony.package}/bin/chronyc
-
-        chrony_cmd() {
-          echo "Chrony going $1."
-          exec $CHRONY -a $1
-        }
-
-        nm_connected() {
-          [ "$(${pkgs.networkmanager}/bin/nmcli -t --fields STATE g)" = 'connected' ]
-        }
-
-        case "$STATUS" in
-          up)
-            chrony_cmd online
-          ;;
-          vpn-up)
-            chrony_cmd online
-          ;;
-          down)
-            # Check for active interface, take offline if none is active
-            nm_connected || chrony_cmd offline
-          ;;
-          vpn-down)
-            # Check for active interface, take offline if none is active
-            nm_connected || chrony_cmd offline
-          ;;
-        esac
-      '';
-    }
-  ];
 }
