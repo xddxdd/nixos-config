0eba2d38ece76f5286f7629bd00aba0378ad4fbc
Lan Tian <xuyh0120@outlook.com>
2025-10-02 20:49:42 -0700

    hosts(alice2): add

---
+++ hosts/alice2/configuration.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  lib,
+  LT,
+  ...
+}:
+{
+  imports = [
+    ../../nixos/server.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  systemd.network.networks.eth0 = {
+    address = [ "2a14:67c0:105:10a::a/128" ];
+    routes = [
+      {
+        # Special config since gateway isn't in subnet
+        Gateway = "2a14:67c0:105::1";
+        GatewayOnLink = true;
+      }
+    ];
+    matchConfig.Name = "eth0";
+  };
+
+  # Cannot connect to log server since this server is IPv6 only
+  services.filebeat.enable = lib.mkForce false;
+
+  services."route-chain" = {
+    enable = true;
+    routes = [
+      "172.22.76.97/29"
+    ];
+  };
+}
