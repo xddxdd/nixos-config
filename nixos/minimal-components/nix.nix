21e1cc46613219fa88f40d2f4a7dca20c4c028f1
Lan Tian <xuyh0120@outlook.com>
2025-10-05 14:23:42 -0700

    nix: limit build concurrency to avoid OOM

--- nixos/minimal-components/nix.nix
+++ nixos/minimal-components/nix.nix
@@ -52,6 +52,7 @@ in
       auto-optimise-store = true;
       builders-use-substitutes = true;
       connect-timeout = 5;
+      cores = 2 * LT.math.round (LT.math.sqrt LT.this.cpuThreads);
       download-buffer-size = 1024 * 1024 * 1024;
       experimental-features = lib.mkForce "nix-command flakes ca-derivations auto-allocate-uids cgroups";
       extra-experimental-features = lib.mkForce "nix-command flakes ca-derivations auto-allocate-uids cgroups";
@@ -60,6 +61,7 @@ in
       keep-outputs = true;
       log-lines = 25;
       max-free = 1000 * 1000 * 1000;
+      max-jobs = LT.math.round (LT.math.sqrt LT.this.cpuThreads);
       min-free = 128 * 1000 * 1000;
       trusted-users = allowedUsers;
       use-cgroups = true;
