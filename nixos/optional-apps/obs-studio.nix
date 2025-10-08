36e5016774ee1826f76ff3700bf42fecf66a0d5f
Lan Tian <xuyh0120@outlook.com>
2025-10-07 22:53:10 -0700

    obs-studio: add and reenable plugins

--- nixos/optional-apps/obs-studio.nix
+++ nixos/optional-apps/obs-studio.nix
@@ -1,14 +1,21 @@
 { pkgs, config, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    obs-studio
-    # FIXME
-    # obs-studio-plugins.looking-glass-obs
-    obs-studio-plugins.obs-gstreamer
-    # FIXME
-    # obs-studio-plugins.obs-nvfbc
-    obs-studio-plugins.wlrobs
-  ];
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      # keep-sorted start
+      input-overlay
+      looking-glass-obs
+      obs-dvd-screensaver
+      obs-gstreamer
+      obs-multi-rtmp
+      obs-mute-filter
+      obs-noise
+      obs-vkcapture
+      wlrobs
+      # keep-sorted end
+    ];
+  };
 
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   # Don't autoload as it conflicts with physical camera on some programs
