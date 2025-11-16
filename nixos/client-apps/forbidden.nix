a43ed539080c05359b2aebb88de53c4125b416ce
Lan Tian <xuyh0120@outlook.com>
2025-11-15 22:13:28 -0800

    general: disallow compiling librewolf as well

--- nixos/client-apps/forbidden.nix
+++ nixos/client-apps/forbidden.nix
@@ -3,6 +3,7 @@ _:
   system.forbiddenDependenciesRegexes = [
     # Disallow compiling firefox/thunderbird ourselves
     "firefox-[0-9\\.]+$"
+    "librewolf-[0-9\\.]+$"
     "thunderbird-[0-9\\.]+$"
   ];
 }
