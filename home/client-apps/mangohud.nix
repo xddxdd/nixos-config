cff0b950d172d4d84f5634232d88103d84864782
Lan Tian <xuyh0120@outlook.com>
2025-11-29 23:28:04 -0800

    home: adjust mangohud style

--- home/client-apps/mangohud.nix
+++ home/client-apps/mangohud.nix
@@ -1,4 +1,9 @@
-{ osConfig, pkgs, ... }:
+{
+  osConfig,
+  pkgs,
+  lib,
+  ...
+}:
 let
   mangohudForCurrentKernel = pkgs.mangohud.override {
     inherit (osConfig.boot.kernelPackages.nvidia_x11.settings) libXNVCtrl;
@@ -9,6 +14,12 @@ in
     enable = true;
     enableSessionWide = true;
     package = mangohudForCurrentKernel;
+
+    settings = lib.mapAttrs (_: lib.mkForce) {
+      background_alpha = 0;
+      font_size = 16 * (osConfig.lantian.hidpi or 1);
+      font_scale = 1;
+    };
 
     settingsPerApplication = {
       mpv = {
