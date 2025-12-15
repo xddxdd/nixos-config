d986c8a0fb1af49b73a028c75a3e50d961514740
Lan Tian <xuyh0120@outlook.com>
2025-12-14 21:36:06 -0800

    hydra: try specify localhost as build machine

--- nixos/optional-apps/hydra.nix
+++ nixos/optional-apps/hydra.nix
@@ -1,6 +1,10 @@
-{ LT, ... }:
+{ LT, pkgs, ... }:
 {
   imports = [ ./postgresql.nix ];
+
+  environment.etc."hydra/machines".text = ''
+    localhost ${pkgs.stdenv.hostPlatform.system} - 8 1 kvm,nixos-test,big-parallel,benchmark - -
+  '';
 
   services.hydra = {
     enable = true;
@@ -8,5 +12,6 @@
     listenHost = LT.this.ltnet.IPv4;
     notificationSender = "postmaster@lantian.pub";
     port = LT.port.Hydra;
+    buildMachinesFiles = [ "/etc/hydra/machines" ];
   };
 }
