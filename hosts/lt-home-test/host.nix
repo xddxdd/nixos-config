5f5f22b9fdf856a1ac048afba087c14e00a37ff7
Lan Tian <xuyh0120@outlook.com>
2025-12-24 00:25:38 -0800

    general: update secrets

--- hosts/lt-home-test/host.nix
+++ hosts/lt-home-test/host.nix
@@ -1,7 +1,10 @@
 { tags, geo, ... }:
 {
   index = 102;
-  tags = with tags; [lan-access ];
+  tags = with tags; [
+    client
+    lan-access
+  ];
   cpuThreads = 8;
   city = geo.cities."US Seattle";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHq9gnvAZdEt84vZf83s4+T+3AhPVY/xz2o5qbqR8ftx";
