5f3dd7444648dd6f41bf8a671f709e90b90179a8
Lan Tian <xuyh0120@outlook.com>
2025-12-24 23:21:50 -0800

    nginx: fix accessing local services from network namespaces

--- nixos/common-apps/nginx/hosts.nix
+++ nixos/common-apps/nginx/hosts.nix
@@ -1,6 +1,11 @@
-{ config, lib, ... }:
 {
-  networking.hosts."127.0.0.1" = builtins.filter (lib.hasInfix ".") (
+  config,
+  lib,
+  LT,
+  ...
+}:
+{
+  networking.hosts."${LT.this.ltnet.IPv4}" = builtins.filter (lib.hasInfix ".") (
     (builtins.attrNames config.lantian.nginxVhosts)
     ++ (builtins.concatLists (
       lib.mapAttrsToList (k: v: v.serverAliases or [ ]) config.lantian.nginxVhosts
