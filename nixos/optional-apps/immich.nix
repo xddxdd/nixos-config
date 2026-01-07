8ea91610bcbc8a44b57608d583a7b78a9d2a5403
Lan Tian <xuyh0120@outlook.com>
2026-01-06 22:28:04 -0800

    immich: switch to vectorchord for vectors

--- nixos/optional-apps/immich.nix
+++ nixos/optional-apps/immich.nix
@@ -33,6 +33,10 @@
       host = "127.0.0.1";
       port = LT.port.Immich;
       mediaLocation = "/run/immich";
+      database = {
+        enableVectorChord = true;
+        enableVectors = false;
+      };
     };
 
     lantian.nginxVhosts = {
