76546bb9eb81bcd21a8bbcb9226e717fc81fbcdc
Lan Tian <xuyh0120@outlook.com>
2026-01-19 01:10:09 -0800

    open5gs: reenable rtpengine kernel module

--- nixos/optional-apps/open5gs/rtpengine.nix
+++ nixos/optional-apps/open5gs/rtpengine.nix
@@ -1,12 +1,12 @@
 {
   pkgs,
   lib,
-  LT,
+  config,
   ...
 }:
 {
-  # boot.kernelModules = [ "xt_RTPENGINE" ];
-  # boot.extraModulePackages = [ config.boot.kernelPackages.xt_rtpengine ];
+  boot.kernelModules = [ "xt_RTPENGINE" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.xt_rtpengine ];
 
   systemd.services.rtpengine = {
     description = "NGCP RTP/media Proxy Daemon";
@@ -14,31 +14,15 @@
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
 
-    serviceConfig = LT.serviceHarden // {
+    serviceConfig = {
       RuntimeDirectory = "rtpengine";
       CacheDirectory = "rtpengine";
       PIDFile = "/run/rtpengine/ngcp-rtpengine-daemon.pid";
       User = "rtpengine";
       Group = "rtpengine";
       LimitNOFILE = 150000;
-
-      PrivateMounts = false;
-      ProcSubset = "all";
-      AmbientCapabilities = [
-        "CAP_NET_ADMIN"
-        "CAP_SYS_NICE"
-      ];
-      CapabilityBoundingSet = [
-        "CAP_NET_ADMIN"
-        "CAP_SYS_NICE"
-      ];
-      RestrictAddressFamilies = [
-        "AF_INET"
-        "AF_INET6"
-        "AF_UNIX"
-        "AF_NETLINK"
-      ];
-      SystemCallFilter = [ "@system-service" ];
+      LimitMEMLOCK = 8388608;
+      UMask = "0077";
 
       Restart = "always";
       RestartSec = "5";
@@ -52,6 +36,50 @@
         "--pidfile /run/rtpengine/ngcp-rtpengine-daemon.pid"
         "--config-file ${./rtpengine.conf}"
       ];
+
+      # https://github.com/sipwise/rtpengine/blob/master/debian/ngcp-rtpengine-daemon.service
+      MemoryDenyWriteExecute = true;
+      ProcSubset = "all";
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectSystem = "strict";
+      ReadWritePaths = [ "/var/cache/rtpengine" ];
+      ProtectKernelLogs = true;
+      ProtectProc = "invisible";
+      SystemCallArchitectures = "native";
+      CapabilityBoundingSet = [
+        "CAP_NET_ADMIN"
+        "CAP_SYS_NICE"
+      ];
+      AmbientCapabilities = [
+        "CAP_NET_ADMIN"
+        "CAP_SYS_NICE"
+      ];
+      PrivateTmp = true;
+      PrivateDevices = true;
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      PrivateUsers = false;
+      RemoveIPC = true;
+      RestrictAddressFamilies = [
+        "AF_NETLINK"
+        "AF_UNIX"
+        "AF_INET"
+        "AF_INET6"
+      ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      PrivateNetwork = false;
+      DevicePolicy = "closed";
+      IPAddressAllow = "any";
+      SystemCallFilter = "@system-service";
+      SystemCallErrorNumber = "ENOSYS";
+      SystemCallLog = "~@system-service seccomp";
     };
   };
 
