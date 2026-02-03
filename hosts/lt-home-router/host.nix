27ff565188fe1cc0d66ab2f1c04970f8f31ad2a9
Lan Tian <xuyh0120@outlook.com>
2026-02-03 00:55:53 -0800

    hosts(lt-home-router): enable auto deploy

--- hosts/lt-home-router/host.nix
+++ hosts/lt-home-router/host.nix
@@ -3,7 +3,6 @@
   index = 112;
   tags = with tags; [ ];
   hostname = "192.168.0.1";
-  manualDeploy = true;
   cpuThreads = 4;
   city = geo.cities."US Seattle";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLpHoRAgRF8ub5vIm0Ah0ZF+0PgEJXP/2o2R9yAywqo";
