fae4ff15f354b6e717ca22d7ba3f159ed78db29e
Lan Tian <xuyh0120@outlook.com>
2026-02-11 19:04:08 -0800

    users: hardcode home path to avoid being overridden by modules

--- nixos/minimal-components/users.nix
+++ nixos/minimal-components/users.nix
@@ -39,6 +39,7 @@ in
       hashedPassword = lib.mkForce unixHashedPassword;
       openssh.authorizedKeys.keys = sshKeys;
       linger = LT.this.hasTag LT.tags.client;
+      home = "/root";
     };
     lantian = {
       hashedPassword = lib.mkForce unixHashedPassword;
@@ -57,6 +58,7 @@ in
       uid = 1000;
       openssh.authorizedKeys.keys = sshKeys;
       createHome = true;
+      home = lib.mkForce "/home/lantian";
       linger = LT.this.hasTag LT.tags.client;
     };
     nix-builder = lib.mkIf (LT.this.hasTag LT.tags.nix-builder) {
