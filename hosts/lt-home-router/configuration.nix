37b269bdd2a95da8a6d9a7fb8d3dfa7fe9a12529
Lan Tian <xuyh0120@outlook.com>
2026-02-15 20:53:11 -0800

    Revert "hosts(lt-home-router): enable suricata"
    This reverts commit c4ccd3775cdce1a65466eeea2bd0f2fa4e392863.

--- hosts/lt-home-router/configuration.nix
+++ hosts/lt-home-router/configuration.nix
@@ -1,4 +1,4 @@
-{ LT, ... }:
+{ ... }:
 {
   imports = [
     ../../nixos/minimal.nix
@@ -22,56 +22,5 @@
       "eth0.2"
       # "eth0.5" # IoT devices not allowed UPnP
     ];
-  };
-
-  services.suricata = {
-    enable = true;
-    settings = {
-      vars.address-groups.HOME_NET = builtins.concatStringsSep "," (
-        LT.constants.reserved.IPv4 ++ LT.constants.reserved.IPv6
-      );
-      host-mode = "router";
-      app-layer.protocols = {
-        telnet.enabled = "yes";
-        dnp3.enabled = "yes";
-        modbus.enabled = "yes";
-      };
-      outputs = [
-        {
-          fast = {
-            enabled = true;
-            filename = "fast.log";
-            append = "yes";
-          };
-        }
-        {
-          eve-log = {
-            enabled = true;
-            filetype = "regular";
-            filename = "eve.json";
-            community-id = true;
-            types = [
-              {
-                alert.tagged-packets = "yes";
-              }
-            ];
-          };
-        }
-      ];
-      af-packet =
-        builtins.map
-          (i: {
-            interface = i;
-            cluster-id = "99";
-            cluster-type = "cluster_flow";
-            defrag = "yes";
-          })
-          [
-            "eth0"
-            "eth0.1"
-            "eth0.2"
-            "eth1"
-          ];
-    };
   };
 }
