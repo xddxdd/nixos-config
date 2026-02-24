b411919459718834438b6a582f065f311f02fd89
Lan Tian <xuyh0120@outlook.com>
2026-02-23 17:12:50 -0800

    Revert "nix: use nix-ubw to orchestrate large builds"
    This reverts commit 032249df505325461e9ca15a968eea5854558124.

--- nixos/minimal-components/nix-ubw/package.nix
+++
@@ -1,9 +0,0 @@
-{
-  rustPlatform,
-  sources,
-}:
-rustPlatform.buildRustPackage {
-  inherit (sources.nix-ubw) pname version src;
-  cargoHash = "sha256-GUdP2NoumbpaLZg8loSNcB3qdtmO6+LZBNIjZWrFFBo=";
-  meta.mainProgram = "nix-ubw";
-}
