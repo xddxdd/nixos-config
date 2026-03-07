110a703588e2f99b57d28b8f717a2c81eeb2c281
Lan Tian <xuyh0120@outlook.com>
2026-03-07 11:34:09 -0800

    hosts(lt-home-lancache): exclude from auto deploy

--- hosts/lt-home-lancache/host.nix
+++ hosts/lt-home-lancache/host.nix
@@ -4,6 +4,7 @@
   tags = with tags; [ lan-access ];
   hostname = "192.168.0.4";
   cpuThreads = 4;
+  manualDeploy = true;
   city = geo.cities."US Seattle";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSEcy/2R5ApnR6nITL9HKfgYPd3zo0G4C3xrTMs5ldz";
   zerotier = "25fded4d0d";
