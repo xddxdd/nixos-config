eaf92fb6fb55d94aa2a4138410a640c410d77817
Lan Tian <xuyh0120@outlook.com>
2026-03-06 21:04:03 -0800

    hosts(lt-home-router): exclude from auto deploy

--- hosts/lt-home-router/host.nix
+++ hosts/lt-home-router/host.nix
@@ -4,6 +4,7 @@
   tags = with tags; [ ];
   hostname = "192.168.0.1";
   cpuThreads = 4;
+  manualDeploy = true;
   city = geo.cities."US Seattle";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLpHoRAgRF8ub5vIm0Ah0ZF+0PgEJXP/2o2R9yAywqo";
   zerotier = "e8b266c1c1";
