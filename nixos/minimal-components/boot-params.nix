267bf0c12040ea057a835cbffa540b263e94b273
Lan Tian <xuyh0120@outlook.com>
2026-03-18 01:38:44 -0700

    boot-params: randomly select grub theme

--- nixos/minimal-components/boot-params.nix
+++ nixos/minimal-components/boot-params.nix
@@ -3,8 +3,24 @@
   lib,
   LT,
   config,
+  inputs,
   ...
 }:
+let
+  installRandomStarRailGrubTheme = pkgs.writeShellScript "install-random-star-rail-grub-theme" ''
+    # Select random Star Rail Grub theme
+    THEME_PATH=$(ls -1 ${inputs.honkai-railway-grub-theme}/assets/themes | grep "_cn" | sort -R | head -n1)
+    echo "Randomly selected theme $THEME_PATH"
+    if [ -d "/boot/theme" ]; then
+      ${pkgs.rsync}/bin/rsync -ar --delete-after \
+        ${inputs.honkai-railway-grub-theme}/assets/themes/$THEME_PATH/ \
+        /boot/theme/
+      echo "Rsync complete"
+    else
+      echo "/boot/theme not found, not installing theme"
+    fi
+  '';
+in
 {
   boot.loader.efi.canTouchEfiVariables = false;
 
@@ -14,6 +30,10 @@
     font = lib.mkForce "${pkgs.nerd-fonts.ubuntu-mono}/share/fonts/truetype/NerdFonts/UbuntuMono/UbuntuMonoNerdFontMono-Regular.ttf";
     fontSize = lib.mkForce 16;
     efiInstallAsRemovable = config.boot.loader.grub.efiSupport;
+
+    extraInstallCommands = ''
+      ${installRandomStarRailGrubTheme}
+    '';
   };
 
   console.earlySetup = true;
@@ -23,6 +43,16 @@
     theme = "Evernight_cn";
   };
   stylix.targets.grub.enable = lib.mkForce false;
+  systemd.services.install-random-star-rail-grub-theme = {
+    description = "Install Random Star Rail Grub Theme";
+    after = [ "boot.mount" ];
+    requires = [ "boot.mount" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${installRandomStarRailGrubTheme}";
+      Type = "oneshot";
+    };
+  };
 
   systemd.services.systemd-sysctl.serviceConfig = {
     ExecStart = [
