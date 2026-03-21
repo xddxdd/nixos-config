6884d9d8f55477807d2e9f8e7520e2626147c892
Lan Tian <xuyh0120@outlook.com>
2026-03-21 00:57:12 -0700

    claude-code: add more config

---
+++ home/client-apps/claude-code.nix
@@ -0,0 +1,12 @@
+{ pkgs, config, ... }:
+{
+  home.packages = [ pkgs.claude-code ];
+
+  home.sessionVariables = {
+    CLAUDE_CONFIG_DIR = "${config.xdg.configHome}/claude";
+    CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
+    CLAUDE_CODE_ATTRIBUTION_HEADER = "0";
+    CLAUDE_CODE_PROXY_RESOLVES_HOSTS = "1";
+    ENABLE_TOOL_SEARCH = "1";
+  };
+}
