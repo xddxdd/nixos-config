08a91698c74046fed4a48ca441c86e0a9f6d5377
Lan Tian <xuyh0120@outlook.com>
2026-03-22 00:37:53 -0700

    n8n: fix ssl key

--- nixos/optional-apps/n8n.nix
+++ nixos/optional-apps/n8n.nix
@@ -89,7 +89,7 @@
       proxyWebsockets = true;
     };
 
-    sslCertificate = "zerossl-${config.networking.hostName}.xuyh0120.win";
+    sslCertificate = "zerossl-xuyh0120.win";
     noIndex.enable = true;
     accessibleBy = "private";
   };
