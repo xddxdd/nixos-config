d9be10e61bbf4300489782e8847c4f4d87fc235f
Lan Tian <xuyh0120@outlook.com>
2026-04-26 21:27:06 -0700

    libltnginx: fix data file

--- pkgs/libltnginx/resources/update.sh
+++ pkgs/libltnginx/resources/update.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p bash -p gitMinimal
 # shellcheck shell=bash
+
+set -euo pipefail
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+TEMPFILE=$(mktemp)
 
 for F in as_del_list ip6_del_list ip_del_list new_gtlds_list nic_handles_list tld_serv_list; do
-    wget "https://github.com/rfc1036/whois/raw/refs/heads/next/$F" -O "$SCRIPT_DIR/$F"
+    wget "https://github.com/rfc1036/whois/raw/refs/heads/next/$F" -O "$TEMPFILE"
+    cat "$TEMPFILE" > "$SCRIPT_DIR/$F"
 done
