3dd07ef565fb9d1cfd607e12002b9d4287c9a38b
Lan Tian <xuyh0120@outlook.com>
2026-04-29 22:06:45 -0700

    scx: switch to scx_flash hoping for better responsiveness

--- nixos/minimal-components/scx.nix
+++ nixos/minimal-components/scx.nix
@@ -1,9 +1,28 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  LT,
+  ...
+}:
 {
   services.scx = {
     # Broken on aarch64
     enable = pkgs.stdenv.isx86_64;
-    scheduler = "scx_pandemonium";
+    scheduler = "scx_flash";
+    extraArgs =
+      if LT.this.hasTag LT.tags.client then
+        [
+          "-m"
+          "all"
+        ]
+      else
+        [
+          "-m"
+          "performance"
+          "-w"
+          "-C"
+          "0"
+        ];
   };
 
   lantian.preservation.directories = [ "/root/.cache/pandemonium" ];
