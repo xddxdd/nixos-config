94f8a80924a0c69ef3c2609e6ed04c743bf88815
Lan Tian <xuyh0120@outlook.com>
2026-05-23 22:01:39 -0700

    xorg: revert to i915 driver

--- nixos/client-components/xorg.nix
+++ nixos/client-components/xorg.nix
@@ -11,9 +11,6 @@
     in
     [
       "i915.enable_guc=${builtins.toString enableGucFlag}"
-      # Use XE driver for i7-11800H
-      "i915.force_probe=!9a60"
-      "xe.force_probe=9a60"
     ];
 
   boot.kernel.sysctl = {
