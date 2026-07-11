749eafaa8ea6574e60b6bdb854b926bc790dbd5a
Lan Tian <xuyh0120@outlook.com>
2026-07-10 18:52:56 -0700

    hosts(alice): update IPv6 address

--- hosts/alice/configuration.nix
+++ hosts/alice/configuration.nix
@@ -12,20 +12,15 @@
   ];
 
   systemd.network.networks.eth0 = {
-    address = [ "2.26.205.141/23" ];
-    gateway = [ "2.26.204.1" ];
-    matchConfig.Name = "eth0";
-  };
-
-  networking.henet = {
-    enable = true;
-    remote = "216.218.221.6";
-    addresses = [
-      "2001:470:18:3de::2/64"
-      "2001:470:19:3db::1/64"
-      "2001:470:fa05::1/48"
+    address = [
+      "2.26.205.141/23"
+      "2a14:67c4:12::c3/64"
     ];
-    gateway = "2001:470:18:3de::1";
+    gateway = [
+      "2.26.204.1"
+      "2a14:67c4:12::1"
+    ];
+    matchConfig.Name = "eth0";
   };
 
   # DN42 legacy address
--- hosts/alice/host.nix
+++ hosts/alice/host.nix
@@ -11,15 +11,12 @@
     public-facing
     server
   ];
-  hostname = "2.26.205.141";
   city = geo.cities."CN Hong Kong";
   ssh.ed25519 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBiZ0rtz7/Ge4NkaVYDCkm7KEjgXOkslc8HmFhOr86dX";
   zerotier = "a863150d72";
   public = {
     IPv4 = "2.26.205.141";
-    IPv6 = "2001:470:19:3db::1";
-    IPv6Alt = "2001:470:fa05::1";
-    IPv6Subnet = "2001:470:fa05:ffff::";
+    IPv6 = "2a14:67c4:12::c3";
   };
   dn42 = {
     IPv4 = "172.22.76.186";
