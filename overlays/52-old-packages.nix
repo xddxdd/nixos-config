a25f85def3a18550f9455f375cb703e8c15be6f3
Lan Tian <xuyh0120@outlook.com>
2026-07-13 20:00:20 -0700

    bepasty: fix startup error

--- overlays/52-old-packages.nix
+++ overlays/52-old-packages.nix
@@ -1,9 +1,2 @@
-{ inputs, ... }:
-final: prev:
-let
-  pkgs-stable = inputs.nixpkgs-stable.legacyPackages."${final.system}";
-in
-rec {
-  # Bepasty doesn't build on nixos-unstable due to xstatic issue
-  inherit (pkgs-stable) bepasty;
+_: final: prev: rec {
 }
