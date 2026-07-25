c92c977f6f6e827ef52e16f107db7ae063685427
Lan Tian <xuyh0120@outlook.com>
2026-07-24 17:44:51 -0700

    postgresql: disable wal_init_zero and wal_recycle on COW FS

--- nixos/optional-apps/postgresql.nix
+++ nixos/optional-apps/postgresql.nix
@@ -2,8 +2,15 @@
   pkgs,
   lib,
   LT,
+  config,
   ...
 }:
+let
+  isBtrfsOrVirtiofsRoot = builtins.elem (config.fileSystems."/nix".fsType or "") [
+    "btrfs"
+    "virtiofs"
+  ];
+in
 {
   services.postgresql = {
     enable = true;
@@ -14,24 +21,28 @@
         pgvector
         vectorchord
       ];
-    settings.listen_addresses = lib.mkForce (
-      lib.concatStringsSep ", " [
-        "127.0.0.1"
-        "::1"
-        LT.this.ltnet.IPv4
-        LT.this.ltnet.IPv6
-      ]
-    );
     authentication = ''
       host all all 198.18.0.0/15 md5
       host all all fdbc:f9dc:67ad::/48 md5
     '';
     settings = {
+      listen_addresses = lib.mkForce (
+        lib.concatStringsSep ", " [
+          "127.0.0.1"
+          "::1"
+          LT.this.ltnet.IPv4
+          LT.this.ltnet.IPv6
+        ]
+      );
       max_connections = "1000";
       io_method = "io_uring";
       idle_in_transaction_session_timeout = "0";
       idle_session_timeout = "0";
-    };
+    }
+    // (lib.optionalAttrs isBtrfsOrVirtiofsRoot {
+      wal_init_zero = "off";
+      wal_recycle = "off";
+    });
   };
 
   systemd.services.postgresql.serviceConfig = LT.serviceHarden // {
