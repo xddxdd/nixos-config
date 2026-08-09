c8b63e48f83091990cd36da4faa22c8211bf0b92
Lan Tian <xuyh0120@outlook.com>
2026-08-08 23:55:25 -0700

    git: ignore pi temp files by default

--- home/client-apps/git.nix
+++ home/client-apps/git.nix
@@ -1,7 +1,12 @@
 { pkgs, lib, ... }:
 {
+  home.file.".gitignore".text = ''
+    .pi-*
+  '';
+
   programs.git = {
     package = lib.mkForce pkgs.git;
+    extraConfig.core.excludesfile = "~/.gitignore";
     signing = {
       key = "B50EC319385FCB0D";
       format = "openpgp";
