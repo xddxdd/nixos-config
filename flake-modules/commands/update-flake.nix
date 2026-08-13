b1a58fd84b05f88458f29c1d7004311301424882
Lan Tian <xuyh0120@outlook.com>
2026-08-13 00:06:25 -0700

    ci: auto update all flake inputs and push to new branch

--- flake-modules/commands/update-flake.nix
+++ flake-modules/commands/update-flake.nix
@@ -8,4 +8,10 @@ in
 ''
   nix flake update
   ${nvfetcherCmd}
+
+  for S in $(find nixos/ -name update.\*) $(find home/ -name update.\*) $(find pkgs/ -name update.\*); do
+    echo "Executing $S"
+    chmod +x "$S"
+    "$S"
+  done
 ''
