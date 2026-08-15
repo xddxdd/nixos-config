1eb59fcd011b208d796bfa6c67eba8eda80c3850
Lan Tian <xuyh0120@outlook.com>
2026-08-14 18:53:35 -0700

    homepage-dashboard: remove

--- hosts/lt-home-vm/configuration.nix
+++ hosts/lt-home-vm/configuration.nix
@@ -26,7 +26,6 @@
     ../../nixos/optional-apps/fastapi-dls.nix
     ../../nixos/optional-apps/glauth.nix
     ../../nixos/optional-apps/handbrake-server.nix
-    # ../../nixos/optional-apps/homepage-dashboard.nix
     ../../nixos/optional-apps/immich.nix
     ../../nixos/optional-apps/iyuuplus.nix
     ../../nixos/optional-apps/librechat.nix
