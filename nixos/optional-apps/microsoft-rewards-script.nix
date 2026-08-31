2ff0c3181e4c5175d106ed79bb864b3609ef6653
Lan Tian <xuyh0120@outlook.com>
2026-08-30 21:08:51 -0700

    microsoft-rewards-script: setup telegram notification

--- nixos/optional-apps/microsoft-rewards-script.nix
+++ nixos/optional-apps/microsoft-rewards-script.nix
@@ -6,6 +6,13 @@
 {
   sops.secrets.microsoft-rewards-script-env.sopsFile =
     inputs.secrets + "/microsoft-rewards-script.yaml";
+  sops.secrets.telegram-bot-token.sopsFile = inputs.secrets + "/common/telegram.yaml";
+  sops.secrets.telegram-bot-chat-id.sopsFile = inputs.secrets + "/common/telegram.yaml";
+  sops.templates.microsoft-rewards-script-additional-env.content = ''
+    CONFIG_TELEGRAM_ENABLED=true
+    CONFIG_TELEGRAM_BOTTOKEN=${config.sops.placeholder.telegram-bot-token}
+    CONFIG_TELEGRAM_CHATID=${config.sops.placeholder.telegram-bot-chat-id}
+  '';
 
   virtualisation.oci-containers.containers.microsoft-rewards-script = {
     image = "ghcr.io/thenetsky/microsoft-rewards-script:4";
@@ -17,7 +24,10 @@
       RUN_ON_START = "true";
       SKIP_RANDOM_SLEEP = "false";
     };
-    environmentFiles = [ config.sops.secrets.microsoft-rewards-script-env.path ];
+    environmentFiles = [
+      config.sops.secrets.microsoft-rewards-script-env.path
+      config.sops.templates.microsoft-rewards-script-additional-env.path
+    ];
     volumes = [
       "/var/lib/microsoft-rewards-script/config:/usr/src/microsoft-rewards-script/config"
       "/var/lib/microsoft-rewards-script/sessions:/usr/src/microsoft-rewards-script/sessions"
