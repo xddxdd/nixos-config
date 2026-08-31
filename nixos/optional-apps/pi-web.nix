ef38a41f6f50dd2bf98617df658794036d7140bc
Lan Tian <xuyh0120@outlook.com>
2026-08-31 02:25:23 -0700

    pi-web: run as user service

--- nixos/optional-apps/pi-web.nix
+++ nixos/optional-apps/pi-web.nix
@@ -1,33 +1,5 @@
+{ LT, ... }:
 {
-  lib,
-  LT,
-  pkgs,
-  ...
-}:
-{
-  systemd.services.pi-web = {
-    description = "Pi Web";
-    wantedBy = [ "multi-user.target" ];
-
-    path = [ pkgs.git ];
-
-    environment = {
-      PORT = LT.portStr.PiWeb;
-      PI_WEB_HOSTNAME = "127.0.0.1";
-      PI_WEB_NO_OPEN = "1";
-      PI_WEB_ALLOWED_HOSTS = "pi-web.localhost";
-    };
-
-    serviceConfig = {
-      ExecStart = lib.getExe pkgs.nur-xddxdd.pi-web;
-      User = "lantian";
-      Group = "lantian";
-
-      Restart = "always";
-      RestartSec = "5";
-    };
-  };
-
   lantian.localVhosts.pi-web = {
     locations = {
       "/" = {
