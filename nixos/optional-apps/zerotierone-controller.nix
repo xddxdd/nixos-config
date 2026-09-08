dd6c8bf7c0f155e52224486c02a66e2d7ca967ea
Lan Tian <xuyh0120@outlook.com>
2026-09-07 23:55:56 -0700

    networking: switch default gateway to pve-epyc for DN42 connectivity issue

--- nixos/optional-apps/zerotierone-controller.nix
+++ nixos/optional-apps/zerotierone-controller.nix
@@ -1,6 +1,7 @@
 { LT, lib, ... }:
 let
-  defaultGatewayHost = LT.hosts.lt-home-router;
+  # Target host MUST have DN42 connectivity
+  defaultGatewayHost = LT.hosts.pve-epyc;
   managedIPv4Ranges = LT.constants.dn42.IPv4 ++ LT.constants.neonetwork.IPv4 ++ [ "198.18.0.0/15" ];
   managedIPv6Ranges =
     LT.constants.dn42.IPv6 ++ LT.constants.neonetwork.IPv6 ++ [ "fdbc:f9dc:67ad::/48" ];
