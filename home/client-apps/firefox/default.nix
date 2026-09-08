bb075b3333c0dca4c566ab09ae29e158704bb1ad
Lan Tian <xuyh0120@outlook.com>
2026-09-07 23:55:30 -0700

    firefox: reenable webgl

--- home/client-apps/firefox/default.nix
+++ home/client-apps/firefox/default.nix
@@ -111,7 +111,7 @@ let
         "sidebar.verticalTabs" = true;
         "svg.context-properties.content.enabled" = true;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "webgl.disabled" = true;
+        "webgl.disabled" = false;
         # keep-sorted end
       }
       // builtins.listToAttrs (
