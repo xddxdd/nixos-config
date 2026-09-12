bf87b6891b5b35378886e04f1dd910016729dda4
Lan Tian <xuyh0120@outlook.com>
2026-09-12 12:10:16 -0700

    nix: add binary cache for llm-agents.nix

--- helpers/constants/nix.nix
+++ helpers/constants/nix.nix
@@ -2,9 +2,11 @@ _: {
   substituters = [
     "https://nix-gaming.cachix.org"
     "https://comfyui.cachix.org"
+    "https://cache.numtide.com"
   ];
   trusted-public-keys = [
     "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
+    "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
   ];
 }
