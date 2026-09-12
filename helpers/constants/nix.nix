91874ae230b867b8fe9f710bcd74099fa3b8f357
Lan Tian <xuyh0120@outlook.com>
2026-09-12 12:04:46 -0700

    nix: remove CUDA binary cache due to custom build

--- helpers/constants/nix.nix
+++ helpers/constants/nix.nix
@@ -1,11 +1,9 @@
 _: {
   substituters = [
-    "https://cache.nixos-cuda.org"
     "https://nix-gaming.cachix.org"
     "https://comfyui.cachix.org"
   ];
   trusted-public-keys = [
-    "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
   ];
