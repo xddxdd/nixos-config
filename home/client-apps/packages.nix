174a17874f1b70e76a9a1a4187b0fa1525daa2b0
Lan Tian <xuyh0120@outlook.com>
2026-09-12 23:48:26 -0700

    home: enable NVIDIA workaround for bambu studio

--- home/client-apps/packages.nix
+++ home/client-apps/packages.nix
@@ -57,6 +57,7 @@ in
         (LT.wrapNetns "tnl-buyvm" deluge)
         (LT.wrapNetns "tnl-buyvm" nur-xddxdd.amule-dlp)
         (LT.wrapNetns "tnl-buyvm" qbittorrent-enhanced)
+        (bambu-studio.override { withNvidiaGLWorkaround = osConfig.hardware.nvidia.enabled; })
         (hashcat.override { cudaSupport = true; })
         # error: collision between `/nix/store/2vkk2dnf693fzhlx7v2wn2kcvflgkih9-qqmusic-1.1.5/opt/LICENSE.electron.txt' and `/nix/store/zwgihw847calnxy6ff341l1qkilmn8hm-qq-3.2.2-18394/opt/LICENSE.electron.txt'
         (lib.hiPrio nur-xddxdd.qq)
@@ -64,7 +65,6 @@ in
         apache-directory-studio
         attic-client
         audacious
-        bambu-studio
         bitwarden-desktop
         brotli
         bzip2
