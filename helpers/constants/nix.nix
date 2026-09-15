8b1459117d51efb303bc93d6f81e2f05f519b7dc
Lan Tian <xuyh0120@outlook.com>
2026-09-15 01:01:30 -0700

    nix: add binary cache for static files

--- helpers/constants/nix.nix
+++ helpers/constants/nix.nix
@@ -3,10 +3,12 @@ _: {
     "https://nix-gaming.cachix.org"
     "https://comfyui.cachix.org"
     "https://cache.numtide.com"
+    "https://lab.pve-epyc.xuyh0120.win/nix-static-file-cache"
   ];
   trusted-public-keys = [
     "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
     "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    "nix-static-file-cache:JgdHxMl9VeWHOOULaTBnsWYJg57hYhJ76ALbNOvTe1s="
   ];
 }
