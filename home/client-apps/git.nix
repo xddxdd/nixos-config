e368f749413256432ea68f62ae652d8969a7b4b6
Lan Tian <xuyh0120@outlook.com>
2026-09-25 22:49:23 -0700

    git: move global gitignore file to xdg config dir

--- home/client-apps/git.nix
+++ home/client-apps/git.nix
@@ -1,13 +1,21 @@
-{ pkgs, lib, ... }:
 {
-  home.file.".gitignore".text = ''
-    .pi
-    .pi-*
-  '';
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  xdg.configFile."git/ignore" = {
+    text = ''
+      .pi
+      .pi-*
+    '';
+    force = true;
+  };
 
   programs.git = {
     package = lib.mkForce pkgs.git;
-    settings.core.excludesfile = "~/.gitignore";
+    settings.core.excludesfile = "${config.xdg.configHome}/git/ignore";
     signing = {
       key = "B50EC319385FCB0D";
       format = "openpgp";
